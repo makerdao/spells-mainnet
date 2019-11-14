7d761a27a1ca4eb1d7cf0c0f01f6d4bba214f35f
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2019-11-14 13:49:56 -0300

    Fix spell time check

--- src/DssTestsAfterSpell.sol
+++ src/DssTestsAfterSpell.sol
@@ -18,6 +18,15 @@ contract ChiefLike {
     function lift(address) public;
 }
 
+contract RegistryLike {
+    function build() public returns (ProxyLike);
+}
+
+contract ProxyLike {
+    function execute(address, bytes memory) public;
+}
+
+
 contract Hevm {
     function warp(uint) public;
 }
@@ -30,8 +39,11 @@ contract DssTestsAfterSpell is DSTest {
     Vow vow;
     ERC20 gov;
     ChiefLike chief;
+    address manager;
+    ProxyLike proxy;
 
     TubLike tub;
+
     DssInitSpell spell;
 
     uint constant RAD = 10 ** 45;
@@ -42,6 +54,8 @@ contract DssTestsAfterSpell is DSTest {
         vow = Vow(0xA950524441892A31ebddF91d3cEEFa04Bf454466);
         chief = ChiefLike(0x9eF05f7F6deB616fd37aC3c959a2dDD25A54E4F5);
         gov = ERC20(0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2);
+        manager = 0x5ef30b9986345249bc32d8928B7ee64DE9435E39;
+        RegistryLike registry = RegistryLike(0x4678f0a6958e4D2Bc4F1BAF7Bc52E8F3564f3fE4);
 
         tub = TubLike(0x448a5065aeBB8E423F0896E6c5D525C040f59af3);
 
@@ -49,6 +63,8 @@ contract DssTestsAfterSpell is DSTest {
 
         hevm = Hevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
         hevm.warp(1574092700);
+
+        proxy = registry.build();
     }
 
     function vote() private {
@@ -63,16 +79,15 @@ contract DssTestsAfterSpell is DSTest {
         chief.vote(vote);
         chief.lift(address(spell));
         assertEq(chief.hat(), address(spell));
-
-        spell.schedule();
     }
 
     function waitAndCast() public {
         hevm.warp(1574092800);
+        spell.schedule();
         spell.cast();
     }
 
-    function testSpellCasted() public {
+    function testSpellIsCasted() public {
         assertEq(vat.Line(), 0);
         (,,, uint line,) = vat.ilks("ETH-A");
         assertEq(line, 0);
@@ -86,7 +101,7 @@ contract DssTestsAfterSpell is DSTest {
 
         assertTrue(spell.done());
         assertEq(vat.Line(), 153000000 * RAD);
-        (,,, uint line,) = vat.ilks("ETH-A");
+        (,,, line,) = vat.ilks("ETH-A");
         assertEq(line, 50000000 * RAD);
         (,,, line,) = vat.ilks("BAT-A");
         assertEq(line, 3000000 * RAD);
@@ -94,13 +109,20 @@ contract DssTestsAfterSpell is DSTest {
         assertEq(line, 100000000 * RAD);
     }
 
-    function testFailSpellCasted() public {
+    function testFailSpellSchedule() public {
         vote();
-
-        spell.cast();
+        spell.schedule();
     }
 
+    // function createETHVault() public {
+    //     vote();
+    //     waitAndCast();
+
+    // }
+
 }
+
+// Launch activation spell:
 
 contract VatLike {
     function file(bytes32,uint) public;
@@ -142,13 +164,13 @@ contract DssInitSpell {
     }
 
     function schedule() public {
+        require(now >= 1574092800, "launch-time-error");
         require(eta == 0, "spell-already-scheduled");
         eta = now + PauseLike(pause).delay();
         pause.plot(action, tag, sig, eta);
     }
 
     function cast() public {
-        require(now >= 1574092800, "time-error");
         require(!done, "spell-already-cast");
         pause.exec(action, tag, sig, eta);
         done = true;
--- test-after-spell.sh
+++ test-after-spell.sh
@@ -6,6 +6,6 @@ set -e
 # [[ $1 ]] && MATCH="--match $1"
 dapp build 
 
-LANG=C.UTF-8 DAPP_TEST_ADDRESS=0x8EE7D9235e01e6B42345120b5d270bdB763624C7 hevm dapp-test --rpc=$ETH_RPC_URL --json-file=out/dapp.sol.json --dapp-root=. --match "testSpellCasted"
+LANG=C.UTF-8 DAPP_TEST_ADDRESS=0x8EE7D9235e01e6B42345120b5d270bdB763624C7 hevm dapp-test --rpc=$ETH_RPC_URL --json-file=out/dapp.sol.json --dapp-root=. --match "testSpellIsCasted"
 
-LANG=C.UTF-8 DAPP_TEST_ADDRESS=0x8EE7D9235e01e6B42345120b5d270bdB763624C7 hevm dapp-test --rpc=$ETH_RPC_URL --json-file=out/dapp.sol.json --dapp-root=. --match "testFailSpellCasted"
+LANG=C.UTF-8 DAPP_TEST_ADDRESS=0x8EE7D9235e01e6B42345120b5d270bdB763624C7 hevm dapp-test --rpc=$ETH_RPC_URL --json-file=out/dapp.sol.json --dapp-root=. --match "testFailSpellSchedule"
