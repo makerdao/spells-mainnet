78772feaa1d8e67b2fb870c304084c1c44e1976a
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2019-11-14 10:45:33 -0500

    add some ilk tests as examples

--- src/DssTests.t.sol
+++ src/DssTests.t.sol
@@ -1,15 +1,31 @@
 pragma solidity ^0.5.12;
+pragma experimental ABIEncoderV2;
 
 import "ds-test/test.sol";
 
 import "dss/dai.sol";
 import "dss/vat.sol";
-import "dss/vow.sol";
+import {Vow} from "dss/vow.sol";
+
+contract VatLike {
+    struct Ilk {
+        uint256 Art;
+        uint256 rate;
+        uint256 spot;
+        uint256 line;
+        uint256 dust;
+    }
+
+    function ilks(bytes32) external view returns (Ilk memory);
+}
 
 contract DssTests is DSTest {
     Dai dai;
     Vat vat;
     Vow vow;
+    VatLike.Ilk ethIlk;
+    VatLike.Ilk batIlk;
+    VatLike.Ilk saiIlk;
 
     function setUp() public {
         dai = Dai(0x6B175474E89094C44Da98b954EedeAC495271d0F);
@@ -23,10 +39,32 @@ contract DssTests is DSTest {
 
     function test_vat() public {
         assertEq(vat.live(), 1);
-
-        (uint256 Art,,,,) = vat.ilks("ETH-A");
-        assertEq(Art, 0);
     }
+
+    function test_eth_a_ilk_init() public {
+        ethIlk = VatLike(address(vat)).ilks("ETH-A");
+        assertTrue(ethIlk.rate > 0);
+    }
+
+    function test_eth_a_ilk_off() public {
+        assertEq(ethIlk.Art, 0);
+        assertEq(ethIlk.line, 0);
+        assertEq(ethIlk.dust, 0);
+    }
+
+    function test_bat_a_ilk_init() public {
+        batIlk = VatLike(address(vat)).ilks("BAT-A");
+        assertEq(batIlk.Art, 0);
+        assertTrue(batIlk.rate > 0);
+    }
+
+    function test_sai_ilk_init() public {
+        saiIlk = VatLike(address(vat)).ilks("SAI");
+        assertEq(saiIlk.Art, 0);
+        assertTrue(saiIlk.rate > 0);
+    }
+
+
 
     function test_vow() public {
         assertEq(vow.live(), 1);
