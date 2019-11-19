b52f8db8cb1d7f0035b825fb645ece1d3208caa4
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2019-11-19 15:12:48 -0500

    add initial MkrAuthority test

--- src/MkrAuthority.t.sol
+++ src/MkrAuthority.t.sol
@@ -7,25 +7,32 @@ import {Vat} from "dss/vat.sol";
 import {Vow} from "dss/vow.sol";
 import {Flopper} from "dss/flop.sol";
 import {Flapper} from "dss/flap.sol";
-import {DSToken} from "ds-token/ds-token.sol";
 
-import {MkrAuthority} from "mkr-authority";
+import {MkrAuthority} from "mkr-authority/MkrAuthority.sol";
+
+/**
+ * Must be executed from the Multisig address.
+ * Use ./test-mkr-authority.sh to test
+ */
 
 contract Hevm {
     function warp(uint) public;
 }
 
-// multisig = 0x8EE7D9235e01e6B42345120b5d270bdB763624C7
+contract ChiefLike {
+    function hat() public view returns (address);
+    function lock(uint) public;
+    function vote(address[] memory) public;
+    function lift(address) public;
+}
 
-contract MultiSig {
-    DSToken gov;
-
-    constructor(address gov_) public {
-        gov = DSToken(gov_);
-    }
-    function setAuthority(address auth) public {
-        gov.setAuthority(auth);
-    }
+contract MkrLike {
+    function setAuthority(address whom) external;
+    function setOwner(address whom) external;
+    function owner() external returns(address);
+    function authority() external returns(address);
+    function approve(address, uint256) external returns(bool);
+    function balanceOf(address) external returns(uint256);
 }
 
 contract MkrAuthorityTest is DSTest {
@@ -34,14 +41,13 @@ contract MkrAuthorityTest is DSTest {
     Dai dai     = Dai(0x6B175474E89094C44Da98b954EedeAC495271d0F);
     Vat vat     = Vat(0x35D1b3F3D7966A1DFe207aa4514C12a259A0492B);
     Vow vow     = Vow(0xA950524441892A31ebddF91d3cEEFa04Bf454466);
-    Flopper flop = Flopper(0xbe00fe8dfd9c079f1e5f5ad7ae9a3ad2c571fcac);
-    Flapper flap = Flapper(0xdfe0fb1be2a52cdbf8fb962d5701d7fd0902db9f);
-    DSToken gov = DSToken(0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2);
+    Flopper flop = Flopper(0xBE00FE8Dfd9C079f1E5F5ad7AE9a3Ad2c571FCAC);
+    Flapper flap = Flapper(0xdfE0fb1bE2a52CDBf8FB962D5701d7fd0902db9f);
+
+    MkrLike gov     = MkrLike(0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2);
+    ChiefLike chief = ChiefLike(0x9eF05f7F6deB616fd37aC3c959a2dDD25A54E4F5);
 
     MkrAuthority mkrauth;
-
-    // TODO: This can be removed once the spell is cast
-    DssLaunchSpell spell = DssLaunchSpell(0xF44113760c4f70aFeEb412C63bC713B13E6e202E);
 
     uint constant RAD = 10 ** 45;
 
@@ -50,34 +56,17 @@ contract MkrAuthorityTest is DSTest {
         hevm.warp(1574092700);
 
         mkrauth = new MkrAuthority();
-
-        // TODO: This can be removed once the spell is cast
-        castSpell();
     }
-
-    // TODO: This can be removed once the spell is cast
-    function castSpell() private {
-        gov.approve(address(chief), uint256(-1));
-        chief.lock(gov.balanceOf(address(this)));
-
-        assertTrue(!spell.done());
-
-        address[] memory vote = new address[](1);
-        vote[0] = address(spell);
-
-        chief.vote(vote);
-        chief.lift(address(spell));
-        assertEq(chief.hat(), address(spell));
-        // Let's push the time to the launch moment
-        hevm.warp(1574092800);
-        spell.cast();
-    }
-
-    function addMkrAuth() private {}
 
     function setupDebtAuction() public {}
 
     function setupSurplusAuction() public {}
+
+    function test_canAddMkrAuth() public {
+        assertTrue(gov.authority() == address(0));
+        gov.setAuthority(address(mkrauth));
+        assertTrue(gov.authority() == address(mkrauth));
+    }
 
     function testFail_cannotDealFlop() public {}
     function testFail_cannotDealFlap() public {}
