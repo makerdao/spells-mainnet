31b6a0731fef603b70dd2eaba92fb9a7eefb7f02
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2019-11-20 16:48:06 -0500

    add test setup that relys the vat on the multisig

--- src/MkrAuthority.t.sol
+++ src/MkrAuthority.t.sol
@@ -15,8 +15,14 @@ import {MkrAuthority} from "mkr-authority/MkrAuthority.sol";
  * Use ./test-mkr-authority.sh to test
  */
 
-contract Hevm {
-    function warp(uint) public;
+// contract Hevm {
+//     function warp(uint) public;
+// }
+
+contract PauseLike {
+    function delay() public view returns (uint256);
+    function plot(address, bytes32, bytes memory, uint256) public;
+    function exec(address, bytes32, bytes memory, uint256) public;
 }
 
 contract ChiefLike {
@@ -35,8 +41,54 @@ contract MkrLike {
     function balanceOf(address) external returns(uint256);
 }
 
+contract VatLike {
+    function rely(address) public;
+    function wards(address) public returns(uint256);
+    function Line() public returns(uint256);
+}
+
+contract TakeOverSpellAction {
+    address public owner; // placeholder for pauseproxy
+    address constant multisig = 0x8EE7D9235e01e6B42345120b5d270bdB763624C7;
+    address constant vat = 0x35D1b3F3D7966A1DFe207aa4514C12a259A0492B;
+
+    function execute() public {
+        VatLike(vat).rely(multisig);
+        require(owner == 0xbE286431454714F511008713973d3B053A2d38f3, "wrong-owner");
+        require(multisig != 0xbE286431454714F511008713973d3B053A2d38f3, "multi-still-pause");
+        require(multisig == 0x8EE7D9235e01e6B42345120b5d270bdB763624C7, "wrong-multi");
+        require(VatLike(vat).wards(multisig) == 1, "Vat/is-ward-now");
+    }
+}
+
+contract TakeOverSpell {
+    PauseLike public pause =
+        PauseLike(0xbE286431454714F511008713973d3B053A2d38f3);
+    address   public action;
+    bytes32   public tag;
+    bytes     public sig;
+    // bool      public done;
+
+    constructor() public {
+        sig = abi.encodeWithSignature("execute()");
+        action = address(new TakeOverSpellAction());
+        bytes32 _tag;
+        address _action = action;
+        assembly { _tag := extcodehash(_action) }
+        tag = _tag;
+    }
+
+    function cast() public {
+        // require(!done, "spell-already-cast");
+        // done = true;
+        pause.plot(action, tag, sig, now);
+        pause.exec(action, tag, sig, now);
+        // emit log_bytes32("Test");
+    }
+}
+
 contract MkrAuthorityTest is DSTest {
-    Hevm hevm;
+    // Hevm hevm;
 
     Dai dai     = Dai(0x6B175474E89094C44Da98b954EedeAC495271d0F);
     Vat vat     = Vat(0x35D1b3F3D7966A1DFe207aa4514C12a259A0492B);
@@ -52,10 +104,37 @@ contract MkrAuthorityTest is DSTest {
     uint constant RAD = 10 ** 45;
 
     function setUp() public {
-        hevm = Hevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
-        hevm.warp(1574092700);
+        // hevm = Hevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+        // hevm.warp(1574092700);
+        // hevm.warp(now);
 
         mkrauth = new MkrAuthority();
+    }
+
+    function masterChief() private {
+        gov.approve(address(chief), uint256(-1));
+        chief.lock(gov.balanceOf(address(this)));
+        address[] memory vote = new address[](1);
+
+        TakeOverSpell spell = new TakeOverSpell();
+
+        vote[0] = address(spell);
+        chief.vote(vote);
+        chief.lift(address(spell));
+        assertEq(chief.hat(), address(spell));
+        spell.cast();
+        // assertTrue(spell.done());
+        // assertEq(vat.wards(address(this)), 1);
+    }
+
+    function setupFlop() private {
+        masterChief();
+        // vat.suck(address(vow), address(this), vow.sump());
+        // vow.flop();
+    }
+
+    function bidFlop() private {
+
     }
 
     function setupDebtAuction() private {}
@@ -74,8 +153,10 @@ contract MkrAuthorityTest is DSTest {
         assertTrue(gov.owner() == address(0));
     }
 
-    function testFail_cannotDealFlop() public {
+    function test_cannotDealFlop() public {
         assertTrue(gov.authority() == address(0));
+        setupFlop();
+        // bidFlop();
     }
 
     function testFail_cannotDealFlap() public {}
