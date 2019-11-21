bea4e02b1b14589a177368c301c65d4aa42b8481
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2019-11-21 14:52:36 -0500

    set up flop tests

--- src/MkrAuthority.t.sol
+++ src/MkrAuthority.t.sol
@@ -15,9 +15,9 @@ import {MkrAuthority} from "mkr-authority/MkrAuthority.sol";
  * Use ./test-mkr-authority.sh to test
  */
 
-// contract Hevm {
-//     function warp(uint) public;
-// }
+contract Hevm {
+    function warp(uint) public;
+}
 
 contract PauseLike {
     function delay() public view returns (uint256);
@@ -48,15 +48,11 @@ contract VatLike {
 }
 
 contract TakeOverSpellAction {
-    address public owner; // placeholder for pauseproxy
-    address public multisig = 0x8EE7D9235e01e6B42345120b5d270bdB763624C7;
+    address constant multisig = 0x8EE7D9235e01e6B42345120b5d270bdB763624C7;
     address constant vat = 0x35D1b3F3D7966A1DFe207aa4514C12a259A0492B;
 
     function execute() public {
         VatLike(vat).rely(multisig);
-        require(owner == 0xbE286431454714F511008713973d3B053A2d38f3, "wrong-owner");
-        require(multisig != 0xbE286431454714F511008713973d3B053A2d38f3, "multi-still-pause");
-        require(multisig == 0x8EE7D9235e01e6B42345120b5d270bdB763624C7, "wrong-multi");
         require(VatLike(vat).wards(multisig) == 1, "Vat/is-ward-now");
     }
 }
@@ -67,7 +63,6 @@ contract TakeOverSpell {
     address   public action;
     bytes32   public tag;
     bytes     public sig;
-    // bool      public done;
 
     constructor() public {
         sig = abi.encodeWithSignature("execute()");
@@ -79,16 +74,13 @@ contract TakeOverSpell {
     }
 
     function cast() public {
-        // require(!done, "spell-already-cast");
-        // done = true;
         pause.plot(action, tag, sig, now);
         pause.exec(action, tag, sig, now);
-        // emit log_bytes32("Test");
     }
 }
 
 contract MkrAuthorityTest is DSTest {
-    // Hevm hevm;
+    Hevm hevm;
 
     Dai dai     = Dai(0x6B175474E89094C44Da98b954EedeAC495271d0F);
     Vat vat     = Vat(0x35D1b3F3D7966A1DFe207aa4514C12a259A0492B);
@@ -101,10 +93,11 @@ contract MkrAuthorityTest is DSTest {
 
     MkrAuthority mkrauth;
 
-    uint constant RAD = 10 ** 45;
+    uint256  constant RAD = 10 ** 45;
+    uint256  constant ONE = 1.00E18;
 
     function setUp() public {
-        // hevm = Hevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+        hevm = Hevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
         // hevm.warp(1574092700);
         // hevm.warp(now);
 
@@ -123,18 +116,42 @@ contract MkrAuthorityTest is DSTest {
         chief.lift(address(spell));
         assertEq(chief.hat(), address(spell));
         spell.cast();
-        // assertTrue(spell.done());
-        // assertEq(vat.wards(address(this)), 1);
+        assertEq(vat.wards(address(this)), 1);
     }
 
-    function setupFlop() private {
-        masterChief();
-        // vat.suck(address(vow), address(this), vow.sump());
-        // vow.flop();
+    function setupMkrAuth() private {
+        gov.setAuthority(address(mkrauth));
+        mkrauth.rely(address(flop));
     }
 
-    function bidFlop() private {
+    function setupFlop() private returns(uint) {
+        masterChief(); // Allows `vat.suck`
+        uint256 surplus = vat.dai(address(vow));
+        uint256 Sin = vow.Sin();
+        uint256 Ash = vow.Ash();
+        uint256 sump = vow.sump();
+        vat.suck(address(vow), address(this), surplus + sump + Ash + Sin);
+        vow.heal(surplus);
+        // assertTrue(vat.dai(address(vow)) == 0);
+        // assertTrue(vat.dai(address(this)) == surplus + sump + Ash + Sin);
+        // uint256 sin = vat.sin(address(vow));
+        // emit log_named_uint("surp", vat.dai(address(vow)));
+        // emit log_named_uint("debt", sin);
+        // emit log_named_uint("sump", sump);
+        // emit log_named_uint("Sin ", Sin);
+        // emit log_named_uint("Ash ", Ash);
+        // emit log_named_uint("debt", sin - Sin - Ash);
+        // assertTrue(sump <= sin - Sin - Ash);
+        return vow.flop();
+    }
 
+    function bidFlop(uint256 id) private {
+        vat.hope(address(flop));
+        uint256 bid = vow.sump();
+        uint256 lot = 1;
+        emit log_named_uint('lot', lot);
+        emit log_named_uint('bid', bid);
+        flop.dent(id, lot, bid);
     }
 
     function setupDebtAuction() private {}
@@ -143,7 +160,7 @@ contract MkrAuthorityTest is DSTest {
 
     function test_canAddMkrAuth() public {
         assertTrue(gov.authority() == address(0));
-        gov.setAuthority(address(mkrauth));
+        setupMkrAuth();
         assertTrue(gov.authority() == address(mkrauth));
     }
 
@@ -153,17 +170,39 @@ contract MkrAuthorityTest is DSTest {
         assertTrue(gov.owner() == address(0));
     }
 
-    function test_cannotDealFlop() public {
+    function testFail_cannotDealFlop() public {
         assertTrue(gov.authority() == address(0));
-        setupFlop();
-        // bidFlop();
+        uint flopId = setupFlop();
+        bidFlop(flopId);
+        hevm.warp(uint48(now) + flop.ttl() + 1);
+
+        flop.deal(flopId);
     }
 
     function testFail_cannotDealFlap() public {}
 
-    function test_canDealFlop_new() public {}
+    function test_canDealFlop_new() public {
+        setupMkrAuth();
+
+        uint flopId = setupFlop();
+        bidFlop(flopId);
+        hevm.warp(uint48(now) + flop.ttl() + 1);
+
+        flop.deal(flopId);
+    }
+
     function test_canDealFlap_new() public {}
 
-    function test_canDealFlop_stuck() public {}
+    function test_canDealFlop_stuck() public {
+        assertTrue(gov.authority() == address(0));
+        uint flopId = setupFlop();
+        bidFlop(flopId);
+        hevm.warp(uint48(now) + flop.ttl() + 1);
+
+        setupMkrAuth();
+
+        flop.deal(flopId);
+    }
+
     function test_canDealFlap_stuck() public {}
 }
--- test-mkrauthority.sh
+++ test-mkrauthority.sh
@@ -8,5 +8,6 @@ dapp build
 
 # LANG=C.UTF-8 DAPP_TEST_ADDRESS=0x8EE7D9235e01e6B42345120b5d270bdB763624C7 hevm dapp-test --rpc="$ETH_RPC_URL" --json-file=out/dapp.sol.json --dapp-root=. --match "test_canRemoveOwner" # --verbose 1
 
-# LANG=C.UTF-8 DAPP_TEST_ADDRESS=0x8EE7D9235e01e6B42345120b5d270bdB763624C7 hevm dapp-test --rpc="$ETH_RPC_URL" --json-file=out/dapp.sol.json --dapp-root=. --match "testFail_cannotDealFlop" # --verbose 1
-LANG=C.UTF-8 DAPP_TEST_ADDRESS=0x8EE7D9235e01e6B42345120b5d270bdB763624C7 hevm dapp-test --rpc="$ETH_RPC_URL" --json-file=out/dapp.sol.json --dapp-root=. --match "test_cannotDealFlop" --verbose 1
+LANG=C.UTF-8 DAPP_TEST_ADDRESS=0x8EE7D9235e01e6B42345120b5d270bdB763624C7 hevm dapp-test --rpc="$ETH_RPC_URL" --json-file=out/dapp.sol.json --dapp-root=. --match "testFail_cannotDealFlop" # --verbose 1
+LANG=C.UTF-8 DAPP_TEST_ADDRESS=0x8EE7D9235e01e6B42345120b5d270bdB763624C7 hevm dapp-test --rpc="$ETH_RPC_URL" --json-file=out/dapp.sol.json --dapp-root=. --match "test_canDealFlop_stuck" # --verbose 1
+LANG=C.UTF-8 DAPP_TEST_ADDRESS=0x8EE7D9235e01e6B42345120b5d270bdB763624C7 hevm dapp-test --rpc="$ETH_RPC_URL" --json-file=out/dapp.sol.json --dapp-root=. --match "test_canDealFlop_new" # --verbose 1
