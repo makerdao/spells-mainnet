6b7449d70dcc104909218b8b5017d987938980c9
Christopher Mooney <chris@dod.net>
2019-12-12 17:24:59 -0500

    most of the test needed, still WIP

--- src/DssIncreaseDelay24Spell.t.sol
+++ src/DssIncreaseDelay24Spell.t.sol
@@ -37,6 +37,18 @@ contract Hevm {
     function warp(uint) public;
 }
 
+contract OSMLike {
+    function rely(address) external;
+    function wards(address) external view returns (uint256);
+}
+
+contract OSMMomLike {
+    function authority() external view returns (address);
+    function osms(bytes32) external view returns (address);
+    function setAuthority(address) external;
+    function setOsm(bytes32, address) external;
+}
+
 contract DssIncreaseDelay24SpellTest is DSTest {
     Hevm hevm;
 
@@ -44,6 +56,9 @@ contract DssIncreaseDelay24SpellTest is DSTest {
         PauseLike(0xbE286431454714F511008713973d3B053A2d38f3);
     ERC20 gov = ERC20(0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2);
     ChiefLike chief = ChiefLike(0x9eF05f7F6deB616fd37aC3c959a2dDD25A54E4F5);
+    OSMLike eth_osm = OSMLike(0x81fe72b5a8d1a857d176c3e7d5bd2679a9b85763);
+    OSMLike bat_osm = OSMLike(0xb4eb54af9cc7882df0121d26c5b97e802915abe6);
+    OSMMomLike osm_mom = OSMMomLike(address(0)); // TODO(cmooney) fix me
 
     DssIncreaseDelay24Spell spell;
 
@@ -62,8 +77,9 @@ contract DssIncreaseDelay24SpellTest is DSTest {
     }
 
     function testIncreaseDelay24SpellIsCast() public {
-        spell = DssIncreaseDelay24Spell(0x1A7D50b73ACf1D2b4073Ca5F94890A3C05C01401);
-        // spell = new DssIncreaseDelay24Spell();
+        spell = DssIncreaseDelay24Spell(
+            //TODO(cmooney) deployed script
+        );
 
         assertEq(pause.delay(), 0);
 
@@ -71,12 +87,26 @@ contract DssIncreaseDelay24SpellTest is DSTest {
         spell.cast();
 
         // test ETH_OSM rely on OSM_MOM
+        assertEq(eth_osm.wards(address(osm_mom)), 1);
+
         // test BAT_OSM rely on OSM_MOM
+        assertEq(bat_osm.wards(address(osm_mom)), 1);
+
         // test OSM_MOM Authority is Chief
+        // TODO(brian): make sure the interface is conformant
+        assertEq(osm_mom.authority(), address(chief));
+
         // test OSM_MOM has OSM for ETH-A
+        assertEq(osm_mom.osms('ETH-A'), address(eth_osm));
+
         // test OSM_MOM has OSM for BAT-A
+        assertEq(osm_mom.osms('BAT-A'), address(bat_osm));
+
+        // test that the new pause delay is 24 hours
         assertEq(pause.delay(), 60 * 60 * 24);
+
+        fail();
     }
 
-    // add test for checking canCall on OSM-MOM
+    // TODO(iamchrissmith): add test for checking canCall on OSM-MOM
 }
