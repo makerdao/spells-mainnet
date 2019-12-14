530338944e34813990e8adff013e74af9834395d
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2019-12-14 01:03:04 -0500

    add initial OSM-MOM setup to spell and spec out tests needed (#7)
    * add initial OSM-MOM setup to spell and spec out tests needed
    
    * add OSMMomLike interface
    
    * most of the test needed, still WIP
    
    * Remove comment, osm mom is conformant
    
    * adding a few more tests
    
    * Add deployed OsmMom address, fix test
    
    * Add deployed spell, fix fail test case

--- src/DssIncreaseDelay24Spell.sol
+++ src/DssIncreaseDelay24Spell.sol
@@ -5,11 +5,35 @@ contract PauseLike {
     function setDelay(uint256) external;
     function plot(address, bytes32, bytes calldata, uint256) external;
     function exec(address, bytes32, bytes calldata, uint256) external;
+    function owner() external returns(address);
+}
+
+contract OSMLike {
+    function rely(address) external;
+}
+
+contract OSMMomLike {
+    function setAuthority(address) external;
+    function setOsm(bytes32, address) external;
 }
 
 contract DssIncreaseDelay24SpellAction {
     address constant public PAUSE = 0xbE286431454714F511008713973d3B053A2d38f3;
+    address constant public CHIEF = 0x9eF05f7F6deB616fd37aC3c959a2dDD25A54E4F5;
+
+    address constant public ETH_OSM = 0x81FE72B5A8d1A857d176C3E7d5Bd2679A9B85763;
+    address constant public BAT_OSM = 0xB4eb54AF9Cc7882DF0121d26c5b97E802915ABe6;
+
+    address constant public OSM_MOM = 0x76416A4d5190d071bfed309861527431304aA14f;
+
     function execute() external {
+        OSMLike(ETH_OSM).rely(OSM_MOM);
+        OSMLike(BAT_OSM).rely(OSM_MOM);
+
+        OSMMomLike(OSM_MOM).setAuthority(CHIEF);
+        OSMMomLike(OSM_MOM).setOsm("ETH-A", ETH_OSM);
+        OSMMomLike(OSM_MOM).setOsm("BAT-A", BAT_OSM);
+
         PauseLike(PAUSE).setDelay(60 * 60 * 24);
     }
 }
--- src/DssIncreaseDelay24Spell.t.sol
+++ src/DssIncreaseDelay24Spell.t.sol
@@ -37,6 +37,20 @@ contract Hevm {
     function warp(uint) public;
 }
 
+contract OSMLike {
+    function rely(address) external;
+    function wards(address) external view returns (uint256);
+}
+
+contract OSMMomLike {
+    function authority() external view returns (address);
+    function osms(bytes32) external view returns (address);
+    function owner() external view returns (address);
+    function setAuthority(address) external;
+    function setOsm(bytes32, address) external;
+    function stop(bytes32) external;
+}
+
 contract DssIncreaseDelay24SpellTest is DSTest {
     Hevm hevm;
 
@@ -44,6 +58,11 @@ contract DssIncreaseDelay24SpellTest is DSTest {
         PauseLike(0xbE286431454714F511008713973d3B053A2d38f3);
     ERC20 gov = ERC20(0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2);
     ChiefLike chief = ChiefLike(0x9eF05f7F6deB616fd37aC3c959a2dDD25A54E4F5);
+    OSMLike eth_osm = OSMLike(0x81FE72B5A8d1A857d176C3E7d5Bd2679A9B85763);
+    OSMLike bat_osm = OSMLike(0xB4eb54AF9Cc7882DF0121d26c5b97E802915ABe6);
+    OSMMomLike osm_mom = OSMMomLike(0x76416A4d5190d071bfed309861527431304aA14f);
+    address pause_proxy = 0xBE8E3e3618f7474F8cB1d074A26afFef007E98FB;
+
     DssIncreaseDelay24Spell spell;
 
     function vote() private {
@@ -61,15 +80,88 @@ contract DssIncreaseDelay24SpellTest is DSTest {
     }
 
     function testIncreaseDelay24SpellIsCast() public {
-        spell = DssIncreaseDelay24Spell(0x1A7D50b73ACf1D2b4073Ca5F94890A3C05C01401);
         // spell = new DssIncreaseDelay24Spell();
+        spell = DssIncreaseDelay24Spell(0xDD4Aa99077C5e976AFc22060EEafBBd1ba34eae9);
 
         assertEq(pause.delay(), 0);
 
         vote();
         spell.cast();
 
+        // test ETH_OSM rely on OSM_MOM
+        assertEq(eth_osm.wards(address(osm_mom)), 1);
+
+        // test BAT_OSM rely on OSM_MOM
+        assertEq(bat_osm.wards(address(osm_mom)), 1);
+
+        // test OSM_MOM authority is chief
+        assertEq(osm_mom.authority(), address(chief));
+
+        // test OSM_MOM owner is pause_proxy from deploy
+        assertEq(osm_mom.owner(), address(pause_proxy));
+
+        // test OSM_MOM has OSM for ETH-A
+        assertEq(osm_mom.osms('ETH-A'), address(eth_osm));
+
+        // test OSM_MOM has OSM for BAT-A
+        assertEq(osm_mom.osms('BAT-A'), address(bat_osm));
+
+        // test that the new pause delay is 24 hours
         assertEq(pause.delay(), 60 * 60 * 24);
+
+        // just make sure the hat can call osm_mom.stop()
+        address[] memory vote = new address[](1);
+        vote[0] = address(this);
+
+        chief.vote(vote);
+        chief.lift(address(this));
+        assertEq(chief.hat(), address(this));
+        assertEq(osm_mom.authority(), address(chief));
+
+        osm_mom.stop('ETH-A');
+        osm_mom.stop('BAT-A');
     }
 
+    // non-authorized call to osm_mom.stop() should fail
+    function testFailCanCall() public {
+        // spell = new DssIncreaseDelay24Spell();
+        spell = DssIncreaseDelay24Spell(0xDD4Aa99077C5e976AFc22060EEafBBd1ba34eae9);
+
+        assertEq(pause.delay(), 0);
+
+        vote();
+        spell.cast();
+
+        // test ETH_OSM rely on OSM_MOM
+        assertEq(eth_osm.wards(address(osm_mom)), 1);
+
+        // test BAT_OSM rely on OSM_MOM
+        assertEq(bat_osm.wards(address(osm_mom)), 1);
+
+        // test OSM_MOM authority is chief
+        assertEq(osm_mom.authority(), address(chief));
+
+        // test OSM_MOM owner is pause_proxy from deploy
+        assertEq(osm_mom.owner(), address(pause_proxy));
+
+        // test OSM_MOM has OSM for ETH-A
+        assertEq(osm_mom.osms('ETH-A'), address(eth_osm));
+
+        // test OSM_MOM has OSM for BAT-A
+        assertEq(osm_mom.osms('BAT-A'), address(bat_osm));
+
+        // test that the new pause delay is 24 hours
+        assertEq(pause.delay(), 60 * 60 * 24);
+
+        // just make sure the hat can call osm_mom.stop()
+        address[] memory vote = new address[](1);
+        vote[0] = address(0x1);
+
+        chief.vote(vote);
+        chief.lift(address(0x1));
+        assertEq(chief.hat(), address(0x1));
+        assertEq(osm_mom.authority(), address(chief));
+
+        osm_mom.stop('ETH-A');
+    }
 }
