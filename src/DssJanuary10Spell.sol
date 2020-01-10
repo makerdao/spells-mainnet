4166c12ae1b247b022c7d127f356f16e7b33a98f
Mariano Conti <nanexcool@gmail.com>
2020-01-10 12:59:19 -0300

    Add January 10 spell

---
+++ src/DssJanuary10Spell.sol
@@ -0,0 +1,80 @@
+pragma solidity 0.5.12;
+
+contract FileLike {
+    function file(bytes32, uint256) external;
+    function file(bytes32, bytes32, uint256) external;
+}
+
+contract JugLike {
+    function drip(bytes32) external;
+    function file(bytes32, bytes32, uint256) external;
+}
+
+contract PotLike {
+    function drip() external;
+    function file(bytes32, uint256) external;
+}
+
+contract PauseLike {
+    function delay() external view returns (uint256);
+    function plot(address, bytes32, bytes calldata, uint256) external;
+    function exec(address, bytes32, bytes calldata, uint256) external;
+}
+
+contract MomLike {
+    function setCap(uint256) external;
+    function setFee(uint256) external;
+}
+
+contract DssUpdateParametersSpellAction {
+    uint256 constant RAD = 10 ** 45;
+    address constant public VAT = 0x35D1b3F3D7966A1DFe207aa4514C12a259A0492B;
+    address constant public JUG = 0x19c0976f590D67707E62397C87829d896Dc0f1F1;
+    address constant public POT = 0x197E90f9FAD81970bA7976f33CbD77088E5D7cf7;
+
+    function execute() external {
+        // drip
+        PotLike(POT).drip();
+        JugLike(JUG).drip("ETH-A");
+        JugLike(JUG).drip("BAT-A");
+
+        // 4%
+        uint256 duty = 1000000001243680656318820312;
+
+        // DSR to 4%
+        PotLike(POT).file("dsr", duty);
+
+
+        // set ETH-A duty to 4%
+        JugLike(JUG).file("ETH-A", "duty", duty);
+
+        // set BAT-A duty to 4%
+        JugLike(JUG).file("BAT-A", "duty", duty);
+    }
+}
+
+contract DssJanuary10Spell {
+    PauseLike public pause =
+        PauseLike(0xbE286431454714F511008713973d3B053A2d38f3);
+    address   public action;
+    bytes32   public tag;
+    uint256   public eta;
+    bytes     public sig;
+    bool      public done;
+
+    constructor() public {
+        sig = abi.encodeWithSignature("execute()");
+        action = address(new DssUpdateParametersSpellAction());
+        bytes32 _tag;
+        address _action = action;
+        assembly { _tag := extcodehash(_action) }
+        tag = _tag;
+    }
+
+    function cast() external {
+        require(!done, "spell-already-cast");
+        done = true;
+        pause.plot(action, tag, sig, now);
+        pause.exec(action, tag, sig, now);
+    }
+}
---
+++ src/DssJanuary10Spell.t.sol
@@ -0,0 +1,149 @@
+pragma solidity ^0.5.12;
+
+import "ds-test/test.sol";
+
+import {Dai} from "dss/dai.sol";
+import {Vat} from "dss/vat.sol";
+import {Vow} from "dss/vow.sol";
+import {Pot} from "dss/pot.sol";
+import {Jug} from "dss/jug.sol";
+import {ERC20} from "erc20/erc20.sol";
+
+import {DssJanuary10Spell} from "./DssJanuary10Spell.sol";
+
+contract ChiefLike {
+    function hat() public view returns (address);
+    function lock(uint) public;
+    function vote(address[] memory) public;
+    function lift(address) public;
+}
+
+contract RegistryLike {
+    function build() public returns (ProxyLike);
+}
+
+contract ProxyLike {
+    function execute(address, bytes memory) public payable;
+}
+
+contract TokenLike {
+    function balanceOf(address) public view returns (uint);
+    function approve(address, uint) public;
+}
+
+contract ManagerLike {
+    function urns(uint) public view returns (address);
+}
+
+contract TubLike {
+    function ink(bytes32) public view returns (uint);
+    function per() public view returns (uint);
+    function rap(bytes32) public returns (uint);
+    function tab(bytes32) public returns (uint);
+    function pep() external returns (address);
+    function cap() external view returns (uint);
+    function fee() external view returns (uint);
+}
+
+contract ValueLike {
+    function peek() public view returns (uint, bool);
+}
+
+contract OtcLike {
+    function getPayAmount(address, address, uint) public view returns (uint);
+}
+
+contract FlopLike {
+    function wards(address) external returns(uint256);
+    function ttl() external returns(uint256);
+    function beg() external returns(uint256);
+    function pad() external returns(uint256);
+    function tau() external returns(uint256);
+
+    function file(bytes32,uint) external;
+
+    function rely(address) external;
+    function deny(address) external;
+    function dent(uint,uint,uint) external;
+    function deal(uint) external;
+    function yank(uint) external;
+    function cage() external;
+    function live() external returns(uint256);
+}
+
+contract WardsLike {
+    function wards(address) external returns(uint256);
+    function rely(address) public;
+    function deny(address) public;
+}
+
+contract Hevm {
+    function warp(uint) public;
+}
+
+contract DssJanuary10SpellTest is DSTest {
+    Hevm hevm;
+
+    Dai dai = Dai(0x6B175474E89094C44Da98b954EedeAC495271d0F);
+    Vat vat = Vat(0x35D1b3F3D7966A1DFe207aa4514C12a259A0492B);
+    Vow vow = Vow(0xA950524441892A31ebddF91d3cEEFa04Bf454466);
+    Pot pot = Pot(0x197E90f9FAD81970bA7976f33CbD77088E5D7cf7);
+    Jug jug = Jug(0x19c0976f590D67707E62397C87829d896Dc0f1F1);
+    ERC20 gov = ERC20(0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2);
+    ChiefLike chief = ChiefLike(0x9eF05f7F6deB616fd37aC3c959a2dDD25A54E4F5);
+    ProxyLike proxy;
+    address proxyActions = 0x82ecD135Dce65Fbc6DbdD0e4237E0AF93FFD5038;
+    address migrationPActions = 0xe4B22D484958E582098A98229A24e8A43801b674;
+    address migration = 0xc73e0383F3Aff3215E6f04B0331D58CeCf0Ab849;
+    address manager = 0x5ef30b9986345249bc32d8928B7ee64DE9435E39;
+    address ethJoin = 0x2F0b23f53734252Bda2277357e97e1517d6B042A;
+    address batJoin = 0x3D0B1912B66114d4096F48A8CEe3A56C231772cA;
+    address daiJoin = 0x9759A6Ac90977b93B58547b4A71c78317f391A28;
+
+    TubLike tub = TubLike(0x448a5065aeBB8E423F0896E6c5D525C040f59af3);
+
+    DssJanuary10Spell spell;
+
+    uint constant RAD = 10 ** 45;
+
+    function vote() private {
+        gov.approve(address(chief), uint256(-1));
+        chief.lock(gov.balanceOf(address(this)));
+
+        assertTrue(!spell.done());
+
+        address[] memory vote = new address[](1);
+        vote[0] = address(spell);
+
+        chief.vote(vote);
+        chief.lift(address(spell));
+        assertEq(chief.hat(), address(spell));
+    }
+
+    function testDssJanuary10SpellIsCast() public {
+        spell = DssJanuary10Spell(0x7A87aCB1f92c50297239EF9B0Ef9387105Bd4Fc5);
+        // spell = new DssJanuary10Spell();
+
+        (uint dutyETH,) = jug.ilks("ETH-A");
+        (uint dutyBAT,) = jug.ilks("BAT-A");
+
+        // 6%
+        assertEq(pot.dsr(), 1000000001847694957439350562);
+        assertEq(dutyETH, 1000000001847694957439350562);
+        assertEq(dutyBAT, 1000000001847694957439350562);
+
+        vote();
+        spell.cast();
+
+        (dutyETH,) = jug.ilks("ETH-A");
+        (dutyBAT,) = jug.ilks("BAT-A");
+
+        assertTrue(spell.done());
+
+        // 4%
+        assertEq(pot.dsr(), 1000000001243680656318820312);
+        assertEq(dutyETH, 1000000001243680656318820312);
+        assertEq(dutyBAT, 1000000001243680656318820312);
+    }
+
+}
