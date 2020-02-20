da8233171340bea9d67184226c9030c1b88f2bf6
Brian McMichael <brian@brianmcmichael.com>
2020-02-20 15:40:00 -0500

    Add feb 21 launch script

---
+++ test-february-21.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+
+[[ "$ETH_RPC_URL" && "$(seth chain)" == "ethlive" ]] || { echo "Please set a mainnet ETH_RPC_URL"; exit 1; }
+
+dapp --use solc:0.5.12 build
+export DAPP_TEST_ADDRESS=0x8EE7D9235e01e6B42345120b5d270bdB763624C7
+export DAPP_TEST_TIMESTAMP=$(seth block latest timestamp)
+export DAPP_TEST_NUMBER=$(seth block latest number)
+LANG=C.UTF-8 hevm dapp-test --rpc="$ETH_RPC_URL" --json-file=out/dapp.sol.json --dapp-root=. --match "testDssFebruary21SpellIsCast" --verbose 1
