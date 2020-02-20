77d1220325e176aa97c3f7e2456c24b43540af4a
Brian McMichael <brian@brianmcmichael.com>
2020-02-20 16:45:37 -0500

    Switch to chief

--- test-february-21.sh
+++ test-february-21.sh
@@ -4,7 +4,10 @@ set -e
 [[ "$ETH_RPC_URL" && "$(seth chain)" == "ethlive" ]] || { echo "Please set a mainnet ETH_RPC_URL"; exit 1; }
 
 dapp --use solc:0.5.12 build
-export DAPP_TEST_ADDRESS=0x8EE7D9235e01e6B42345120b5d270bdB763624C7
+# Multisig
+#export DAPP_TEST_ADDRESS=0x8EE7D9235e01e6B42345120b5d270bdB763624C7
+# Chief
+export DAPP_TEST_ADDRESS=0x9eF05f7F6deB616fd37aC3c959a2dDD25A54E4F5
 export DAPP_TEST_TIMESTAMP=$(seth block latest timestamp)
 export DAPP_TEST_NUMBER=$(seth block latest number)
 LANG=C.UTF-8 hevm dapp-test --rpc="$ETH_RPC_URL" --json-file=out/dapp.sol.json --dapp-root=. --match "testDssFebruary21SpellIsCast" --verbose 1
