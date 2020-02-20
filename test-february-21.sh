831916e97614029f20b7ec594958aa95519edb32
Brian McMichael <brian@brianmcmichael.com>
2020-02-20 17:56:28 -0500

    Use flop address

--- test-february-21.sh
+++ test-february-21.sh
@@ -7,7 +7,9 @@ dapp --use solc:0.5.12 build
 # Multisig
 #export DAPP_TEST_ADDRESS=0x8EE7D9235e01e6B42345120b5d270bdB763624C7
 # Chief
-export DAPP_TEST_ADDRESS=0x9eF05f7F6deB616fd37aC3c959a2dDD25A54E4F5
+#export DAPP_TEST_ADDRESS=0x9eF05f7F6deB616fd37aC3c959a2dDD25A54E4F5
+# Flop
+export DAPP_TEST_ADDRESS=0x4D95A049d5B0b7d32058cd3F2163015747522e99
 export DAPP_TEST_TIMESTAMP=$(seth block latest timestamp)
 export DAPP_TEST_NUMBER=$(seth block latest number)
 LANG=C.UTF-8 hevm dapp-test --rpc="$ETH_RPC_URL" --json-file=out/dapp.sol.json --dapp-root=. --match "testDssFebruary21SpellIsCast" --verbose 1
