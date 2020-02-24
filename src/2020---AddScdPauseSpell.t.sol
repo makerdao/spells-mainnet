ddb5095590a9d5a64420c47426a1a463784c2d13
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2020-02-24 12:09:56 -0500

    add passing pause test spell

--- src/2020---AddScdPauseSpell.t.sol
+++ src/2020---AddScdPauseSpell.t.sol
@@ -10,8 +10,45 @@ contract Hevm {
     function warp(uint) public;
 }
 
-contract TestDelaySpell {
+contract TestDelaySpellAction {
+    address constant public SAIMOM = 0xF2C5369cFFb8Ea6284452b0326e326DbFdCb867C;
+    address constant public VAT = 0x35D1b3F3D7966A1DFe207aa4514C12a259A0492B;
 
+    function execute() external {
+        VatAbstract(VAT).file("Line", 0);
+        SaiMomAbstract(SAIMOM).setCap(0);
+    }
+}
+
+contract TestDelaySpell is DSMath {
+    DSPauseAbstract  public pause =
+        DSPauseAbstract(0xbE286431454714F511008713973d3B053A2d38f3);
+    address          public action;
+    bytes32          public tag;
+    uint256          public eta;
+    bytes            public sig;
+    bool             public done;
+
+    constructor() public {
+        sig = abi.encodeWithSignature("execute()");
+        action = address(new TestDelaySpellAction());
+        bytes32 _tag;
+        address _action = action;
+        assembly { _tag := extcodehash(_action) }
+        tag = _tag;
+    }
+
+    function schedule() public {
+        require(eta == 0, "spell-already-scheduled");
+        eta = add(now, DSPauseAbstract(pause).delay());
+        pause.plot(action, tag, sig, eta);
+    }
+
+    function cast() public {
+        require(!done, "spell-already-cast");
+        done = true;
+        pause.exec(action, tag, sig, eta);
+    }
 }
 
 contract DssSpellTest is DSTest, DSMath {
@@ -23,7 +60,9 @@ contract DssSpellTest is DSTest, DSMath {
     DSChiefAbstract chief = DSChiefAbstract(0x9eF05f7F6deB616fd37aC3c959a2dDD25A54E4F5);
     address pause_proxy = 0xBE8E3e3618f7474F8cB1d074A26afFef007E98FB;
     address constant public SAIMOM = 0xF2C5369cFFb8Ea6284452b0326e326DbFdCb867C;
+    address constant public SAITUB = 0x448a5065aeBB8E423F0896E6c5D525C040f59af3;
     address constant public SAITOP = 0x9b0ccf7C8994E19F39b2B4CF708e0A7DF65fA8a3;
+    address constant public VAT = 0x35D1b3F3D7966A1DFe207aa4514C12a259A0492B;
 
     DssSpell spell;
 
@@ -37,26 +76,24 @@ contract DssSpellTest is DSTest, DSMath {
         gov.mint(address(this), 1000001 ether);
     }
 
-    function vote() private {
+    function vote(address spell) private {
         if (chief.hat() != address(spell)) {
             gov.approve(address(chief), uint256(-1));
             chief.lock(sub(gov.balanceOf(address(this)), 1 ether));
-
-            assertTrue(!spell.done());
 
             address[] memory yays = new address[](1);
             yays[0] = address(spell);
 
             chief.vote(yays);
-            chief.lift(address(spell));
+            chief.lift(spell);
         }
-        assertEq(chief.hat(), address(spell));
+        assertEq(chief.hat(), spell);
     }
 
-    function scheduleWaitAndCast() private {
-        spell.schedule();
+    function scheduleWaitAndCast(address spell) private {
+        DssSpell(spell).schedule();
         hevm.warp(add(now, pause.delay()));
-        spell.cast();
+        DssSpell(spell).cast();
     }
 
     function testAddPauseToScd() public {
@@ -68,8 +105,8 @@ contract DssSpellTest is DSTest, DSMath {
         assertTrue(SaiTopAbstract(SAITOP).owner() == address(0x0));
         assertTrue(SaiTopAbstract(SAITOP).authority() == address(chief));
 
-        vote();
-        scheduleWaitAndCast();
+        vote(address(spell));
+        scheduleWaitAndCast(address(spell));
 
         assertTrue(SaiMomAbstract(SAIMOM).owner() == address(pause_proxy));
         assertTrue(SaiMomAbstract(SAIMOM).authority() == address(0x0));
@@ -87,28 +124,22 @@ contract DssSpellTest is DSTest, DSMath {
         );
     }
 
-    // non-authorized call to osm_mom.stop() should fail
-    // function testFailCanCall() public {
-    //     spell = new DssSpell();
-    //     // spell = DssSpell(0xDD4Aa99077C5e976AFc22060EEafBBd1ba34eae9);
+    // after passing, new spells can affect SCD after delay
+    function testScdControlledByPause() public {
+        spell = new DssSpell();
+        // spell = DssSpell(0xDD4Aa99077C5e976AFc22060EEafBBd1ba34eae9);
+        vote(address(spell));
+        scheduleWaitAndCast(address(spell));
 
-    //     assertEq(pause.delay(), 0);
+        TestDelaySpell testSpell = new TestDelaySpell();
 
-    //     vote();
-    //     spell.cast();
+        assertTrue(VatAbstract(VAT).Line() != 0);
+        assertTrue(SaiTubAbstract(SAITUB).cap() != 0);
 
-    //     // test that the new pause delay is 24 hours
-    //     assertEq(pause.delay(), 60 * 60 * 24);
+        vote(address(testSpell));
+        scheduleWaitAndCast(address(testSpell));
 
-    //     // just make sure the hat can call osm_mom.stop()
-    //     // address[] memory vote = new address[](1);
-    //     // vote[0] = address(0x1);
-
-    //     // chief.vote(vote);
-    //     // chief.lift(address(0x1));
-    //     // assertEq(chief.hat(), address(0x1));
-    //     // assertEq(osm_mom.authority(), address(chief));
-
-    //     // osm_mom.stop('ETH-A');
-    // }
+        assertTrue(VatAbstract(VAT).Line() == 0);
+        assertTrue(SaiTubAbstract(SAITUB).cap() == 0);
+    }
 }
