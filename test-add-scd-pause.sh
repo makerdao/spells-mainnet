fa35185b17beb86fa045a88117a2f67a20bdcd0b
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2020-02-24 10:06:19 -0500

    test from the flopper address to allow taking over the hat

--- test-add-scd-pause.sh
+++ test-add-scd-pause.sh
@@ -4,8 +4,8 @@ set -e
 [[ "$ETH_RPC_URL" && "$(seth chain)" == "ethlive" ]] || { echo "Please set a mainnet ETH_RPC_URL"; exit 1; }
 
 dapp --use solc:0.5.12 build
-# using the chief not multisig
-export DAPP_TEST_ADDRESS=0x9eF05f7F6deB616fd37aC3c959a2dDD25A54E4F5
+# using the flop as test-actor
+export DAPP_TEST_ADDRESS=0x8EE7D9235e01e6B42345120b5d270bdB763624C7
 export DAPP_TEST_TIMESTAMP=$(seth block latest timestamp)
 export DAPP_TEST_NUMBER=$(seth block latest number)
 LANG=C.UTF-8 hevm dapp-test --rpc="$ETH_RPC_URL" --json-file=out/dapp.sol.json --dapp-root=. --match "testFeb21IncreaseDelay" --verbose 1
