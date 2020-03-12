580078c1c0fdf068f60e08523288025fa892454c
Brian McMichael <brian@brianmcmichael.com>
2020-03-12 15:14:30 -0400

    Add 3/12 offband exec spell and tests

---
+++ src/2020-03-12-Executive.sol
@@ -0,0 +1,170 @@
+pragma solidity ^0.5.12;
+
+import "lib/dss-interfaces/src/dapp/DSPauseAbstract.sol";
+import "lib/dss-interfaces/src/dss/PotAbstract.sol";
+import "lib/dss-interfaces/src/dss/JugAbstract.sol";
+//import "lib/dss-interfaces/src/dss/OsmAbstract.sol";
+//import "lib/dss-interfaces/src/dss/OsmMomAbstract.sol";
+import "lib/dss-interfaces/src/dss/VatAbstract.sol";
+import "lib/dss-interfaces/src/dss/VowAbstract.sol";
+//import "lib/dss-interfaces/src/dss/FlapAbstract.sol";
+//import "lib/dss-interfaces/src/sai/SaiMomAbstract.sol";
+
+
+contract SpellAction {
+    // Provides a descriptive tag for bot consumption
+    // This should be modified weekly to provide a summary of the actions
+    string  constant public description = "MakerDAO Weekly Executive Spell";
+
+    // The contracts in this list should correspond to MCD core contracts, verify
+    //  against the current release list at:
+    //     https://changelog.makerdao.com/releases/mainnet/1.0.3/contracts.json
+    //
+    // Contract addresses pertaining to the SCD ecosystem can be found at:
+    //     https://github.com/makerdao/sai#dai-v1-current-deployments
+    address constant public MCD_PAUSE = 0xbE286431454714F511008713973d3B053A2d38f3;
+    address constant public MCD_JUG = 0x19c0976f590D67707E62397C87829d896Dc0f1F1;
+    address constant public MCD_POT = 0x197E90f9FAD81970bA7976f33CbD77088E5D7cf7;
+    address constant public MCD_VOW = 0xA950524441892A31ebddF91d3cEEFa04Bf454466;
+    address constant public MCD_VAT = 0x35D1b3F3D7966A1DFe207aa4514C12a259A0492B;
+    //address constant public MCD_ADM = 0x9eF05f7F6deB616fd37aC3c959a2dDD25A54E4F5;
+    //address constant public OSM_MOM = 0x76416A4d5190d071bfed309861527431304aA14f;
+    //address constant public PIP_ETH = 0x81FE72B5A8d1A857d176C3E7d5Bd2679A9B85763;
+    //address constant public PIP_BAT = 0xB4eb54AF9Cc7882DF0121d26c5b97E802915ABe6;
+    //address constant public MCD_FLAP = 0xdfE0fb1bE2a52CDBf8FB962D5701d7fd0902db9f;
+
+
+    // Many of the settings that change weekly rely on the rate accumulator
+    // described at https://docs.makerdao.com/smart-contract-modules/rates-module
+    // To check this yourself, use the following rate calculation (example 8%):
+    //
+    // $ bc -l <<< 'scale=27; e( l(1.08)/(60 * 60 * 24 * 365) )'
+    //
+    uint256 constant public ZERO_PCT_RATE = 1000000000000000000000000000;
+    uint256 constant public FOUR_PCT_RATE = 1000000001243680656318820312;
+    //uint256 constant public EIGHT_PCT_RATE = 1000000002440418608258400030;
+    //uint256 constant public NINE_PCT_RATE  = 1000000002732676825177582095;
+    //uint256 constant public TEN_PCT_RATE   = 1000000003022265980097387650;
+
+    uint256 constant public RAD = 10**45;
+    uint256 constant public MILLION = 10**6;
+    uint256 constant public DAY = 86400; // in seconds
+
+    function execute() external {
+
+        // Drip Pot and Jugs prior to all modifications.
+        PotAbstract(MCD_POT).drip();
+        JugAbstract(MCD_JUG).drip("ETH-A");
+        JugAbstract(MCD_JUG).drip("BAT-A");
+
+
+        // MCD Modifications
+
+
+        // Set the Dai Savings Rate
+        // DSR_RATE is a value determined by the rate accumulator calculation (see above)
+        //  ex. an 8% annual rate will be 1000000002440418608258400030
+        //
+        // Existing Rate: 8%
+        // New Rate: 0%
+        uint256 DSR_RATE = ZERO_PCT_RATE;
+        PotAbstract(MCD_POT).file("dsr", DSR_RATE);
+
+
+        // Set the ETH-A debt ceiling
+        // ETH_LINE is the number of Dai that can be created with WETH token collateral
+        //  ex. a 100 million Dai ETH ceiling will be ETH_LINE = 100000000
+        //
+        // Existing Line: 150m
+        // New Line: 110m
+        uint256 ETH_LINE = 110 * MILLION;
+        VatAbstract(MCD_VAT).file("ETH-A", "line", ETH_LINE * RAD);
+
+
+        // Set the ETH-A stability fee
+        // ETH_FEE is a value determined by the rate accumulator calculation (see above)
+        //  ex. an 8% annual rate will be 1000000002440418608258400030
+        //
+        // Existing Rate: 8%
+        // New Rate: 4%
+        uint256 ETH_FEE = FOUR_PCT_RATE;
+        JugAbstract(MCD_JUG).file("ETH-A", "duty", ETH_FEE);
+
+
+        // Set the BAT-A stability fee
+        // BAT_FEE is a value determined by the rate accumulator calculation (see above)
+        //  ex. an 8% annual rate will be 1000000002440418608258400030
+        //
+        // Existing Rate: 8%
+        // New Rate: 4%
+        uint256 BAT_FEE = FOUR_PCT_RATE;
+        JugAbstract(MCD_JUG).file("BAT-A", "duty", BAT_FEE);
+
+
+        // Set the global debt ceiling
+        //
+        // GLOBAL_AMOUNT is the total number of Dai that can be created by all collateral types
+        //  as a whole number
+        //  ex. a 100 million Dai global ceiling will be GLOBAL_AMOUNT = 100000000
+        //
+        // Existing Ceiling: 183m
+        // New Ceiling: 143m
+        uint256 GLOBAL_AMOUNT = 143 * MILLION;
+        VatAbstract(MCD_VAT).file("Line", GLOBAL_AMOUNT * RAD);
+
+
+        // Increase the wait delay for flop auctions
+        //
+        // WAIT_DELAY is the number of seconds that pass before debt is auctioned for MKR tokens
+        //
+        // Existing wait: 2 days
+        // New wait: 14 days
+        uint256 WAIT_DELAY = 14 * DAY;
+        VowAbstract(MCD_VOW).file("wait", WAIT_DELAY);
+    }
+}
+
+contract DssSpell {
+
+    DSPauseAbstract  public pause =
+        DSPauseAbstract(0xbE286431454714F511008713973d3B053A2d38f3);
+    address constant public SAI_MOM = 0xF2C5369cFFb8Ea6284452b0326e326DbFdCb867C;
+    address          public action;
+    bytes32          public tag;
+    uint256          public eta;
+    bytes            public sig;
+    uint256          public expiration;
+    bool             public done;
+
+    uint256 constant internal WAD = 10**18;
+
+    constructor() public {
+        sig = abi.encodeWithSignature("execute()");
+        action = address(new SpellAction());
+        bytes32 _tag;
+        address _action = action;
+        assembly { _tag := extcodehash(_action) }
+        tag = _tag;
+        expiration = now + 30 days;
+    }
+
+    function description() public view returns (string memory) {
+        return SpellAction(action).description();
+    }
+
+    function schedule() public {
+        require(now <= expiration, "This contract has expired");
+        require(eta == 0, "This spell has already been scheduled");
+        eta = now + DSPauseAbstract(pause).delay();
+        pause.plot(action, tag, sig, eta);
+
+        // NOTE: 'eta' check should mimic the old behavior of 'done', thus
+        // preventing these SCD changes from being executed again.
+    }
+
+    function cast() public {
+        require(!done, "spell-already-cast");
+        done = true;
+        pause.exec(action, tag, sig, eta);
+    }
+}
---
+++ src/2020-03-12-Executive.t.sol
@@ -0,0 +1,263 @@
+pragma solidity 0.5.12;
+
+import "ds-math/math.sol";
+import "ds-test/test.sol";
+import "lib/dss-interfaces/src/Interfaces.sol";
+
+import {DssSpell, SpellAction} from "./2020-03-12-Executive.sol";
+
+contract Hevm { function warp(uint) public; }
+
+contract DssSpellTest is DSTest, DSMath {
+
+    // Replace with mainnet spell address to test against live
+    address constant MAINNET_SPELL = address(0);
+
+    struct SystemValues {
+        uint256 dsr;
+        uint256 dsrPct;
+        uint256 lineETH;
+        uint256 dutyETH;
+        uint256 pctETH;
+        uint256 lineBAT;
+        uint256 dutyBAT;
+        uint256 pctBAT;
+        uint256 lineSAI;
+        uint256 lineGlobal;
+        uint256 saiCap;
+        uint256 saiFee;
+        uint256 saiPct;
+    }
+
+    // If last week's spell was cast successfully, you can copy the
+    //  the values from that week's `thisWeek` var into this week's
+    //  `lastWeek` var. Or go back to the last successful executive.
+    SystemValues lastWeek = SystemValues({
+        dsr: 1000000002440418608258400030,
+        dsrPct: 8 * 1000,
+        lineETH: mul(150000000, RAD),
+        dutyETH: 1000000002440418608258400030,
+        pctETH: 8 * 1000,
+        lineBAT: mul(3000000, RAD),
+        dutyBAT: 1000000002440418608258400030,
+        pctBAT: 8 * 1000,
+        lineSAI: mul(30000000, RAD),
+        lineGlobal: mul(183000000, RAD),
+        saiCap: mul(30000000, WAD),
+        saiFee: 1000000002877801985002875644,
+        saiPct: 9.5 * 1000
+    });
+
+    SystemValues thisWeek = SystemValues({
+        dsr: 1000000000000000000000000000,
+        dsrPct: 0 * 1000,
+        lineETH: mul(110000000, RAD),
+        dutyETH: 1000000001243680656318820312,
+        pctETH: 4 * 1000,
+        lineBAT: mul(3000000, RAD),
+        dutyBAT: 1000000001243680656318820312,
+        pctBAT: 4 * 1000,
+        lineSAI: mul(30000000, RAD),
+        lineGlobal: mul(143000000, RAD),
+        saiCap: mul(30000000, WAD),
+        saiFee: 1000000002877801985002875644,
+        saiPct: 9.5 * 1000
+    });
+
+    Hevm hevm;
+
+    DSPauseAbstract pause =
+        DSPauseAbstract(0xbE286431454714F511008713973d3B053A2d38f3);
+    DSChiefAbstract chief =
+        DSChiefAbstract(0x9eF05f7F6deB616fd37aC3c959a2dDD25A54E4F5);
+    VatAbstract     vat = VatAbstract(0x35D1b3F3D7966A1DFe207aa4514C12a259A0492B);
+    VowAbstract     vow = VowAbstract(0xA950524441892A31ebddF91d3cEEFa04Bf454466);
+    PotAbstract     pot = PotAbstract(0x197E90f9FAD81970bA7976f33CbD77088E5D7cf7);
+    JugAbstract     jug = JugAbstract(0x19c0976f590D67707E62397C87829d896Dc0f1F1);
+    MKRAbstract     gov = MKRAbstract(0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2);
+    SaiTubAbstract  tub = SaiTubAbstract(0x448a5065aeBB8E423F0896E6c5D525C040f59af3);
+
+    DssSpell spell;
+
+    // CHEAT_CODE = 0x7109709ECfa91a80626fF3989D68f67F5b1DD12D
+    bytes20 constant CHEAT_CODE =
+        bytes20(uint160(uint256(keccak256('hevm cheat code'))));
+
+    // not provided in DSMath
+    uint constant RAD = 10 ** 45;
+    function rpow(uint x, uint n, uint b) internal pure returns (uint z) {
+      assembly {
+        switch x case 0 {switch n case 0 {z := b} default {z := 0}}
+        default {
+          switch mod(n, 2) case 0 { z := b } default { z := x }
+          let half := div(b, 2)  // for rounding.
+          for { n := div(n, 2) } n { n := div(n,2) } {
+            let xx := mul(x, x)
+            if iszero(eq(div(xx, x), x)) { revert(0,0) }
+            let xxRound := add(xx, half)
+            if lt(xxRound, xx) { revert(0,0) }
+            x := div(xxRound, b)
+            if mod(n,2) {
+              let zx := mul(z, x)
+              if and(iszero(iszero(x)), iszero(eq(div(zx, x), z))) { revert(0,0) }
+              let zxRound := add(zx, half)
+              if lt(zxRound, zx) { revert(0,0) }
+              z := div(zxRound, b)
+            }
+          }
+        }
+      }
+    }
+    // 10^-5 (tenth of a basis point) as a RAY
+    uint256 TOLERANCE = 10 ** 22;
+
+    function setUp() public {
+        hevm = Hevm(address(CHEAT_CODE));
+        // Using the Flopper test address, mint enough MKR to overcome the current hat.
+        gov.mint(address(this), 300000 ether);
+    }
+
+    function vote() private {
+        if (chief.hat() != address(spell)) {
+            gov.approve(address(chief), uint256(-1));
+            chief.lock(sub(gov.balanceOf(address(this)), 1 ether));
+
+            assertTrue(!spell.done());
+
+            address[] memory yays = new address[](1);
+            yays[0] = address(spell);
+
+            chief.vote(yays);
+            chief.lift(address(spell));
+        }
+        assertEq(chief.hat(), address(spell));
+    }
+
+    function scheduleWaitAndCast() public {
+        spell.schedule();
+        hevm.warp(add(now, pause.delay()));
+        spell.cast();
+    }
+
+    function stringToBytes32(string memory source) public pure returns (bytes32 result) {
+        assembly {
+        result := mload(add(source, 32))
+        }
+    }
+
+    function yearlyYield(uint256 duty) public pure returns (uint256) {
+        return rpow(duty, (365 * 24 * 60 *60), RAY);
+    }
+
+    function expectedRate(uint256 percentValue) public pure returns (uint256) {
+        return (100000 + percentValue) * (10 ** 22);
+    }
+
+    function diffCalc(uint256 expectedRate, uint256 yearlyYield) public pure returns (uint256) {
+        return (expectedRate > yearlyYield) ? expectedRate - yearlyYield : yearlyYield - expectedRate;
+    }
+
+    function testSpellIsCast() public {
+
+        spell = MAINNET_SPELL != address(0) ? DssSpell(MAINNET_SPELL) : new DssSpell();
+
+        // Test description
+        string memory description = new SpellAction().description();
+        assertTrue(bytes(description).length > 0);
+        // DS-Test can't handle strings directly, so cast to a bytes32.
+        assertEq(stringToBytes32(spell.description()),
+            stringToBytes32(description));
+
+        // Test expiration
+        // TODO fix this for deployed contract
+        if(address(spell) != address(MAINNET_SPELL)) {
+            assertEq(spell.expiration(), (now + 30 days));
+        }
+
+        // (ETH-A, BAT-A, DSR)
+        (uint dutyETH,) = jug.ilks("ETH-A");
+        (uint dutyBAT,) = jug.ilks("BAT-A");
+        assertEq(dutyETH,   lastWeek.dutyETH);
+        assertTrue(diffCalc(expectedRate(lastWeek.pctETH), yearlyYield(lastWeek.dutyETH)) <= TOLERANCE);
+        assertEq(dutyBAT,   lastWeek.dutyBAT);
+        assertTrue(diffCalc(expectedRate(lastWeek.pctBAT), yearlyYield(lastWeek.dutyBAT)) <= TOLERANCE);
+        assertEq(pot.dsr(), lastWeek.dsr);
+        assertTrue(diffCalc(expectedRate(lastWeek.dsrPct), yearlyYield(lastWeek.dsr)) <= TOLERANCE);
+
+        // ETH-A line
+        (,,, uint256 lineETH,) = vat.ilks("ETH-A");
+        assertEq(lineETH, lastWeek.lineETH);
+
+        // BAT-A line
+        (,,, uint256 lineBAT,) = vat.ilks("BAT-A");
+        assertEq(lineBAT, lastWeek.lineBAT);
+
+        // SAI line
+        (,,, uint256 lineSAI,) = vat.ilks("SAI");
+        assertEq(lineSAI, lastWeek.lineSAI);
+
+        // Line
+        assertEq(vat.Line(), lastWeek.lineGlobal);
+
+        // SCD DC
+        assertEq(tub.cap(), lastWeek.saiCap);
+
+        // SCD Fee
+        assertEq(tub.fee(), lastWeek.saiFee);
+        assertTrue(diffCalc(expectedRate(lastWeek.saiPct), yearlyYield(lastWeek.saiFee)) <= TOLERANCE);
+
+        vote();
+        scheduleWaitAndCast();
+
+        // spell done
+        assertTrue(spell.done());
+
+        // dsr
+        assertEq(pot.dsr(), thisWeek.dsr);
+        assertTrue(diffCalc(expectedRate(thisWeek.dsrPct), yearlyYield(thisWeek.dsr)) <= TOLERANCE);
+
+        // (ETH-A, BAT-A)
+        (dutyETH,) = jug.ilks("ETH-A");
+        (dutyBAT,) = jug.ilks("BAT-A");
+        assertEq(dutyETH, thisWeek.dutyETH);
+        assertTrue(diffCalc(expectedRate(thisWeek.pctETH), yearlyYield(thisWeek.dutyETH)) <= TOLERANCE);
+        assertEq(dutyBAT, thisWeek.dutyBAT);
+        assertTrue(diffCalc(expectedRate(thisWeek.pctETH), yearlyYield(thisWeek.dutyETH)) <= TOLERANCE);
+
+        // ETH-A line
+        (,,, lineETH,) = vat.ilks("ETH-A");
+        assertEq(lineETH, thisWeek.lineETH);
+
+        // BAT-A line
+        (,,, lineBAT,) = vat.ilks("BAT-A");
+        assertEq(lineBAT, thisWeek.lineBAT);
+
+        // SAI line
+        (,,, lineSAI,) = vat.ilks("SAI");
+        assertEq(lineSAI, thisWeek.lineSAI);
+
+        // Line
+        assertEq(vat.Line(), thisWeek.lineGlobal);
+
+        // SCD DC
+        assertEq(tub.cap(), thisWeek.saiCap);
+
+        // SCD Fee
+        assertEq(tub.fee(), thisWeek.saiFee);
+        assertTrue(diffCalc(expectedRate(thisWeek.saiPct), yearlyYield(thisWeek.saiFee)) <= TOLERANCE);
+
+    }
+
+    function testFlopDelay() public {
+        spell = MAINNET_SPELL != address(0) ? DssSpell(MAINNET_SPELL) : new DssSpell();
+
+        // Vow Flop Delay precheck
+        assertEq(vow.wait(), 172800);
+
+        vote();
+        scheduleWaitAndCast();
+
+        // Vow Flop Delay
+        assertEq(vow.wait(), 1209600);
+    }
+}
