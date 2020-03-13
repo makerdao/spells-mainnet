4d8f85055693cdd45e115098f63a8370b8d1f64f
wilbarnes <wbarnesf@gmail.com>
2020-03-13 19:07:28 -0400

    added tests and made some improvements to the spell

--- src/DssReplaceFlipper.sol
+++ src/DssReplaceFlipper.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.5.12;
 
 import "ds-math/math.sol";
+import {Flipper} from "dss/flip.sol";
 
 import "lib/dss-interfaces/src/dapp/DSPauseAbstract.sol";
 import "lib/dss-interfaces/src/dss/FlipAbstract.sol";
@@ -9,36 +10,52 @@ import "lib/dss-interfaces/src/dss/VatAbstract.sol";
 import "lib/dss-interfaces/src/dss/EndAbstract.sol";
 
 contract SpellAction {
-    // mainnet variables
-    address constant public cat = 0x78F2c2AF65126834c51822F56Be0d7469D7A523E;
-    address constant public vow = 0xA950524441892A31ebddF91d3cEEFa04Bf454466;
-    address constant public vat = 0x35D1b3F3D7966A1DFe207aa4514C12a259A0492B;
-    address constant public end = 0xaB14d3CE3F733CACB76eC2AbE7d2fcb00c99F3d5;
-    address constant public oldFlipper = 0xaB14d3CE3F733CACB76eC2AbE7d2fcb00c99F3d5;
-    // address constant public newFlipper = _NEWFLIPPER_;
+    // -------------------------------------------
+    // ------------ MAINNET ADDRESSES ------------
+    // -------------------------------------------
+    // address constant public cat = 0x78F2c2AF65126834c51822F56Be0d7469D7A523E;
+    // address constant public vow = 0xA950524441892A31ebddF91d3cEEFa04Bf454466;
+    // address constant public vat = 0x35D1b3F3D7966A1DFe207aa4514C12a259A0492B;
+    // address constant public end = 0xaB14d3CE3F733CACB76eC2AbE7d2fcb00c99F3d5;
+    // address constant public oldFlipper = 0xaB14d3CE3F733CACB76eC2AbE7d2fcb00c99F3d5;
+
+    // -------------------------------------------
+    // ------------- KOVAN ADDRESSES -------------
+    // -------------------------------------------
+    address constant public cat = 0x0511674A67192FE51e86fE55Ed660eB4f995BDd6;
+    address constant public vow = 0x0F4Cbe6CBA918b7488C26E29d9ECd7368F38EA3b;
+    address constant public vat = 0xbA987bDB501d131f766fEe8180Da5d81b34b69d9;
+    address constant public end = 0x24728AcF2E2C403F5d2db4Df6834B8998e56aA5F;
+    address constant public oldFlipper = 0xB40139Ea36D35d0C9F6a2e62601B616F1FfbBD1b;
 
     function execute() public {
-        // file the new flipper on the cat
-        // this nopes the old flipper, sets ilks[ilk].flip to the new flip
-        // and hopes the new flip
-        CatAbstract(cat).file("ETH-A", "flip", newFlipper);
+        Flipper newFlipper = new Flipper(vat, "ETH-A");
 
-        // rely the new flipper on the cat
-        FlipAbstract(newFlipper).rely(cat);
-        FlipAbstract(newFlipper).rely(end);
+        // file ETH-A ilk type to update cat to new flipper
+        CatAbstract(cat).file("ETH-A", "flip", address(newFlipper));
+
+        // rely the cat and end on the new flipper
+        newFlipper.rely(cat);
+        newFlipper.rely(end);
 
         // rely the new flipper on the end
-        EndAbstract(end).rely(newFlipper);
+        EndAbstract(end).rely(address(newFlipper));
 
-        // deny on the old flopper
+        // deny the end and cat on the old flipper
         FlipAbstract(oldFlipper).deny(end);
         FlipAbstract(oldFlipper).deny(cat);
     }
 }
 
 contract DssReplaceFlipper is DSMath {
+    // MAINNET ADDRESS
+    // DSPauseAbstract public pause = DSPauseAbstract(
+    //     0xbE286431454714F511008713973d3B053A2d38f3
+    // );
+
+    // KOVAN ADDRESS
     DSPauseAbstract public pause = DSPauseAbstract(
-        0xbE286431454714F511008713973d3B053A2d38f3
+        0x8754E6ecb4fe68DaA5132c2886aB39297a5c7189
     );
 
     address public action;
@@ -53,10 +70,11 @@ contract DssReplaceFlipper is DSMath {
 
         sig = abi.encodeWithSignature("execute()");
 
-        bytes _tag;
+        bytes32 _tag;
         address _action = action;
         assembly { _tag := extcodehash(_action) }
         tag = _tag;
+        expiration = now + 30 days;
     }
 
     function schedule() public {
---
+++ src/DssReplaceFlipper.t.sol
@@ -0,0 +1,92 @@
+pragma solidity ^0.5.12;
+
+import "ds-math/math.sol";
+import "ds-test/test.sol";
+import "lib/dss-interfaces/src/Interfaces.sol";
+
+import {DssReplaceFlipper} from "./DssReplaceFlipper.sol";
+
+contract Hevm {
+    function warp(uint256) public;
+}
+
+contract DssSpellTest is DSTest, DSMath {
+    // populate with mainnet spell if needed
+    address constant MAINNET_SPELL = address(0); 
+
+    // -------------------------------------------
+    // ------------ MAINNET ADDRESSES ------------
+    // -------------------------------------------
+    // DSPauseAbstract pause = DSPauseAbstract(
+    //     0xbE286431454714F511008713973d3B053A2d38f3
+    // );
+    // VatAbstract vat = VatAbstract(
+    //     0x35D1b3F3D7966A1DFe207aa4514C12a259A0492B
+    // );
+    // DSChiefAbstract chief = DSChiefAbstract(
+    //     0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2
+    // );
+    // MKRAbstract gov = MkrAbstract(
+    //     0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2
+    // );
+    
+    // -------------------------------------------
+    // ------------- KOVAN ADDRESSES -------------
+    // -------------------------------------------
+    DSPauseAbstract pause = DSPauseAbstract(
+        0x8754E6ecb4fe68DaA5132c2886aB39297a5c7189
+    );
+    VatAbstract vat = VatAbstract(
+        0xbA987bDB501d131f766fEe8180Da5d81b34b69d9
+    );
+    DSChiefAbstract chief = DSChiefAbstract(
+        0xbBFFC76e94B34F72D96D054b31f6424249c1337d
+    );
+    MKRAbstract gov = MKRAbstract(
+        0xAaF64BFCC32d0F15873a02163e7E500671a4ffcD
+    );
+    
+    DssReplaceFlipper spell;
+
+    Hevm hevm;
+
+    // CHEAT_CODE = 0x7109709ECfa91a80626fF3989D68f67F5b1DD12D
+    bytes20 constant CHEAT_CODE =
+        bytes20(uint160(uint256(keccak256('hevm cheat code'))));
+
+    function setUp() public {
+        hevm = Hevm(address(CHEAT_CODE));
+        gov.mint(address(this), 300000 ether);
+    }
+
+    function vote() private {
+        if (chief.hat() != address(spell)) {
+            gov.approve(address(chief), uint256(-1));
+            chief.lock(sub(gov.balanceOf(address(this)), 1 ether));
+
+            assertTrue(!spell.done());
+
+            address[] memory yays = new address[](1);
+            yays[0] = address(spell);
+
+            chief.vote(yays);
+            chief.lift(address(spell));
+        }
+        assertEq(chief.hat(), address(spell));
+    }
+
+    function scheduleWaitAndCast() public {
+        spell.schedule();
+        hevm.warp(add(now, pause.delay()));
+        spell.cast();
+    }
+
+    function testSpellIsCast() public {
+        spell = MAINNET_SPELL != 
+            address(0) ? DssReplaceFlipper(MAINNET_SPELL) : 
+            new DssReplaceFlipper();
+        vote();
+        scheduleWaitAndCast();
+        assertTrue(spell.done());
+    }
+}
