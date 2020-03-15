b6f8a969c1078d111cd800e95a651f241518d42d
wilbarnes <wbarnesf@gmail.com>
2020-03-15 00:08:06 -0400

    removed kovan addresses, add more tests

--- src/DssDeployFlipperMom.t.sol
+++ src/DssDeployFlipperMom.t.sol
@@ -11,40 +11,33 @@ contract Hevm {
 }
 
 contract FlipMomLike {
-    
+    function setOwner(address) external;
+    function setAuthority(address) external;
+    function rely(address, address) external;
+    function deny(address, address) external;
 }
 
 contract DssSpellTest is DSTest, DSMath {
     // populate with mainnet spell if needed
     address constant MAINNET_SPELL = address(0); 
 
+    address constant MCD_PAUSE_PROXY = 0xBE8E3e3618f7474F8cB1d074A26afFef007E98FB;
+    address constant MCD_ADM = 0x9eF05f7F6deB616fd37aC3c959a2dDD25A54E4F5;
+    address constant MCD_CAT = 0x78F2c2AF65126834c51822F56Be0d7469D7A523E;
+    address constant MCD_FLIP_ETH_A = 0xd8a04F5412223F513DC55F839574430f5EC15531;
+    address constant MCD_FLIP_BAT_A = 0xaA745404d55f88C108A28c86abE7b5A1E7817c07;
+
     // address constant FLIPPER_MOM = ;
 
-    // -------------------------------------------
-    // ------------ MAINNET ADDRESSES ------------
-    // -------------------------------------------
-    // DSPauseAbstract pause = DSPauseAbstract(
-    //     0xbE286431454714F511008713973d3B053A2d38f3
-    // );
-    // DSChiefAbstract chief = DSChiefAbstract(
-    //     0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2
-    // );
-    // MKRAbstract gov = MkrAbstract(
-    //     0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2
-    // );
-    
-
-    // -------------------------------------------
-    // ------------- KOVAN ADDRESSES -------------
-    // -------------------------------------------
+    // MAINNET ADDRESSES
     DSPauseAbstract pause = DSPauseAbstract(
-        0x8754E6ecb4fe68DaA5132c2886aB39297a5c7189
+        0xbE286431454714F511008713973d3B053A2d38f3
     );
     DSChiefAbstract chief = DSChiefAbstract(
-        0xbBFFC76e94B34F72D96D054b31f6424249c1337d
+        0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2
     );
-    MKRAbstract gov = MKRAbstract(
-        0xAaF64BFCC32d0F15873a02163e7E500671a4ffcD
+    MKRAbstract gov = MkrAbstract(
+        0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2
     );
     
     DssDeployFlipperMom spell;
@@ -100,5 +93,38 @@ contract DssSpellTest is DSTest, DSMath {
         vote();
         scheduleWaitAndCast();
 
+        address flipChief = FlipMomLike(FLIPPER_MOM).authority();
+        assertEq(flipChief, MCD_ADM);
+
+        address flipMomOwner = FlipMomLike(FLIPPER_MOM).owner();
+        assertEq(flipMomOwner, MCD_PAUSE_PROXY);
+
+        uint256 flipEthAWard = FlipAbstract(MCD_FLIP_ETH_A).wards(FLIPPER_MOM);
+        assertEq(flipEthAWard, 1);
+
+        uint256 flipBatAWard = FlipAbstract(MCD_FLIP_BAT_A).wards(FLIPPER_MOM);
+        assertEq(flipBatAWard, 1);
+    }
+
+    function testFailOnSetOwner() public {
+        spell = MAINNET_SPELL != address(0) ? 
+            DssDeployFlipperMom(MAINNET_SPELL) :
+            new DssDeployFlipperMom();
+
+        vote();
+        scheduleWaitAndCast();
+
+        FlipMomLike(FLIPPER_MOM).setOwner(address(this));
+    }
+
+    function testFailOnSetAuthority() public {
+        spell = MAINNET_SPELL != address(0) ? 
+            DssDeployFlipperMom(MAINNET_SPELL) :
+            new DssDeployFlipperMom();
+
+        vote();
+        scheduleWaitAndCast();
+
+        FlipMomLike(FLIPPER_MOM).setAuthority(address(this));
     }
 }
