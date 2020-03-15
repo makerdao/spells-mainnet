9868ccd74174b702b8420e28896796d5b87ec210
wilbarnes <wbarnesf@gmail.com>
2020-03-15 00:44:13 -0400

    using MCD constant style functions

--- src/DssDeployFlipperMom.sol
+++ src/DssDeployFlipperMom.sol
@@ -7,11 +7,6 @@ import {FlipperMom} from "flipper-mom/FlipperMom.sol";
 import "lib/dss-interfaces/src/dapp/DSPauseAbstract.sol";
 import "lib/dss-interfaces/src/dss/FlipAbstract.sol";
 
-contract MkrAuthLike {
-    function rely(address) public;
-    function deny(address) public;
-}
-
 contract FlipMomLike {
     function setOwner(address) external;
     function setAuthority(address) external;
@@ -20,21 +15,20 @@ contract FlipMomLike {
 }
 
 contract SpellAction {
-    address constant public pauseProxy = 0xBE8E3e3618f7474F8cB1d074A26afFef007E98FB;
-    address constant public flipperEthA = 0xd8a04F5412223F513DC55F839574430f5EC15531;
-    address constant public flipperBatA = 0xaA745404d55f88C108A28c86abE7b5A1E7817c07;
-    address constant public auth = 0x6eEB68B2C7A918f36B78E2DB80dcF279236DDFb8;
-    address constant public chief = 0x9eF05f7F6deB616fd37aC3c959a2dDD25A54E4F5;
+    address constant public DS_PAUSE_PROXY = 0xBE8E3e3618f7474F8cB1d074A26afFef007E98FB;
+    address constant public MCD_FLIP_ETH_A = 0xd8a04F5412223F513DC55F839574430f5EC15531;
+    address constant public MCD_FLIP_BAT_A = 0xaA745404d55f88C108A28c86abE7b5A1E7817c07;
+    address constant public MCD_ADM = 0x9eF05f7F6deB616fd37aC3c959a2dDD25A54E4F5;
     // address constant public flipperMom = ;
 
     function execute() public {
         // set flipper mom auth to MCD_ADM
-        FlipMomLike(flipperMom).setAuthority(chief);
+        FlipMomLike(flipperMom).setAuthority(MCD_ADM);
         // set flipper mom owner to MCD_PAUSE_PROXY
-        FlipMomLike(flipperMom).setOwner(pauseProxy);
+        FlipMomLike(flipperMom).setOwner(DS_PAUSE_PROXY);
         // rely the flipper mom on both ETH-A and BAT-A flippers
-        FlipAbstract(flipperEthA).rely(address(flipperMom));
-        FlipAbstract(flipperBatA).rely(address(flipperMom));
+        FlipAbstract(MCD_FLIP_ETH_A).rely(address(flipperMom));
+        FlipAbstract(MCD_FLIP_BAT_A).rely(address(flipperMom));
     }
 }
 
@@ -53,9 +47,7 @@ contract DssDeployFlipperMom is DSMath {
 
     constructor() public {
         action = address(new SpellAction());
-
         sig = abi.encodeWithSignature("execute()");
-
         bytes32 _tag;
         address _action = action;
         assembly { _tag := extcodehash(_action) }
