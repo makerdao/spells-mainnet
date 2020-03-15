07160644282b9c8fb2b2da1393b3ed50eaaf98fc
wilbarnes <wbarnesf@gmail.com>
2020-03-15 01:12:03 -0400

    solidity pragma 0.5.12 and setting mainnet multisig test address

--- test-flipper-mom-deploy.sh
+++ test-flipper-mom-deploy.sh
@@ -4,15 +4,17 @@ SPELL="DssDeployFlipperMom"
 SPELLFILE="${SPELL}.sol"
 TESTFILE="${SPELL}.t.sol"
 
-dapp build --extract
+dapp --use solc:0.5.12 build --extract
 
 flipper_addr=$(seth --to-checksum-address $(dapp create $SPELL))
 
 # mainnet multisig
-# export DAPP_TEST_ADDRESS=0x8EE7D9235e01e6B42345120b5d270bdB763624C7
+export DAPP_TEST_ADDRESS=0x8EE7D9235e01e6B42345120b5d270bdB763624C7
 
 # kovan deployer
-export DAPP_TEST_ADDRESS=0xdB33dFD3D61308C33C63209845DaD3e6bfb2c674
+# export DAPP_TEST_ADDRESS=0xdB33dFD3D61308C33C63209845DaD3e6bfb2c674
+
 export DAPP_TEST_TIMESTAMP=$(seth block latest timestamp)
 export DAPP_TEST_NUMBER=$(seth block latest number)
+
 LANG=C.UTF-8 hevm dapp-test --rpc="$ETH_RPC_URL" --json-file=out/dapp.sol.json --dapp-root=. --match "test" --verbose 1
