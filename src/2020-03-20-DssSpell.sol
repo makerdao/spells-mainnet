2a2a6a5c78755a151fc3491bccdc2dabb8ffb5e4
Christopher Mooney <chris@dod.net>
2020-03-20 11:52:29 -0400

    20200320 Weekly Executive

---
+++ src/2020-03-20-DssSpell.sol
@@ -0,0 +1,130 @@
+pragma solidity 0.5.12;
+
+import "lib/dss-interfaces/src/dapp/DSPauseAbstract.sol";
+import "lib/dss-interfaces/src/dss/PotAbstract.sol";
+import "lib/dss-interfaces/src/dss/JugAbstract.sol";
+import "lib/dss-interfaces/src/dss/VatAbstract.sol";
+import "lib/dss-interfaces/src/dss/CatAbstract.sol";
+import "lib/dss-interfaces/src/dss/VowAbstract.sol";
+import "lib/dss-interfaces/src/dss/FlipAbstract.sol";
+import "lib/dss-interfaces/src/dss/FlopAbstract.sol";
+import "lib/dss-interfaces/src/sai/SaiMomAbstract.sol";
+
+contract SpellAction {
+    // Provides a descriptive tag for bot consumption
+    // This should be modified weekly to provide a summary of the actions
+    string  constant public description = "03/20/2020 MakerDAO Executive Spell";
+
+    // The contracts in this list should correspond to MCD core contracts, verify
+    //  against the current release list at:
+    //     https://changelog.makerdao.com/releases/mainnet/1.0.4/contracts.json
+    //
+    // Contract addresses pertaining to the SCD ecosystem can be found at:
+    //     https://github.com/makerdao/sai#dai-v1-current-deployments
+    address constant public MCD_JUG = 0x19c0976f590D67707E62397C87829d896Dc0f1F1;
+    address constant public MCD_POT = 0x197E90f9FAD81970bA7976f33CbD77088E5D7cf7;
+
+
+    // Many of the settings that change weekly rely on the rate accumulator
+    // described at https://docs.makerdao.com/smart-contract-modules/rates-module
+    // To check this yourself, use the following rate calculation (example 8%):
+    //
+    // $ bc -l <<< 'scale=27; e( l(1.08)/(60 * 60 * 24 * 365) )'
+    //
+    uint256 constant public ZERO_PCT_RATE = 0;
+
+    uint256 constant public WAD = 10**18;
+    uint256 constant public RAD = 10**45;
+    uint256 constant public MILLION = 10**6;
+    uint256 constant public HOUR = 3600; // in seconds
+
+    function execute() external {
+
+        // Drip Pot and Jugs prior to all modifications.
+        PotAbstract(MCD_POT).drip();
+        JugAbstract(MCD_JUG).drip("ETH-A");
+        JugAbstract(MCD_JUG).drip("BAT-A");
+        JugAbstract(MCD_JUG).drip("USDC-A");
+
+
+        // MCD Modifications
+
+
+        // Set the ETH-A stability fee
+        // ETH_FEE is a value determined by the rate accumulator calculation (see above)
+        //  ex. an 8% annual rate will be 1000000002440418608258400030
+        //
+        // Existing Rate: 0.5%
+        // New Rate: 0%
+        uint256 ETH_FEE = ZERO_PCT_RATE;
+        JugAbstract(MCD_JUG).file("ETH-A", "duty", ETH_FEE);
+
+
+        // Set the BAT-A stability fee
+        // BAT_FEE is a value determined by the rate accumulator calculation (see above)
+        //  ex. an 8% annual rate will be 1000000002440418608258400030
+        //
+        // Existing Rate: 0.5%
+        // New Rate: 0%
+        uint256 BAT_FEE = ZERO_PCT_RATE;
+        JugAbstract(MCD_JUG).file("BAT-A", "duty", BAT_FEE);
+
+    }
+}
+
+contract DssSpell {
+
+    DSPauseAbstract  public pause =
+        DSPauseAbstract(0xbE286431454714F511008713973d3B053A2d38f3);
+    address constant public SAI_MOM = 0xF2C5369cFFb8Ea6284452b0326e326DbFdCb867C;
+    address          public action;
+    bytes32          public tag;
+    uint256          public eta;
+    bytes            public sig;
+    uint256          public expiration;
+    bool             public done;
+
+    uint256 constant internal MILLION = 10**6;
+    uint256 constant internal WAD = 10**18;
+
+    constructor() public {
+        sig = abi.encodeWithSignature("execute()");
+        action = address(new SpellAction());
+        bytes32 _tag;
+        address _action = action;
+        assembly { _tag := extcodehash(_action) }
+        tag = _tag;
+        expiration = now + 30 days;
+    }
+
+    function description() public view returns (string memory) {
+        return SpellAction(action).description();
+    }
+
+    function schedule() public {
+        require(now <= expiration, "This contract has expired");
+        require(eta == 0, "This spell has already been scheduled");
+        eta = now + DSPauseAbstract(pause).delay();
+        pause.plot(action, tag, sig, eta);
+
+        // NOTE: 'eta' check should mimic the old behavior of 'done', thus
+        // preventing these SCD changes from being executed again.
+
+
+        // Set the Sai stability fee
+        // SAI_FEE is a value determined by the rate accumulator calculation (see above)
+        //  ex. an 10% annual rate will be 1000000003022265980097387650
+        //
+        // Existing Rate: 4.0%
+        // New Rate: 3.5%
+        uint256 SAI_FEE = 1000000001090862085746321732;
+        SaiMomAbstract(SAI_MOM).setFee(SAI_FEE);
+
+    }
+
+    function cast() public {
+        require(!done, "spell-already-cast");
+        done = true;
+        pause.exec(action, tag, sig, eta);
+    }
+}
