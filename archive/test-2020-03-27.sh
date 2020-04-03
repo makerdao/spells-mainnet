dc37dac2b51e842bb82bcdf41a0c620f9e7dee5a
wilbarnes <wbarnesf@gmail.com>
2020-04-02 21:06:08 -0400

    executive draft

---
+++ archive/test-2020-03-27.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
+[[ "$ETH_RPC_URL" && "$(seth chain)" == "ethlive" ]] || { echo "Please set a mainnet ETH_RPC_URL"; exit 1; }
+
+dapp --use solc:0.5.12 build
+
+# MkrAuthority
+export DAPP_TEST_ADDRESS=0x6eEB68B2C7A918f36B78E2DB80dcF279236DDFb8
+export DAPP_TEST_TIMESTAMP=$(seth block latest timestamp)
+export DAPP_TEST_NUMBER=$(seth block latest number)
+
+LANG=C.UTF-8 hevm dapp-test --rpc="$ETH_RPC_URL" --json-file=out/dapp.sol.json --dapp-root=. --verbose 1
