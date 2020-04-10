5db7165b444e4b7a18328bfb3d0818535ce13329
Brian McMichael <brian@brianmcmichael.com>
2020-04-10 11:04:48 -0400

    Add copyright holder

--- src/2020-04-10-DssSpell.sol
+++ src/2020-04-10-DssSpell.sol
@@ -1,4 +1,4 @@
-// Copyright (C) 2020
+// Copyright (C) 2020, The Maker Foundation
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
