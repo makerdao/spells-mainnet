bfb8d480a1df52a275e77c9322d20d97c427b480
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2020-04-28 12:51:41 -0300

    Adding base spell for May 1st executive (WBTC)

---
+++ src/2020-05-01-DssSpell.sol
@@ -0,0 +1,162 @@
+// Copyright (C) 2020 Maker Ecosystem Growth Holdings, INC.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+pragma solidity 0.5.12;
+
+import "lib/dss-interfaces/src/dapp/DSPauseAbstract.sol";
+import "lib/dss-interfaces/src/dss/VatAbstract.sol";
+import "lib/dss-interfaces/src/dss/CatAbstract.sol";
+import "lib/dss-interfaces/src/dss/JugAbstract.sol";
+import "lib/dss-interfaces/src/dss/FlipAbstract.sol";
+import "lib/dss-interfaces/src/dss/SpotAbstract.sol";
+import "lib/dss-interfaces/src/dss/OsmAbstract.sol";
+import "lib/dss-interfaces/src/dss/OsmMomAbstract.sol";
+
+contract FlipFabAbstract {
+    function newFlip(address, bytes32) public returns (address);
+}
+
+contract SpellAction {
+    // Provides a descriptive tag for bot consumption
+    // This should be modified weekly to provide a summary of the actions
+    string constant public description = "2020-05-01 MakerDAO Executive Spell";
+
+    // The contracts in this list should correspond to MCD core contracts, verify
+    //  against the current release list at:
+    //     https://changelog.makerdao.com/releases/mainnet/1.0.5/contracts.json
+    //
+    // Contract addresses pertaining to the SCD ecosystem can be found at:
+    //     https://github.com/makerdao/sai#dai-v1-current-deployments
+    address constant public MCD_VAT = 0x35D1b3F3D7966A1DFe207aa4514C12a259A0492B;
+    address constant public MCD_CAT = 0x78F2c2AF65126834c51822F56Be0d7469D7A523E;
+    address constant public MCD_JUG = 0x19c0976f590D67707E62397C87829d896Dc0f1F1;
+    address constant public MCD_SPOT = 0x65C79fcB50Ca1594B025960e539eD7A9a6D434A3;
+    address constant public MCD_END = 0xaB14d3CE3F733CACB76eC2AbE7d2fcb00c99F3d5;
+    address constant public FLIPPER_MOM = 0x9BdDB99625A711bf9bda237044924E34E8570f75;
+    address constant public FLIP_FAB = 0xBAB4FbeA257ABBfe84F4588d4Eedc43656E46Fc5;
+    address constant public OSM_MOM = 0x76416A4d5190d071bfed309861527431304aA14f;
+
+    address constant public MCD_JOIN_WBTC_A = ;
+    address constant public PIP_WBTC = ;
+
+    uint256 constant public THOUSAND = 10**3;
+    uint256 constant public MILLION = 10**6;
+    uint256 constant public WAD = 10**18;
+    uint256 constant public RAY = 10**27;
+    uint256 constant public RAD = 10**45;
+
+    // Many of the settings that change weekly rely on the rate accumulator
+    // described at https://docs.makerdao.com/smart-contract-modules/rates-module
+    // To check this yourself, use the following rate calculation (example 8%):
+    //
+    // $ bc -l <<< 'scale=27; e( l(1.08)/(60 * 60 * 24 * 365) )'
+    //
+    uint256 constant public ONE_PCT_RATE =  1000000000315522921573372069;
+
+    function execute() external {
+        bytes32 ilk = "WBTC-A";
+
+        // Create the WBTC-A Flipper via the original FlipFab
+        address MCD_FLIP_WBTC_A = FlipFabAbstract(FLIP_FAB).newFlip(MCD_VAT, ilk);
+
+        // Set the WBTC PIP in the Spotter
+        SpotAbstract(MCD_SPOT).file(ilk, "pip", PIP_WBTC);
+
+        // Set the WBTC-A Flipper in the Cat
+        CatAbstract(MCD_CAT).file(ilk, "flip", MCD_FLIP_WBTC_A);
+
+        // Init WBTC-A ilk in Vat
+        VatAbstract(MCD_VAT).init(ilk);
+        // Init WBTC-A ilk in Jug
+        JugAbstract(MCD_JUG).init(ilk);
+
+        // Allow WBTC-A Join to modify Vat registry
+        VatAbstract(MCD_VAT).rely(MCD_JOIN_WBTC_A);
+        // Allow Cat to kick auctions in WBTC-A Flipper
+        FlipAbstract(MCD_FLIP_WBTC_A).rely(MCD_CAT);
+        // Allow End to yank auctions in WBTC-A Flipper
+        FlipAbstract(MCD_FLIP_WBTC_A).rely(MCD_END);
+        // Allow FlipperMom to access to the WBTC-A Flipper
+        FlipAbstract(MCD_FLIP_WBTC_A).rely(FLIPPER_MOM);
+
+        // Allow OsmMom to access to the WBTC Osm
+        OsmAbstract(PIP_WBTC).rely(OSM_MOM);
+        // Set WBTC Osm in the OsmMom for new ilk
+        OsmMomAbstract(OSM_MOM).setOsm(ilk, PIP_WBTC);
+
+        // Set the global debt ceiling to 143 MM
+        VatAbstract(MCD_VAT).file("Line", 143 * MILLION * RAD);
+        // Set the WBTC-A debt ceiling to 20 MM
+        VatAbstract(MCD_VAT).file(ilk, "line", 20 * MILLION * RAD);
+        // Set the WBTC-A dust
+        VatAbstract(MCD_VAT).file(ilk, "dust", 20 * RAD);
+        // Set the Lot size to 6 WBTC-A
+        CatAbstract(MCD_CAT).file(ilk, "lump", 6 * WAD);
+        // Set the WBTC-A liquidation penalty to 13%
+        CatAbstract(MCD_CAT).file(ilk, "chop", 113 * RAY / 100);
+        // Set the WBTC-A stability fee to 1%
+        JugAbstract(MCD_JUG).file(ilk, "duty", ONE_PCT_RATE);
+        // Set the WBTC-A percentage between bids to 3%
+        FlipAbstract(MCD_FLIP_WBTC_A).file("beg", 103 * WAD / 100);
+        // Set the WBTC-A time max time between bids to 6 hours
+        FlipAbstract(MCD_FLIP_WBTC_A).file("ttl", 6 hours);
+        // Set the WBTC-A max auction duration to 3 days
+        FlipAbstract(MCD_FLIP_WBTC_A).file("tau", 3 days);
+        // Set the WBTC-A min collateralization ratio to 150%
+        SpotAbstract(MCD_SPOT).file(ilk, "mat", 150 * RAY / 100);
+
+        // Update WBTC-A spot value in Vat
+        SpotAbstract(MCD_SPOT).poke(ilk);
+    }
+}
+
+contract DssSpell {
+
+    DSPauseAbstract  public pause =
+        DSPauseAbstract(0xbE286431454714F511008713973d3B053A2d38f3);
+    address          public action;
+    bytes32          public tag;
+    uint256          public eta;
+    bytes            public sig;
+    uint256          public expiration;
+    bool             public done;
+
+    constructor() public {
+        sig = abi.encodeWithSignature("execute()");
+        action = address(new SpellAction());
+        bytes32 _tag;
+        address _action = action;
+        assembly { _tag := extcodehash(_action) }
+        tag = _tag;
+        expiration = now + 30 days;
+    }
+
+    function description() public view returns (string memory) {
+        return SpellAction(action).description();
+    }
+
+    function schedule() public {
+        require(now <= expiration, "This contract has expired");
+        require(eta == 0, "This spell has already been scheduled");
+        eta = now + DSPauseAbstract(pause).delay();
+        pause.plot(action, tag, sig, eta);
+    }
+
+    function cast() public {
+        require(!done, "spell-already-cast");
+        done = true;
+        pause.exec(action, tag, sig, eta);
+    }
+}
---
+++ src/2020-05-01-DssSpell.t.sol
@@ -0,0 +1,452 @@
+// Copyright (C) 2020 Maker Ecosystem Growth Holdings, INC.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+pragma solidity 0.5.12;
+
+import "ds-math/math.sol";
+import "ds-test/test.sol";
+import "lib/dss-interfaces/src/Interfaces.sol";
+
+import {DssSpell, SpellAction} from "./2020-04-24-DssSpell.sol";
+
+contract Hevm { function warp(uint) public; }
+
+contract DssSpellTest is DSTest, DSMath {
+
+    // Replace with mainnet spell address to test against live
+    address constant MAINNET_SPELL = address(0);
+
+    struct SystemValues {
+        uint256 dsr;
+        uint256 dsrPct;
+        uint256 lineETH;
+        uint256 dutyETH;
+        uint256 pctETH;
+        uint256 matETH;
+        uint48  tauETH;
+        uint256 lineUSDC;
+        uint256 dutyUSDC;
+        uint256 pctUSDC;
+        uint256 matUSDC;
+        uint48  tauUSDC;
+        uint256 lineBAT;
+        uint256 dutyBAT;
+        uint256 pctBAT;
+        uint256 matBAT;
+        uint48  tauBAT;
+        uint256 lineSAI;
+        uint256 lineGlobal;
+        uint256 saiCap;
+        uint256 saiFee;
+        uint256 saiPct;
+        uint256 pauseDelay;
+    }
+
+    // If last week's spell was cast successfully, you can copy the
+    //  the values from that week's `afterSpell` var into this week's
+    //  `beforeSpell` var. Or go back to the last successful executive.
+    SystemValues beforeSpell = SystemValues({
+        dsr: 1000000000000000000000000000,
+        dsrPct: 0 * 1000,
+        lineETH: mul(100000000, RAD),
+        dutyETH: 1000000000000000000000000000,
+        pctETH: 0 * 1000,
+        matETH: mul(150, RAY) / 100,
+        tauETH: 6 hours,
+        lineUSDC: mul(20000000, RAD),
+        dutyUSDC: 1000000001847694957439350562,
+        pctUSDC: 6 * 1000,
+        matUSDC: mul(120, RAY) / 100,
+        tauUSDC: 3 days,
+        lineBAT: mul(3000000, RAD),
+        dutyBAT: 1000000000000000000000000000,
+        pctBAT: 0 * 1000,
+        matBAT: mul(150, RAY) / 100,
+        tauBAT: 6 hours,
+        lineSAI: mul(0, RAD),
+        lineGlobal: mul(123000000, RAD),
+        saiCap: mul(20000000, WAD),
+        saiFee: 1000000002586884420913935572,
+        saiPct: 8.5 * 1000,
+        pauseDelay: 12 * 60 * 60
+    });
+
+    SystemValues afterSpell = SystemValues({
+        dsr: 1000000000000000000000000000,
+        dsrPct: 0 * 1000,
+        lineETH: mul(100000000, RAD),
+        dutyETH: 1000000000000000000000000000,
+        pctETH: 0 * 1000,
+        matETH: mul(150, RAY) / 100,
+        tauETH: 6 hours,
+        lineUSDC: mul(20000000, RAD),
+        dutyUSDC: 1000000001847694957439350562,
+        pctUSDC: 6 * 1000,
+        matUSDC: mul(120, RAY) / 100,
+        tauUSDC: 3 days,
+        lineBAT: mul(3000000, RAD),
+        dutyBAT: 1000000000000000000000000000,
+        pctBAT: 0 * 1000,
+        matBAT: mul(150, RAY) / 100,
+        tauBAT: 6 hours,
+        lineSAI: mul(0, RAD),
+        lineGlobal: mul(123000000, RAD),
+        saiCap: mul(20000000, WAD),
+        saiFee: 1000000002586884420913935572,
+        saiPct: 8.5 * 1000,
+        pauseDelay: 12 * 60 * 60
+    });
+
+    Hevm hevm;
+
+    DSPauseAbstract pause   = DSPauseAbstract(0xbE286431454714F511008713973d3B053A2d38f3);
+    DSChiefAbstract chief   = DSChiefAbstract(0x9eF05f7F6deB616fd37aC3c959a2dDD25A54E4F5);
+    VatAbstract     vat     = VatAbstract(0x35D1b3F3D7966A1DFe207aa4514C12a259A0492B);
+    CatAbstract     cat     = CatAbstract(0x78F2c2AF65126834c51822F56Be0d7469D7A523E);
+    PotAbstract     pot     = PotAbstract(0x197E90f9FAD81970bA7976f33CbD77088E5D7cf7);
+    JugAbstract     jug     = JugAbstract(0x19c0976f590D67707E62397C87829d896Dc0f1F1);
+    SpotAbstract   spot     = SpotAbstract(0x65C79fcB50Ca1594B025960e539eD7A9a6D434A3);
+    MKRAbstract     gov     = MKRAbstract(0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2);
+    SaiTubAbstract  tub     = SaiTubAbstract(0x448a5065aeBB8E423F0896E6c5D525C040f59af3);
+    FlipAbstract  eflip     = FlipAbstract(0xd8a04F5412223F513DC55F839574430f5EC15531);
+    FlipAbstract  bflip     = FlipAbstract(0xaA745404d55f88C108A28c86abE7b5A1E7817c07);
+    FlipAbstract  uflip     = FlipAbstract(0xE6ed1d09a19Bd335f051d78D5d22dF3bfF2c28B1);
+    SaiTopAbstract  top     = SaiTopAbstract(0x9b0ccf7C8994E19F39b2B4CF708e0A7DF65fA8a3);
+    GemAbstract     usdc    = GemAbstract(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+    DSValueAbstract uPip    = DSValueAbstract(0x77b68899b99b686F415d074278a9a16b336085A0);
+
+    DssSpell spell;
+
+    // this spell is intended to run as the MkrAuthority
+    function canCall(address, address, bytes4) public pure returns (bool) {
+        return true;
+    }
+
+    // CHEAT_CODE = 0x7109709ECfa91a80626fF3989D68f67F5b1DD12D
+    bytes20 constant CHEAT_CODE =
+        bytes20(uint160(uint256(keccak256('hevm cheat code'))));
+
+    // not provided in DSMath
+    uint constant RAD = 10 ** 45;
+    function rpow(uint x, uint n, uint b) internal pure returns (uint z) {
+      assembly {
+        switch x case 0 {switch n case 0 {z := b} default {z := 0}}
+        default {
+          switch mod(n, 2) case 0 { z := b } default { z := x }
+          let half := div(b, 2)  // for rounding.
+          for { n := div(n, 2) } n { n := div(n,2) } {
+            let xx := mul(x, x)
+            if iszero(eq(div(xx, x), x)) { revert(0,0) }
+            let xxRound := add(xx, half)
+            if lt(xxRound, xx) { revert(0,0) }
+            x := div(xxRound, b)
+            if mod(n,2) {
+              let zx := mul(z, x)
+              if and(iszero(iszero(x)), iszero(eq(div(zx, x), z))) { revert(0,0) }
+              let zxRound := add(zx, half)
+              if lt(zxRound, zx) { revert(0,0) }
+              z := div(zxRound, b)
+            }
+          }
+        }
+      }
+    }
+    // 10^-5 (tenth of a basis point) as a RAY
+    uint256 TOLERANCE = 10 ** 22;
+
+    function setUp() public {
+        hevm = Hevm(address(CHEAT_CODE));
+        // Using the MkrAuthority test address, mint enough MKR to overcome the
+        // current hat.
+        gov.mint(address(this), 300000 ether);
+
+        spell = MAINNET_SPELL != address(0) ? DssSpell(MAINNET_SPELL) : new DssSpell();
+    }
+
+    function vote() private {
+        if (chief.hat() != address(spell)) {
+            gov.approve(address(chief), uint256(-1));
+            chief.lock(sub(gov.balanceOf(address(this)), 1 ether));
+
+            assertTrue(!spell.done());
+
+            address[] memory yays = new address[](1);
+            yays[0] = address(spell);
+
+            chief.vote(yays);
+            chief.lift(address(spell));
+        }
+        assertEq(chief.hat(), address(spell));
+    }
+
+    function waitAndCast() public {
+        hevm.warp(add(now, pause.delay()));
+        spell.cast();
+    }
+
+    function scheduleWaitAndCast() public {
+        spell.schedule();
+        hevm.warp(add(now, pause.delay()));
+        spell.cast();
+    }
+
+    function stringToBytes32(string memory source) public pure returns (bytes32 result) {
+        assembly {
+        result := mload(add(source, 32))
+        }
+    }
+
+    function yearlyYield(uint256 duty) public pure returns (uint256) {
+        return rpow(duty, (365 * 24 * 60 *60), RAY);
+    }
+
+    function expectedRate(uint256 percentValue) public pure returns (uint256) {
+        return (100000 + percentValue) * (10 ** 22);
+    }
+
+    function diffCalc(uint256 expectedRate_, uint256 yearlyYield_) public pure returns (uint256) {
+        return (expectedRate_ > yearlyYield_) ? expectedRate_ - yearlyYield_ : yearlyYield_ - expectedRate_;
+    }
+
+    function testSpellIsCast() public {
+        // Test description
+        string memory description = new SpellAction().description();
+        assertTrue(bytes(description).length > 0);
+        // DS-Test can't handle strings directly, so cast to a bytes32.
+        assertEq(stringToBytes32(spell.description()),
+            stringToBytes32(description));
+
+        // Test expiration
+        // TODO fix this for deployed contract
+        if(address(spell) != address(MAINNET_SPELL)) {
+            assertEq(spell.expiration(), (now + 30 days));
+        }
+
+        // (ETH-A, USDC-A, BAT-A, DSR)
+        (uint dutyETH,)  = jug.ilks("ETH-A");
+        (uint dutyUSDC,) = jug.ilks("USDC-A");
+        (uint dutyBAT,)  = jug.ilks("BAT-A");
+        assertEq(dutyETH,   beforeSpell.dutyETH);
+        assertTrue(diffCalc(expectedRate(beforeSpell.pctETH), yearlyYield(beforeSpell.dutyETH)) <= TOLERANCE);
+        assertEq(dutyUSDC,   beforeSpell.dutyUSDC);
+        assertTrue(diffCalc(expectedRate(beforeSpell.pctUSDC), yearlyYield(beforeSpell.dutyUSDC)) <= TOLERANCE);
+        assertEq(dutyBAT,   beforeSpell.dutyBAT);
+        assertTrue(diffCalc(expectedRate(beforeSpell.pctBAT), yearlyYield(beforeSpell.dutyBAT)) <= TOLERANCE);
+        assertEq(pot.dsr(), beforeSpell.dsr);
+        assertTrue(diffCalc(expectedRate(beforeSpell.dsrPct), yearlyYield(beforeSpell.dsr)) <= TOLERANCE);
+
+        // ETH-A line
+        (,,, uint256 lineETH,) = vat.ilks("ETH-A");
+        assertEq(lineETH, beforeSpell.lineETH);
+
+        // USDC-A line
+        (,,, uint256 lineUSDC,) = vat.ilks("USDC-A");
+        assertEq(lineUSDC, beforeSpell.lineUSDC);
+
+        // BAT-A line
+        (,,, uint256 lineBAT,) = vat.ilks("BAT-A");
+        assertEq(lineBAT, beforeSpell.lineBAT);
+
+        // SAI line
+        (,,, uint256 lineSAI,) = vat.ilks("SAI");
+        assertEq(lineSAI, beforeSpell.lineSAI);
+
+        // Line
+        assertEq(vat.Line(), beforeSpell.lineGlobal);
+
+        // ETH-A mat
+        (,uint256 matETH) = spot.ilks("ETH-A");
+        assertEq(matETH, beforeSpell.matETH);
+
+        // USDC-A mat
+        (,uint256 matUSDC) = spot.ilks("USDC-A");
+        assertEq(matUSDC, beforeSpell.matUSDC);
+
+        // BAT-A mat
+        (,uint256 matBAT) = spot.ilks("BAT-A");
+        assertEq(matBAT, beforeSpell.matBAT);
+
+        // SCD DC
+        assertEq(tub.cap(), beforeSpell.saiCap);
+
+        // SCD Fee
+        assertEq(tub.fee(), beforeSpell.saiFee);
+        assertTrue(diffCalc(expectedRate(beforeSpell.saiPct), yearlyYield(beforeSpell.saiFee)) <= TOLERANCE);
+
+        // flip tau amount precheck
+        assertEq(uint256(eflip.tau()), beforeSpell.tauETH);
+        assertEq(uint256(uflip.tau()), beforeSpell.tauUSDC);
+        assertEq(uint256(bflip.tau()), beforeSpell.tauBAT);
+
+        // Pause delay
+        assertEq(pause.delay(), beforeSpell.pauseDelay);
+
+        vote();
+
+        scheduleWaitAndCast();
+
+        // spell done
+        assertTrue(spell.done());
+
+        // dsr
+        assertEq(pot.dsr(), afterSpell.dsr);
+        assertTrue(diffCalc(expectedRate(afterSpell.dsrPct), yearlyYield(afterSpell.dsr)) <= TOLERANCE);
+
+        // (ETH-A, USDC-A, BAT-A)
+        (dutyETH,)  = jug.ilks("ETH-A");
+        (dutyUSDC,) = jug.ilks("USDC-A");
+        (dutyBAT,)  = jug.ilks("BAT-A");
+        assertEq(dutyETH, afterSpell.dutyETH);
+        assertTrue(diffCalc(expectedRate(afterSpell.pctETH), yearlyYield(afterSpell.dutyETH)) <= TOLERANCE);
+        assertEq(dutyUSDC, afterSpell.dutyUSDC);
+        assertTrue(diffCalc(expectedRate(afterSpell.pctUSDC), yearlyYield(afterSpell.dutyUSDC)) <= TOLERANCE);
+        assertEq(dutyBAT, afterSpell.dutyBAT);
+        assertTrue(diffCalc(expectedRate(afterSpell.pctBAT), yearlyYield(afterSpell.dutyBAT)) <= TOLERANCE);
+
+        // ETH-A line
+        (,,, lineETH,) = vat.ilks("ETH-A");
+        assertEq(lineETH, afterSpell.lineETH);
+
+        // USDC-A line
+        (,,, lineUSDC,) = vat.ilks("USDC-A");
+        assertEq(lineUSDC, afterSpell.lineUSDC);
+
+        // BAT-A line
+        (,,, lineBAT,) = vat.ilks("BAT-A");
+        assertEq(lineBAT, afterSpell.lineBAT);
+
+        // SAI line
+        (,,, lineSAI,) = vat.ilks("SAI");
+        assertEq(lineSAI, afterSpell.lineSAI);
+
+        // Line
+        assertEq(vat.Line(), afterSpell.lineGlobal);
+
+        // ETH-A mat
+        (, matETH) = spot.ilks("ETH-A");
+        assertEq(matETH, afterSpell.matETH);
+
+        // USDC-A mat
+        (, matUSDC) = spot.ilks("USDC-A");
+        assertEq(matUSDC, afterSpell.matUSDC);
+
+        // BAT-A mat
+        (, matBAT) = spot.ilks("BAT-A");
+        assertEq(matBAT, afterSpell.matBAT);
+
+        // SCD DC
+        assertEq(tub.cap(), afterSpell.saiCap);
+
+        // SCD Fee
+        assertEq(tub.fee(), afterSpell.saiFee);
+        assertTrue(diffCalc(expectedRate(afterSpell.saiPct), yearlyYield(afterSpell.saiFee)) <= TOLERANCE);
+
+        // Pause delay
+        assertEq(pause.delay(), afterSpell.pauseDelay);
+
+        // flip tau amount
+        assertEq(uint256(eflip.tau()), afterSpell.tauETH);
+        assertEq(uint256(uflip.tau()), afterSpell.tauUSDC);
+        assertEq(uint256(bflip.tau()), afterSpell.tauBAT);
+    }
+
+    function testNewCollateral() public {
+        vote();
+        scheduleWaitAndCast();
+
+        // spell done
+        assertTrue(spell.done());
+
+        // WBTC-A fee
+        (uint256 dutyWBTC,) = jug.ilks("WBTC-A");
+        assertEq(dutyWBTC, 1000000005781378656804591712);
+        assertTrue(diffCalc(expectedRate(20 * 1000), yearlyYield(dutyWBTC)) <= TOLERANCE);
+
+        // WBTC-A line and dust
+        (,,, uint256 lineWBTC, uint256 dustWBTC) = vat.ilks("WBTC-A");
+        assertEq(lineWBTC, 20 * MILLION * RAD);
+        assertEq(dustWBTC, 20 * RAD);
+
+        // WBTC-A liquidation penalty and lot size
+        (address aux, uint256 chop, uint256 lump) = cat.ilks("WBTC-A");
+        FlipAbstract uFlip = FlipAbstract(aux);
+        assertEq(chop, 113 * RAY / 100);
+        assertEq(lump, 50 * THOUSAND * WAD);
+
+        // WBTC-A percentage between bids
+        assertEq(uFlip.beg(), 103 * WAD / 100);
+        // WBTC-A max time between bids
+        assertEq(uint256(uFlip.ttl()), 6 hours);
+        // WBTC-A max auction duration
+        assertEq(uint256(uFlip.tau()), 3 days);
+
+        // WBTC-A min collateralization ratio
+        (, uint256 mat) = spot.ilks("WBTC-A");
+        assertEq(mat, 125 * RAY / 100);
+
+        // Line
+        assertEq(vat.Line(), 133 * MILLION * RAD);
+
+        // WBTC Pip => 1 WBTC == 1 DAI
+        assertEq(uint256(wPip.read()), 1 * WAD);
+        // WBTC Pip Owner
+        assertEq(wPip.owner(), pauseProxy);
+        // WBTC Pip Authority
+        assertEq(wPip.authority(), address(0));
+
+        // Authorization
+        assertEq(wJoin.wards(pauseProxy), 1);
+        assertEq(vat.wards(address(wJoin)), 1);
+        assertEq(wFlip.wards(address(cat)), 0); // FlipperMom denied it at the end of the spell (no liquidations on first phase)
+        assertEq(wFlip.wards(address(end)), 1);
+        assertEq(wFlip.wards(flipperMom), 1);
+
+        // Start testing Vault
+
+        // Join to adapter
+        assertEq(wbtc.balanceOf(address(this)), 40 * 10 ** 6);
+        assertEq(vat.gem("WBTC-A", address(this)), 0);
+        wbtc.approve(address(wJoin), 40 * 10 ** 6);
+        wJoin.join(address(this), 40 * 10 ** 6);
+        assertEq(wbtc.balanceOf(address(this)), 0);
+        assertEq(vat.gem("WBTC-A", address(this)), 40 * WAD);
+
+        // Deposit collateral, generate DAI
+        assertEq(vat.dai(address(this)), 0);
+        vat.frob("WBTC-A", address(this), address(this), address(this), int(40 * WAD), int(25 * WAD));
+        assertEq(vat.gem("WBTC-A", address(this)), 0);
+        assertEq(vat.dai(address(this)), 25 * RAD);
+
+        // Payback DAI, withdraw collateral
+        vat.frob("WBTC-A", address(this), address(this), address(this), -int(40 * WAD), -int(25 * WAD));
+        assertEq(vat.gem("WBTC-A", address(this)), 40 * WAD);
+        assertEq(vat.dai(address(this)), 0);
+
+        // Withdraw from adapter
+        wJoin.exit(address(this), 40 * 10 ** 6);
+        assertEq(wbtc.balanceOf(address(this)), 40 * 10 ** 6);
+        assertEq(vat.gem("WBTC-A", address(this)), 0);
+
+        // // Generate new DAI to force a liquidation
+        // wbtc.approve(address(wJoin), 40 * 10 ** 6);
+        // wJoin.join(address(this), 40 * 10 ** 6);
+        // vat.frob("WBTC-A", address(this), address(this), address(this), int(40 * WAD), int(32 * WAD)); // Max amount of DAI
+        // hevm.warp(now + 1);
+        // jug.drip("WBTC-A");
+        // assertEq(wFlip.kicks(), 0);
+        // cat.bite("WBTC-A", address(this));
+        // assertEq(wFlip.kicks(), 1);
+    }
+}
