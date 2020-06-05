19270e276ab3fe35b7dd908362a4c54aaf995614
andy8052 <andychorlian@gmail.com>
2020-06-05 11:41:21 -0400

    Cleaning up spell

--- src/2020-06-05-DssSpell.sol
+++ src/2020-06-05-DssSpell.sol
@@ -36,38 +36,12 @@ contract SpellAction {
     // Contract addresses pertaining to the SCD ecosystem can be found at:
     //     https://github.com/makerdao/sai#dai-v1-current-deployments
     address constant public MCD_VAT             = 0x35D1b3F3D7966A1DFe207aa4514C12a259A0492B;
-    address constant public MCD_CAT             = 0x78F2c2AF65126834c51822F56Be0d7469D7A523E;
     address constant public MCD_JUG             = 0x19c0976f590D67707E62397C87829d896Dc0f1F1;
     address constant public MCD_POT             = 0x197E90f9FAD81970bA7976f33CbD77088E5D7cf7;
-
-    address constant public MCD_SPOT            = 0x65C79fcB50Ca1594B025960e539eD7A9a6D434A3;
-    address constant public MCD_END             = 0xaB14d3CE3F733CACB76eC2AbE7d2fcb00c99F3d5;
-    address constant public FLIPPER_MOM         = 0x9BdDB99625A711bf9bda237044924E34E8570f75;
-
-    // USDC specific addresses
-    address constant public MCD_JOIN_USDC_B     = 0x2600004fd1585f7270756DDc88aD9cfA10dD0428;
-    address constant public PIP_USDC            = 0x77b68899b99b686F415d074278a9a16b336085A0;
-    address constant public MCD_FLIP_USDC_B     = 0xec25Ca3fFa512afbb1784E17f1D414E16D01794F;
-
-    // TUSD specific addresses
-    address constant public MCD_JOIN_TUSD_A     = 0x4454aF7C8bb9463203b66C816220D41ED7837f44;
-    address constant public PIP_TUSD            = 0xeE13831ca96d191B688A670D47173694ba98f1e5;
-    address constant public MCD_FLIP_TUSD_A     = 0xba3f6a74BD12Cf1e48d4416c7b50963cA98AfD61;
     
     // decimals & precision
-    uint256 constant public THOUSAND            = 10 ** 3;
     uint256 constant public MILLION             = 10 ** 6;
-    uint256 constant public WAD                 = 10 ** 18;
-    uint256 constant public RAY                 = 10 ** 27;
     uint256 constant public RAD                 = 10 ** 45;
-
-    // Many of the settings that change weekly rely on the rate accumulator
-    // described at https://docs.makerdao.com/smart-contract-modules/rates-module
-    // To check this yourself, use the following rate calculation (example 8%):
-    //
-    // $ bc -l <<< 'scale=27; e( l(1.08)/(60 * 60 * 24 * 365) )'
-    //
-    uint256 constant public ZERO_PCT_RATE       = 1000000000000000000000000000;
 
     function execute() external {
 
@@ -75,7 +49,9 @@ contract SpellAction {
         JugAbstract(MCD_JUG).drip("ETH-A");
         JugAbstract(MCD_JUG).drip("BAT-A");
         JugAbstract(MCD_JUG).drip("USDC-A");
+        JugAbstract(MCD_JUG).drip("USDC-B");
         JugAbstract(MCD_JUG).drip("WBTC-A");
+        JugAbstract(MCD_JUG).drip("TUSD-A");
 
         ////////////////////////////////////////////////////////////////////////////////
         // GLOBAL 
