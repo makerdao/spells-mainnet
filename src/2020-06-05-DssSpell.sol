c8f07a0b825efebe3859efa9611603d8dba81aec
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2020-06-07 20:13:57 -0300

    2020-06-05 DssSpell Initial Commit (#55)
    * 2020-06-05 DssSpell Initial Commit
    
    * Cleaning up spell
    
    * Remove unused imports
    
    * Update test with deployed spell
    
    Co-authored-by: andy8052 <andychorlian@gmail.com>
    Co-authored-by: Mariano Conti <nanexcool@gmail.com>

---
+++ src/2020-06-05-DssSpell.sol
@@ -0,0 +1,109 @@
+// Copyright (C) 2020 Maker Ecosystem Growth Holdings, INC.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+pragma solidity 0.5.12;
+
+import "lib/dss-interfaces/src/dapp/DSPauseAbstract.sol";
+import "lib/dss-interfaces/src/dss/VatAbstract.sol";
+import "lib/dss-interfaces/src/dss/PotAbstract.sol";
+import "lib/dss-interfaces/src/dss/JugAbstract.sol";
+
+contract SpellAction {
+    // Provides a descriptive tag for bot consumption
+    // This should be modified weekly to provide a summary of the actions
+    string constant public description = "2020-06-05 MakerDAO Executive Spell";
+
+    // The contracts in this list should correspond to MCD core contracts, verify
+    //  against the current release list at:
+    //     https://changelog.makerdao.com/releases/mainnet/1.0.7/contracts.json
+    //
+    // Contract addresses pertaining to the SCD ecosystem can be found at:
+    //     https://github.com/makerdao/sai#dai-v1-current-deployments
+    address constant public MCD_VAT             = 0x35D1b3F3D7966A1DFe207aa4514C12a259A0492B;
+    address constant public MCD_JUG             = 0x19c0976f590D67707E62397C87829d896Dc0f1F1;
+    address constant public MCD_POT             = 0x197E90f9FAD81970bA7976f33CbD77088E5D7cf7;
+
+    // decimals & precision
+    uint256 constant public MILLION             = 10 ** 6;
+    uint256 constant public RAD                 = 10 ** 45;
+
+    function execute() external {
+
+        PotAbstract(MCD_POT).drip();
+        JugAbstract(MCD_JUG).drip("ETH-A");
+        JugAbstract(MCD_JUG).drip("BAT-A");
+        JugAbstract(MCD_JUG).drip("USDC-A");
+        JugAbstract(MCD_JUG).drip("USDC-B");
+        JugAbstract(MCD_JUG).drip("WBTC-A");
+        JugAbstract(MCD_JUG).drip("TUSD-A");
+
+        ////////////////////////////////////////////////////////////////////////////////
+        // GLOBAL
+
+        // set the global debt ceiling to 185,000,000
+        // 165 (current DC) + 20 (ETH-A)
+        uint256 ALL_LINE = 185 * MILLION;
+        VatAbstract(MCD_VAT).file("Line", ALL_LINE * RAD);
+
+        ////////////////////////////////////////////////////////////////////////////////
+        // ETH-A
+
+        // Set the ETH-A debt ceiling
+        // ETH_LINE is the number of Dai that can be created with WETH token collateral
+        //  ex. a 100 million Dai ETH ceiling will be ETH_LINE = 100,000,000
+        // Existing Line: 120m
+        // New Line: 140m
+        uint256 ETH_LINE = 140 * MILLION;
+        VatAbstract(MCD_VAT).file("ETH-A", "line", ETH_LINE * RAD);
+    }
+}
+
+contract DssSpell {
+    DSPauseAbstract  public pause =
+        DSPauseAbstract(0xbE286431454714F511008713973d3B053A2d38f3);
+    address          public action;
+    bytes32          public tag;
+    uint256          public eta;
+    bytes            public sig;
+    uint256          public expiration;
+    bool             public done;
+
+    constructor() public {
+        sig = abi.encodeWithSignature("execute()");
+        action = address(new SpellAction());
+        bytes32 _tag;
+        address _action = action;
+        assembly { _tag := extcodehash(_action) }
+        tag = _tag;
+        expiration = now + 30 days;
+    }
+
+    function description() public view returns (string memory) {
+        return SpellAction(action).description();
+    }
+
+    function schedule() public {
+        require(now <= expiration, "This contract has expired");
+        require(eta == 0, "This spell has already been scheduled");
+        eta = now + DSPauseAbstract(pause).delay();
+        pause.plot(action, tag, sig, eta);
+    }
+
+    function cast() public {
+        require(!done, "spell-already-cast");
+        done = true;
+        pause.exec(action, tag, sig, eta);
+    }
+}
