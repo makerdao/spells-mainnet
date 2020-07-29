396ba76fb929da119adbed63cedfaf01fe7746f3
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2020-07-29 17:34:45 -0300

    Some cleaning

--- src/2020-07-29-DssSpell.sol
+++ src/2020-07-29-DssSpell.sol
@@ -17,14 +17,11 @@ pragma solidity 0.5.12;
 
 import "lib/dss-interfaces/src/dapp/DSPauseAbstract.sol";
 import "lib/dss-interfaces/src/dss/VatAbstract.sol";
-import "lib/dss-interfaces/src/dss/JugAbstract.sol";
-import "lib/dss-interfaces/src/dss/PotAbstract.sol";
 
 contract SpellAction {
 
     // Provides a descriptive tag for bot consumption
     // This should be modified weekly to provide a summary of the actions
-    // Hash: seth keccak -- "$(wget https://raw.githubusercontent.com/makerdao/community/cc819c75fc8f1b622cbe06acfd0d11bf64545622/governance/votes/Executive%20vote%20-%20July%2027%2C%202020%20.md -q -O - 2>/dev/null)"
     string constant public description =
         "2020-07-29 MakerDAO Executive Spell | Off-cycle Executive";
 
@@ -34,37 +31,13 @@ contract SpellAction {
     // against the current release list at:
     //     https://changelog.makerdao.com/releases/mainnet/1.0.9/contracts.json
 
-    address constant MCD_VAT             = 0x35D1b3F3D7966A1DFe207aa4514C12a259A0492B;
-    address constant MCD_JUG             = 0x19c0976f590D67707E62397C87829d896Dc0f1F1;
-    address constant MCD_POT             = 0x197E90f9FAD81970bA7976f33CbD77088E5D7cf7;
+    address constant MCD_VAT = 0x35D1b3F3D7966A1DFe207aa4514C12a259A0492B;
 
     // Decimals & precision
-    uint256 constant THOUSAND = 10 ** 3;
     uint256 constant MILLION  = 10 ** 6;
-    uint256 constant WAD      = 10 ** 18;
-    uint256 constant RAY      = 10 ** 27;
     uint256 constant RAD      = 10 ** 45;
 
-    // Many of the settings that change weekly rely on the rate accumulator
-    // described at https://docs.makerdao.com/smart-contract-modules/rates-module
-    // To check this yourself, use the following rate calculation (example 8%):
-    //
-    // $ bc -l <<< 'scale=27; e( l(1.08)/(60 * 60 * 24 * 365) )'
-
     function execute() external {
-        // Perform drips
-        PotAbstract(MCD_POT).drip();
-
-        JugAbstract(MCD_JUG).drip("ETH-A");
-        JugAbstract(MCD_JUG).drip("BAT-A");
-        JugAbstract(MCD_JUG).drip("USDC-A");
-        JugAbstract(MCD_JUG).drip("USDC-B");
-        JugAbstract(MCD_JUG).drip("TUSD-A");
-        JugAbstract(MCD_JUG).drip("WBTC-A");
-        JugAbstract(MCD_JUG).drip("KNC-A");
-        JugAbstract(MCD_JUG).drip("ZRX-A");
-        JugAbstract(MCD_JUG).drip("MANA-A");
-
         // Set the global debt ceiling
         // Existing Line: 386m
         // New Line: 550m
@@ -73,27 +46,27 @@ contract SpellAction {
         // Set the ETH-A debt ceiling
         // Existing line: 260m
         // New line: 340m
-        VatAbstract(MCD_VAT).file("ETH-A", "line", 340 * MILLION * RAD); // 260 MM debt ceiling
+        VatAbstract(MCD_VAT).file("ETH-A", "line", 340 * MILLION * RAD);
 
         // Set the BAT-A debt ceiling
         // Existing line: 3m
         // New line: 5m
-        VatAbstract(MCD_VAT).file("ETH-A", "line", 5 * MILLION * RAD); // 260 MM debt ceiling
+        VatAbstract(MCD_VAT).file("ETH-A", "line", 5 * MILLION * RAD);
 
         // Set the USDC-A debt ceiling
         // Existing line: 80m
         // New line: 140m
-        VatAbstract(MCD_VAT).file("USDC-A", "line", 140 * MILLION * RAD); // 260 MM debt ceiling
+        VatAbstract(MCD_VAT).file("USDC-A", "line", 140 * MILLION * RAD);
 
         // Set the WBTC-A debt ceiling
         // Existing line: 20m
         // New line: 40m
-        VatAbstract(MCD_VAT).file("WBTC-A", "line", 40 * MILLION * RAD); // 260 MM debt ceiling
+        VatAbstract(MCD_VAT).file("WBTC-A", "line", 40 * MILLION * RAD);
 
         // Set the USDC-B debt ceiling
         // Existing line: 10m
         // New line: 30m
-        VatAbstract(MCD_VAT).file("USDC-B", "line", 30 * MILLION * RAD); // 260 MM debt ceiling
+        VatAbstract(MCD_VAT).file("USDC-B", "line", 30 * MILLION * RAD);
 
     }
 }
