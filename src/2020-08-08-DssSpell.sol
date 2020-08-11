a18f5a08f8b63a391453e46caaf5371691fbea9c
Brian L. McMichael <brian@brianmcmichael.com>
2020-08-11 11:43:26 -0400

    2020/08/08 Executive precision fix (#73)
    Co-authored-by: Mariano Conti <nanexcool@gmail.com>

---
+++ src/2020-08-08-DssSpell.sol
@@ -0,0 +1,85 @@
+// Copyright (C) 2020 Maker Ecosystem Growth Holdings, INC.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+pragma solidity 0.5.12;
+
+import "lib/dss-interfaces/src/dapp/DSPauseAbstract.sol";
+import "lib/dss-interfaces/src/dss/VatAbstract.sol";
+
+contract SpellAction {
+
+    // Provides a descriptive tag for bot consumption
+    // This should be modified weekly to provide a summary of the actions
+    string constant public description =
+        "2020-08-08 MakerDAO Precision Fix";
+
+    // MAINNET ADDRESSES
+    //
+    // The contracts in this list should correspond to MCD core contracts, verify
+    // against the current release list at:
+    //     https://changelog.makerdao.com/releases/mainnet/1.0.9/contracts.json
+
+    address constant public MCD_VAT = 0x35D1b3F3D7966A1DFe207aa4514C12a259A0492B;
+
+    uint256 constant public RAD = 10**45;
+    uint256 constant public MILLION = 10**6;
+
+    function execute() external {
+        // Set the WBTC-A debt ceiling
+        //
+        // Existing debt: ~0 (error in last executive)
+        // New debt ceiling: 80 million
+        uint256 WBTC_LINE = 80 * MILLION * RAD;
+        VatAbstract(MCD_VAT).file("WBTC-A", "line", WBTC_LINE);
+    }
+}
+
+contract DssSpell {
+    DSPauseAbstract public pause =
+        DSPauseAbstract(0xbE286431454714F511008713973d3B053A2d38f3);
+    address         public action;
+    bytes32         public tag;
+    uint256         public eta;
+    bytes           public sig;
+    uint256         public expiration;
+    bool            public done;
+
+    constructor() public {
+        sig = abi.encodeWithSignature("execute()");
+        action = address(new SpellAction());
+        bytes32 _tag;
+        address _action = action;
+        assembly { _tag := extcodehash(_action) }
+        tag = _tag;
+        expiration = now + 30 days;
+    }
+
+    function description() public view returns (string memory) {
+        return SpellAction(action).description();
+    }
+
+    function schedule() public {
+        require(now <= expiration, "This contract has expired");
+        require(eta == 0, "This spell has already been scheduled");
+        eta = now + DSPauseAbstract(pause).delay();
+        pause.plot(action, tag, sig, eta);
+    }
+
+    function cast() public {
+        require(!done, "spell-already-cast");
+        done = true;
+        pause.exec(action, tag, sig, eta);
+    }
+}
