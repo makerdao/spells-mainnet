d9c9038ea68d2eca2bcc71dff995935e71d195d7
wilbarnes <wbarnesf@gmail.com>
2020-09-04 11:14:57 -0400

    adding suggested comment

--- src/DssSpell.sol
+++ src/DssSpell.sol
@@ -203,6 +203,7 @@ contract SpellAction {
 
         // Allow USDT-A Join to modify Vat registry
         VatAbstract(MCD_VAT).rely(MCD_JOIN_USDT_A);
+        // Allow USDT-A Flipper on the Cat
         CatAbstract(MCD_CAT).rely(MCD_FLIP_USDT_A);
 
         // Allow cat to kick auctions in USDT-A Flipper
@@ -263,6 +264,7 @@ contract SpellAction {
 
         // Allow PAXUSD-A Join to modify Vat registry
         VatAbstract(MCD_VAT).rely(MCD_JOIN_PAXUSD_A);
+        // Allow PAXUSD-A Flipper on the Cat
         CatAbstract(MCD_CAT).rely(MCD_FLIP_PAXUSD_A);
 
         // Allow cat to kick auctions in PAXUSD-A Flipper
