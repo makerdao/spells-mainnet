754a2e8f01c23a9998d77dc91126869008c6d717
Kurt Barry <kurt.m.barry@gmail.com>
2020-10-23 11:08:08 -0400

    test against mainnet

--- src/DssSpell.t.sol
+++ src/DssSpell.t.sol
@@ -21,9 +21,9 @@ interface MedianizerV1Abstract {
 
 contract DssSpellTest is DSTest, DSMath {
     // populate with mainnet spell if needed
-    address constant MAINNET_SPELL = address(0);
+    address constant MAINNET_SPELL = address(0xb04A29de213411DDb7196eD1327b3B6144893E59);
     // this needs to be updated
-    uint256 constant SPELL_CREATED = 1602860406;
+    uint256 constant SPELL_CREATED = 1603464858;
 
     struct CollateralValues {
         uint256 line;
