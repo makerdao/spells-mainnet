0e16b110e647faa3a1f73ea68c081e824186f096
Kurt Barry <kurt@makerdao.com>
2020-10-26 11:41:59 -0700

    Sc 6757 oct2020 monthly exec (#103)
    Co-authored-by: Lucas <lucas@makerdao.com>
    Co-authored-by: Brian McMichael <brian@brianmcmichael.com>

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
-all    :; SOLC_FLAGS="--optimize --optimize-runs=1" \
-		dapp --use solc:0.5.12 build
-clean  :; dapp clean
-test   :; ./test-dssspell.sh
-deploy :; SOLC_FLAGS="--optimize --optimize-runs=1" \
+all     :; SOLC_FLAGS="--optimize --optimize-runs=1" \
+               dapp --use solc:0.5.12 build
+clean   :; dapp clean
+test    :; ./test-dssspell.sh
+deploy  :; SOLC_FLAGS="--optimize --optimize-runs=1" \
     dapp --use solc:0.5.12 build && \
     dapp create DssSpell --gas=${ETH_GAS} --gas-price=${ETH_GAS_PRICE}
+flatten :; hevm flatten --source-file "src/DssSpell.sol"
---
+++ compare-bytecode.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Purpose:
+#  To compare the runtime bytecode of the current spell (src/DssSpell.sol) as
+#  output by the compiler against the onchain bytecode of a provided contract
+#  address. Can be used to verify that the deployed bytecode is correct even
+#  if Etherscan is down.
+#
+# Prerequisites:
+#  Your ETH_RPC_URL environment variable must be set to an Ethereum node
+#  endpoint exposing the getCode API.
+#
+# Arguments:
+#  $1 : address of on-chain spell
+#
+# Example usage:
+#  ./compare-bytecode.sh 0xb04A29de213411DDb7196eD1327b3B6144893E59
+#
+# Output:
+#  A message stating whether the locally compiled spell matches the specified
+#  contract, or possibly some sort of weird error.
+
+make all &> /dev/null
+COMPILED_BYTECODE=0x`jq '.contracts|.["src/DssSpell.sol:DssSpell"]|.["bin-runtime"]' ./out/dapp.sol.json | sed 's/"//g'`
+CB=${COMPILED_BYTECODE::-104}  # Trim swarm hash
+ONCHAIN_BYTECODE=`curl -s --data '{"method": "eth_getCode", "params":["'${1}'", "latest"], "id":1, "jsonrpc":"2.0"}' -H "Content-Type: application/json" -X POST ${ETH_RPC_URL} | jq '.result' | sed 's/"//g'`
+OB=${ONCHAIN_BYTECODE::-104}  # Trim swarm hash
+if [ "$CB" = "$OB" ] ; then
+    echo -e "\e[32mSUCCESS! \e[39mBytecodes match."
+else
+    echo -e "\e[31mFAILURE. \e[39mBytecodes do NOT match."
+fi
