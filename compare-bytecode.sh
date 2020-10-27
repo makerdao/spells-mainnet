ec8baa4a2badb5fc72fd9238b47ec18427a98b42
Lucas Manuel <lucas@makerdao.com>
2020-10-27 11:54:22 -0400

    feat: Change address to optional param in compare-bytecode.sh (#104)
    * feat: change address to optional param in compare-bytecode.sh
    
    * Fix for mac os x bash
    
    * fix: update vow.hump
    
    * Show bytecode snippet
    
    Co-authored-by: Gonzalo Balabasquer <gbalabasquer@gmail.com>
    Co-authored-by: Brian McMichael <brian@brianmcmichael.com>

--- compare-bytecode.sh
+++ compare-bytecode.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 # Purpose:
 #  To compare the runtime bytecode of the current spell (src/DssSpell.sol) as
 #  output by the compiler against the onchain bytecode of a provided contract
@@ -22,9 +21,12 @@
 
 make all &> /dev/null
 COMPILED_BYTECODE=0x`jq '.contracts|.["src/DssSpell.sol:DssSpell"]|.["bin-runtime"]' ./out/dapp.sol.json | sed 's/"//g'`
-CB=${COMPILED_BYTECODE::-104}  # Trim swarm hash
-ONCHAIN_BYTECODE=`curl -s --data '{"method": "eth_getCode", "params":["'${1}'", "latest"], "id":1, "jsonrpc":"2.0"}' -H "Content-Type: application/json" -X POST ${ETH_RPC_URL} | jq '.result' | sed 's/"//g'`
-OB=${ONCHAIN_BYTECODE::-104}  # Trim swarm hash
+CB=${COMPILED_BYTECODE::${#COMPILED_BYTECODE}-104}  # Trim swarm hash
+DEPLOYED_ADDRESS=`cat src/DssSpell.t.sol | grep "address constant MAINNET_SPELL" | sed -e 's#.*address(\(\)#\1#' | sed 's/);.*//'`
+ONCHAIN_BYTECODE=`curl -s --data '{"method": "eth_getCode", "params":["'${1-$DEPLOYED_ADDRESS}'", "latest"], "id":1, "jsonrpc":"2.0"}' -H "Content-Type: application/json" -X POST ${ETH_RPC_URL} | jq '.result' | sed 's/"//g'`
+OB=${ONCHAIN_BYTECODE::${#ONCHAIN_BYTECODE}-104}  # Trim swarm hash
+echo "Compiled Bytecode: ${CB:0:40}..."
+echo "On-Chain Bytecode: ${OB:0:40}..."
 if [ "$CB" = "$OB" ] ; then
     echo -e "\e[32mSUCCESS! \e[39mBytecodes match."
 else
--- src/DssSpell.t.sol
+++ src/DssSpell.t.sol
@@ -153,7 +153,7 @@ contract DssSpellTest is DSTest, DSMath {
             vow_dump:     250,             // In whole Dai units
             vow_sump:     50000,           // In whole Dai units
             vow_bump:     10000,           // In whole Dai units
-            vow_hump:     2 * MILLION,     // In whole Dai units
+            vow_hump:     4 * MILLION,     // In whole Dai units
             cat_box:      15 * MILLION,    // In whole Dai units
             ilk_count:    15               // Num expected in system
         });
