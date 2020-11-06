787fd83fcf729d8b46a178c6372aa6783ba9c353
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2020-11-06 15:11:09 -0300

    SC-7188 2020-11-06 Weekly Executive (#108)
    * SC-7188 2020-11-06 Weekly Executive
    
    * Changes
    
    * Finish spell + some changes on tests
    
    * Fix sumlines
    
    * Uncomment tests
    
    * Rename key from CHAINLOG to CHANGELOG
    
    * Link + hash changed due content fix
    
    * New link + hash
    
    * Add mom authority checks back
    
    * Fix checks
    
    * Test with deployed spell
    
    * Fix deployed address format for script
    
    * Fix some shellcheck issues
    
    * Archive spell
    
    Co-authored-by: Christopher Mooney <chris@dod.net>

--- estimate-deploy-gas.sh
+++ estimate-deploy-gas.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-BIN=`jq '.contracts|.["src/DssSpell.sol:DssSpell"]|.bin' ./out/dapp.sol.json | sed 's/"//g'`
-seth estimate --create 0x${BIN} "DssSpell()"
+BIN=$(jq '.contracts|.["src/DssSpell.sol:DssSpell"]|.bin' ./out/dapp.sol.json | sed 's/"//g')
+seth estimate --create "0x${BIN}" "DssSpell()"
