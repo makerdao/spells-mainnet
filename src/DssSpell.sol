e96b57b4838b5696f16f984317aaa8bc53ca11e2
Lucas <lucas@makerdao.com>
2020-11-23 10:40:42 -0500

    fix: update expiration

--- src/DssSpell.sol
+++ src/DssSpell.sol
@@ -104,7 +104,7 @@ contract DssSpell {
         address _action = action;
         assembly { _tag := extcodehash(_action) }
         tag = _tag;
-        expiration = now + 30 days;
+        expiration = now + 4 days + 2 hours;
     }
 
     modifier limited {
--- src/DssSpell.t.sol
+++ src/DssSpell.t.sol
@@ -663,9 +663,9 @@ contract DssSpellTest is DSTest, DSMath {
                 stringToBytes32(description));
 
         if(address(spell) != address(MAINNET_SPELL)) {
-            assertEq(spell.expiration(), (now + 30 days));
+            assertEq(spell.expiration(), (now + 4 days + 2 hours));
         } else {
-            assertEq(spell.expiration(), (SPELL_CREATED + 30 days));
+            assertEq(spell.expiration(), (SPELL_CREATED + 4 days + 2 hours));
         }
 
         vote();
