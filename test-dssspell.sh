b10a5cfcc0d1bbdd15331dbdbdd4efe9b22e335f
Lucas Manuel <lucas@makerdao.com>
2020-12-18 10:06:05 -0500

    feat: Add UNIV2DAIETH-A (#125)
    Co-authored-by: Brian McMichael <brian@brianmcmichael.com>

--- test-dssspell.sh
+++ test-dssspell.sh
@@ -8,4 +8,4 @@ SOLC_FLAGS="--optimize --optimize-runs 1" dapp --use solc:0.6.11 build
 export DAPP_TEST_TIMESTAMP=$(seth block latest timestamp)
 export DAPP_TEST_NUMBER=$(seth block latest number)
 
-LANG=C.UTF-8 hevm dapp-test --rpc="$ETH_RPC_URL" --json-file=out/dapp.sol.json --dapp-root=. --verbose 1
+LANG=C.UTF-8 hevm dapp-test --match testSpellIsCast_UNIV2DAIETH_INTEGRATION --rpc="$ETH_RPC_URL" --json-file=out/dapp.sol.json --dapp-root=. --verbose 1
