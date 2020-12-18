badeb9978515c7b4688a6cdfa2f8381601baa1fb
Lucas <lucas@makerdao.com>
2020-12-18 12:53:04 -0500

    fix: update description hash/comment URL

--- src/DssSpell.sol
+++ src/DssSpell.sol
@@ -345,7 +345,7 @@ contract DssSpell {
 
     // Provides a descriptive tag for bot consumption
     // This should be modified weekly to provide a summary of the actions
-    // Hash: seth keccak -- "$(wget https://raw.githubusercontent.com/LongForWisdom/community/35801ba163235cce1d9bfcf67098d4af120c6574/governance/votes/Executive%20vote%20-%20December%2018%2C%202020.md -q -O - 2>/dev/null)"
+    // Hash: seth keccak -- "$(wget https://raw.githubusercontent.com/makerdao/community/be0d35d9e457d1f87a4ea06863d3e8db90bff7b9/governance/votes/Executive%20vote%20-%20December%2018%2C%202020.md -q -O - 2>/dev/null)"
     string constant public description =
         "2020-12-18 MakerDAO Executive Spell | Hash: 0x88410d74be25d71344e5c2e2b1855b666e865ac29271778a9e5ff1b8bc931318";
 
