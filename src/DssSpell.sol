b04d001f0d49ccdc7e79036bad36e1e3b42d42d7
Brian McMichael <brian@brianmcmichael.com>
2021-01-14 17:31:24 -0500

    Set oracles

--- src/DssSpell.sol
+++ src/DssSpell.sol
@@ -20,7 +20,11 @@ import "dss-exec-lib/DssAction.sol";
 
 contract SpellAction is DssAction {
 
+    // Provides a descriptive tag for bot consumption
+    // This should be modified weekly to provide a summary of the actions
+    // Hash: seth keccak -- "$(wget https://raw.githubusercontent.com/TODO -q -O - 2>/dev/null)"
     string public constant description = "TODO";
+
 
     // Gnosis
     address constant GNOSIS      = 0xD5885fbCb9a8a8244746010a3BC6F1C6e0269777;
@@ -28,11 +32,21 @@ contract SpellAction is DssAction {
     address constant BTCUSD_OSM  = 0xf185d0682d50819263941e5f4EacC763CC5C6C42;
     address constant LINKUSD_OSM = 0x9B0C694C6939b5EA9584e9b61C7815E8d97D9cC7;
     address constant COMPUSD_OSM = 0xBED0879953E633135a48a157718Aa791AC0108E4;
-    address constant YFIUSD_OSM = 0x5F122465bCf86F45922036970Be6DD7F58820214;
-    address constant ZRXUSD_OSM = 0x7382c066801E7Acb2299aC8562847B9883f5CD3c;
+    address constant YFIUSD_OSM  = 0x5F122465bCf86F45922036970Be6DD7F58820214;
+    address constant ZRXUSD_OSM  = 0x7382c066801E7Acb2299aC8562847B9883f5CD3c;
 
-    //    AAVEUSD MED = 0xe62872DFEbd323b03D27946f8e2491B454a69811
-    //Whitelist Addr = 0x8b1C079f8192706532cC0Bf0C02dcC4fF40d045D
+    // SET
+    address constant SET_AAVE    = 0x8b1C079f8192706532cC0Bf0C02dcC4fF40d045D;
+    address constant AAVEUSD_MED = 0xe62872DFEbd323b03D27946f8e2491B454a69811;
+    address constant SET_LRC     = 0x1D5d9a2DDa0843eD9D8a9Bddc33F1fca9f9C64a0;
+    address constant LRCUSD_MED  = 0xcCe92282d9fe310F4c232b0DA9926d5F24611C7B;
+    address constant SET_YFI     = 0x1686d01Bd776a1C2A3cCF1579647cA6D39dd2465;
+    address constant YFIUSD_MED  = 0x89AC26C0aFCB28EC55B6CD2F6b7DAD867Fa24639;
+    address constant SET_ZRX     = 0xFF60D1650696238F81BE53D23b3F91bfAAad938f;
+    address constant ZRXUSD_MED  = 0x956ecD6a9A9A0d84e8eB4e6BaaC09329E202E55e;
+    address constant SET_UNI     = 0x3c3Afa479d8C95CF0E1dF70449Bb5A14A3b7Af67;
+    address constant UNIUSD_MED  = 0x52f761908cC27B4D77AD7A329463cf08baf62153;
+
 
     // Many of the settings that change weekly rely on the rate accumulator
     // described at https://docs.makerdao.com/smart-contract-modules/rates-module
@@ -81,38 +95,29 @@ contract SpellAction is DssAction {
         // Decrease the AAVE-A Stability Fee from 6% to 4%.
         setIlkStabilityFee("AAVE-A", FOUR_PERCENT_RATE);
 
+
         // Increase the System Surplus Buffer - January 11, 2021
         // https://vote.makerdao.com/polling/QmcXtm1d#poll-detail
         setSurplusBuffer(10_000_000);
+
+
+        // Whitelist Gnosis on Multiple Oracles - January 11, 2021
+        // https://vote.makerdao.com/polling/QmNwTMcB#poll-detail
+        // TODO OSM tests
+        addReaderToOSMWhitelist(BTCUSD_OSM,  GNOSIS);
+        addReaderToOSMWhitelist(LINKUSD_OSM, GNOSIS);
+        addReaderToOSMWhitelist(COMPUSD_OSM, GNOSIS);
+        addReaderToOSMWhitelist(YFIUSD_OSM,  GNOSIS);
+        addReaderToOSMWhitelist(ZRXUSD_OSM,  GNOSIS);
+
+
+        // Whitelist Set Protocol on Multiple Oracles - January 11, 2021
+        // https://vote.makerdao.com/polling/QmTctW6i#poll-detail
+        // TODO medianizer tests
+        addReaderToMedianWhitelist(AAVEUSD_MED, SET_AAVE);
+        addReaderToMedianWhitelist(LRCUSD_MED,  SET_LRC);
+        addReaderToMedianWhitelist(YFIUSD_MED,  SET_YFI);
+        addReaderToMedianWhitelist(ZRXUSD_MED,  SET_ZRX);
+        addReaderToMedianWhitelist(UNIUSD_MED,  SET_UNI);
     }
-
-    /* TODO
-    *Whitelist Gnosis: 0xD5885fbCb9a8a8244746010a3BC6F1C6e0269777*
-    BTCUSD OSM = 0xf185d0682d50819263941e5f4EacC763CC5C6C42
-    LINKUSD OSM = 0x9B0C694C6939b5EA9584e9b61C7815E8d97D9cC7
-    COMPUSD OSM = 0xBED0879953E633135a48a157718Aa791AC0108E4
-    YFIUSD OSM = 0x5F122465bCf86F45922036970Be6DD7F58820214
-    ZRXUSD OSM = 0x7382c066801E7Acb2299aC8562847B9883f5CD3c
-
-    *Whitelist Set:*
-    AAVEUSD MED = 0xe62872DFEbd323b03D27946f8e2491B454a69811
-    Whitelist Addr = 0x8b1C079f8192706532cC0Bf0C02dcC4fF40d045D
-
-    LRCUSD MED = 0xcCe92282d9fe310F4c232b0DA9926d5F24611C7B
-    Whitelist Addr = 0x1D5d9a2DDa0843eD9D8a9Bddc33F1fca9f9C64a0
-
-    YFIUSD MED = 0x89AC26C0aFCB28EC55B6CD2F6b7DAD867Fa24639
-    Whitelist Addr = 0x1686d01Bd776a1C2A3cCF1579647cA6D39dd2465
-
-
-    ZRXUSD MED = 0x956ecD6a9A9A0d84e8eB4e6BaaC09329E202E55e
-    Whitelist Addr = 0xFF60D1650696238F81BE53D23b3F91bfAAad938f
-
-    UNIUSD MED = 0x52f761908cc27b4d77ad7a329463cf08baf62153
-    Whitelist Addr = 0x3c3Afa479d8C95CF0E1dF70449Bb5A14A3b7Af67
-    */
-
-
-    // Surplus buffer
-
 }
