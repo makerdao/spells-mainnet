4198fcf0435d9e7e7bb404afaab598d6b1fbd16d
Sam MacPherson <xylom44@gmail.com>
2021-01-18 19:18:12 -0500

    add archive spells

---
+++ archive/2021-01-18-DssSpell.sol
@@ -0,0 +1,157 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (C) 2021 Maker Ecosystem Growth Holdings, INC.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+pragma solidity 0.6.11;
+
+import "lib/dss-interfaces/src/dapp/DSPauseAbstract.sol";
+import "lib/dss-interfaces/src/dss/ChainlogAbstract.sol";
+import "lib/dss-interfaces/src/dss/VatAbstract.sol";
+
+contract SpellAction {
+    // Office hours enabled if true
+    bool constant public officeHours = false;
+
+    // MAINNET ADDRESSES
+    //
+    // The contracts in this list should correspond to MCD core contracts, verify
+    //  against the current release list at:
+    //     https://changelog.makerdao.com/releases/mainnet/active/contracts.json
+    ChainlogAbstract constant CHANGELOG =
+        ChainlogAbstract(0xdA0Ab1e0017DEbCd72Be8599041a2aa3bA7e740F);
+
+    // Ilks
+    bytes32 constant ILK_ETH_A          = "ETH-A";
+    bytes32 constant ILK_WBTC_A         = "WBTC-A";
+
+    // decimals & precision
+    uint256 constant THOUSAND = 10 ** 3;
+    uint256 constant MILLION  = 10 ** 6;
+    uint256 constant WAD      = 10 ** 18;
+    uint256 constant RAY      = 10 ** 27;
+    uint256 constant RAD      = 10 ** 45;
+
+    // Many of the settings that change weekly rely on the rate accumulator
+    // described at https://docs.makerdao.com/smart-contract-modules/rates-module
+    // To check this yourself, use the following rate calculation (example 8%):
+    //
+    // $ bc -l <<< 'scale=27; e( l(1.08)/(60 * 60 * 24 * 365) )'
+    //
+    // A table of rates can be found at
+    //    https://ipfs.io/ipfs/QmefQMseb3AiTapiAKKexdKHig8wroKuZbmLtPLv4u2YwW
+    //
+
+    modifier limited {
+        if (officeHours) {
+            uint day = (block.timestamp / 1 days + 3) % 7;
+            require(day < 5, "Can only be cast on a weekday");
+            uint hour = block.timestamp / 1 hours % 24;
+            require(hour >= 14 && hour < 21, "Outside office hours");
+        }
+        _;
+    }
+
+    function execute() external limited {
+        address MCD_VAT      = CHANGELOG.getAddress("MCD_VAT");
+
+        // Set the global debt ceiling
+        // + 260 M for ETH-A
+        // + 50 M for WBTC-A
+        VatAbstract(MCD_VAT).file("Line",
+            VatAbstract(MCD_VAT).Line()
+            + 260 * MILLION * RAD
+            + 50 * MILLION * RAD
+        );
+
+        //
+        // Update the Debt Ceilings
+        //
+        VatAbstract(MCD_VAT).file(ILK_ETH_A, "line", 1000 * MILLION * RAD);
+        VatAbstract(MCD_VAT).file(ILK_WBTC_A, "line", 210 * MILLION * RAD);
+    }
+}
+
+contract DssSpell {
+    ChainlogAbstract constant CHANGELOG =
+        ChainlogAbstract(0xdA0Ab1e0017DEbCd72Be8599041a2aa3bA7e740F);
+
+    DSPauseAbstract immutable public pause;
+    address         immutable public action;
+    bytes32         immutable public tag;
+    uint256         immutable public expiration;
+    uint256         public eta;
+    bytes           public sig;
+    bool            public done;
+
+    // Provides a descriptive tag for bot consumption
+    // This should be modified weekly to provide a summary of the actions
+    // Hash: seth keccak -- "$(wget https://raw.githubusercontent.com/makerdao/community/7545776ce23113335369331dff39d71d9e767a19/governance/votes/Executive%20vote%20-%20January%2018%2C%202021.md -q -O - 2>/dev/null)"
+    string constant public description =
+        "2021-01-18 MakerDAO Executive Spell | Hash: 0x71eb4d0a0f678bc2d706033c2ad238e637fb7665521040a31a193ad27d89183c";
+
+    function officeHours() external view returns (bool) {
+        return SpellAction(action).officeHours();
+    }
+
+    constructor() public {
+        pause = DSPauseAbstract(CHANGELOG.getAddress("MCD_PAUSE"));
+        sig = abi.encodeWithSignature("execute()");
+        bytes32 _tag;
+        address _action = action = address(new SpellAction());
+        assembly { _tag := extcodehash(_action) }
+        tag = _tag;
+        expiration = block.timestamp + 30 days;
+    }
+
+    function nextCastTime() external view returns (uint256 castTime) {
+        require(eta != 0, "DSSSpell/spell-not-scheduled");
+        castTime = block.timestamp > eta ? block.timestamp : eta; // Any day at XX:YY
+
+        if (SpellAction(action).officeHours()) {
+            uint256 day    = (castTime / 1 days + 3) % 7;
+            uint256 hour   = castTime / 1 hours % 24;
+            uint256 minute = castTime / 1 minutes % 60;
+            uint256 second = castTime % 60;
+
+            if (day >= 5) {
+                castTime += (6 - day) * 1 days;                 // Go to Sunday XX:YY
+                castTime += (24 - hour + 14) * 1 hours;         // Go to 14:YY UTC Monday
+                castTime -= minute * 1 minutes + second;        // Go to 14:00 UTC
+            } else {
+                if (hour >= 21) {
+                    if (day == 4) castTime += 2 days;           // If Friday, fast forward to Sunday XX:YY
+                    castTime += (24 - hour + 14) * 1 hours;     // Go to 14:YY UTC next day
+                    castTime -= minute * 1 minutes + second;    // Go to 14:00 UTC
+                } else if (hour < 14) {
+                    castTime += (14 - hour) * 1 hours;          // Go to 14:YY UTC same day
+                    castTime -= minute * 1 minutes + second;    // Go to 14:00 UTC
+                }
+            }
+        }
+    }
+
+    function schedule() external {
+        require(block.timestamp <= expiration, "DSSSpell/spell-has-expired");
+        require(eta == 0, "DSSSpell/spell-already-scheduled");
+        eta = block.timestamp + DSPauseAbstract(pause).delay();
+        pause.plot(action, tag, sig, eta);
+    }
+
+    function cast() external {
+        require(!done, "DSSSpell/spell-already-cast");
+        done = true;
+        pause.exec(action, tag, sig, eta);
+    }
+}
