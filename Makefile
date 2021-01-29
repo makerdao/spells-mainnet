52c7c082707fe9f3c02378f97e9187c02169a201
Brian L. McMichael <brian@brianmcmichael.com>
2021-01-29 16:12:24 -0500

    SC-8758: Weekly Exec January 29, 2021 (#137)

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
 all     :; DAPP_SOLC_OPTIMIZE=true DAPP_SOLC_OPTIMIZE_RUNS=1 SOLC_FLAGS="--optimize --optimize-runs=1" \
                dapp --use solc:0.6.11 build
 clean   :; dapp clean
-test    :; ./test-dssspell.sh
+           # Usage example: make test match=SpellIsCast
+test    :; ./test-dssspell.sh $(match)
 deploy  :; DAPP_SOLC_OPTIMIZE=true DAPP_SOLC_OPTIMIZE_RUNS=1 SOLC_FLAGS="--optimize --optimize-runs=1" \
     dapp --use solc:0.6.11 build && \
     dapp create DssSpell
--- test-dssspell.sh
+++ test-dssspell.sh
@@ -8,4 +8,8 @@ SOLC_FLAGS="--optimize --optimize-runs 1" dapp --use solc:0.6.11 build
 export DAPP_TEST_TIMESTAMP=$(seth block latest timestamp)
 export DAPP_TEST_NUMBER=$(seth block latest number)
 
-LANG=C.UTF-8 hevm dapp-test --rpc="$ETH_RPC_URL" --json-file=out/dapp.sol.json --dapp-root=. --verbose 1
+if [[ -z "$1" ]]; then
+  LANG=C.UTF-8 hevm dapp-test --rpc="$ETH_RPC_URL" --json-file=out/dapp.sol.json --dapp-root=. --verbose 1
+else
+  LANG=C.UTF-8 hevm dapp-test --rpc="$ETH_RPC_URL" --json-file=out/dapp.sol.json --dapp-root=. --match "$1" --verbose 1
+fi
