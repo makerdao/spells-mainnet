a228d4d03b48aeda0cd692edf04f3c4377b54efa
Sam MacPherson <xylom44@gmail.com>
2021-02-11 10:44:11 -0500

    use rpc-url and verbose

--- test-dssspell.sh
+++ test-dssspell.sh
@@ -4,7 +4,7 @@ set -e
 [[ "$ETH_RPC_URL" && "$(seth chain)" == "ethlive"  ]] || { echo "Please set a mainnet ETH_RPC_URL"; exit 1;  }
 
 if [[ -z "$1" ]]; then
-  dapp --use solc:0.6.11 test --rpc
+  dapp --use solc:0.6.11 test --rpc-url="$ETH_RPC_URL" -v
 else
-  dapp --use solc:0.6.11 test --rpc --match "$1"
+  dapp --use solc:0.6.11 test --rpc-url="$ETH_RPC_URL" --match "$1" -v
 fi
