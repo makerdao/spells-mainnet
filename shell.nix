ab59fd5e7491b8ed0a39d45685eadceac5b01428
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2021-02-12 12:48:44 -0300

    Use temp dapptools version

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 { url
   , dappPkgs ? (
-    import (fetchTarball "https://github.com/makerdao/makerpkgs/tarball/master") {}
-  ).dappPkgsVersions.hevm-0_43_1
+    import (fetchTarball "https://github.com/makerdao/makerpkgs/tarball/temp-fix-json") {}
+  ).dappPkgsVersions.temp-fix-json
 }: with dappPkgs;
 
 mkShell {
