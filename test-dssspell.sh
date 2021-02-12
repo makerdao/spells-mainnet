521d37985ff5e4dba1bdd9a1270ea63233514fab
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2021-02-12 14:00:03 -0300

    Use verbose=3 when match is being pass to the script

--- test-dssspell.sh
+++ test-dssspell.sh
@@ -9,5 +9,5 @@ export DAPP_LINK_TEST_LIBRARIES=0
 if [[ -z "$1" ]]; then
   dapp --use solc:0.6.11 test --rpc-url="$ETH_RPC_URL" -v
 else
-  dapp --use solc:0.6.11 test --rpc-url="$ETH_RPC_URL" --match "$1" -v
+  dapp --use solc:0.6.11 test --rpc-url="$ETH_RPC_URL" --match "$1" -vvv
 fi
