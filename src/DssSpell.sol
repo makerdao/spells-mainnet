337af320086aae5cc2b63933f7de69a406ea7d90
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2021-03-26 12:29:35 -0300

    Update license headers

--- src/DssSpell.sol
+++ src/DssSpell.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: AGPL-3.0-or-later
 // Copyright (C) 2021 Maker Ecosystem Growth Holdings, INC.
 //
 // This program is free software: you can redistribute it and/or modify
@@ -13,6 +13,7 @@
 //
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 pragma solidity 0.6.12;
 
 import "dss-exec-lib/DssExec.sol";
