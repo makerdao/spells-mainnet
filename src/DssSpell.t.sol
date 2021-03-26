c1ade83b4020219eed4e21cd35f4f3de90d2b39d
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2021-03-26 12:37:34 -0300

    Test with deployed spell

--- src/DssSpell.t.sol
+++ src/DssSpell.t.sol
@@ -193,10 +193,10 @@ contract DssSpellTest is DSTest, DSMath {
         // Test for spell-specific parameters
         //
         spellValues = SpellValues({
-            deployed_spell:                 address(0),        // populate with deployed spell if deployed
-            deployed_spell_created:         1616173084,                 // use get-created-timestamp.sh if deployed
+            deployed_spell:                 address(0x0CF9B10FE87dC44B0307b954074faAC7e756d6Fa),        // populate with deployed spell if deployed
+            deployed_spell_created:         1616771470,                 // use get-created-timestamp.sh if deployed
             previous_spell:                 address(0),        // supply if there is a need to test prior to its cast() function being called on-chain.
-            previous_spell_execution_time:  1616421600,                 // Time to warp to in order to allow the previous spell to be cast ignored if PREV_SPELL is SpellLike(address(0)).
+            previous_spell_execution_time:  0,                 // Time to warp to in order to allow the previous spell to be cast ignored if PREV_SPELL is SpellLike(address(0)).
             office_hours_enabled:           false,              // true if officehours is expected to be enabled in the spell
             expiration_threshold:           weekly_expiration  // (weekly_expiration,monthly_expiration) if weekly or monthly spell
         });
