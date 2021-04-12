04b3ba84fad5f692cd78f92bafa59b3f8cb8767d
Brian McMichael <brian@brianmcmichael.com>
2021-04-12 13:04:33 -0400

    Add deployed spell

--- src/DssSpell.t.sol
+++ src/DssSpell.t.sol
@@ -405,8 +405,8 @@ contract DssSpellTest is DSTest, DSMath {
         // Test for spell-specific parameters
         //
         spellValues = SpellValues({
-            deployed_spell:                 address(0),        // populate with deployed spell if deployed
-            deployed_spell_created:         1618236000,        // use get-created-timestamp.sh if deployed
+            deployed_spell:                 address(0x637F98654cC147C263ae740455d350D0339b19F5),        // populate with deployed spell if deployed
+            deployed_spell_created:         1618246761,        // use get-created-timestamp.sh if deployed
             previous_spell:                 address(0),        // supply if there is a need to test prior to its cast() function being called on-chain.
             previous_spell_execution_time:  1617926460 + 2 days,        // Time to warp to in order to allow the previous spell to be cast ignored if PREV_SPELL is SpellLike(address(0)).
             office_hours_enabled:           true,              // true if officehours is expected to be enabled in the spell
