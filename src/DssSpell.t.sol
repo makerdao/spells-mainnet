839b9641cdc8909fd3838a9c64bc06e3df95bb27
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2021-05-14 13:40:55 -0300

    Add prev spell and remove actual one

--- src/DssSpell.t.sol
+++ src/DssSpell.t.sol
@@ -219,9 +219,9 @@ contract DssSpellTest is DSTest, DSMath {
         // Test for spell-specific parameters
         //
         spellValues = SpellValues({
-            deployed_spell:                 address(0xc5Ed356eadd9863eF78F74155fa4bEE1fc4ad879),        // populate with deployed spell if deployed
+            deployed_spell:                 address(0),        // populate with deployed spell if deployed
             deployed_spell_created:         1620401989,        // use get-created-timestamp.sh if deployed
-            previous_spell:                 address(0x3Baab03C225eB9033FC6B73C16D310D56Afa922F),        // supply if there is a need to test prior to its cast() function being called on-chain.
+            previous_spell:                 address(0xc5Ed356eadd9863eF78F74155fa4bEE1fc4ad879),        // supply if there is a need to test prior to its cast() function being called on-chain.
             office_hours_enabled:           true,              // true if officehours is expected to be enabled in the spell
             expiration_threshold:           weekly_expiration  // (weekly_expiration,monthly_expiration) if weekly or monthly spell
         });
