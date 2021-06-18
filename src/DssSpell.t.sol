0b13b9230713a1dda6fac8a0b67be69666f3e55f
Julien Martin <julienmartinlevrai@protonmail.ch>
2021-06-18 14:56:50 -0400

    test: deployed spell address & timestamp

--- src/DssSpell.t.sol
+++ src/DssSpell.t.sol
@@ -239,8 +239,8 @@ contract DssSpellTest is DSTest, DSMath {
         // Test for spell-specific parameters
         //
         spellValues = SpellValues({
-            deployed_spell:                 address(0),        // populate with deployed spell if deployed
-            deployed_spell_created:         1623430271,        // use get-created-timestamp.sh if deployed
+            deployed_spell:                 address(0xd498E7DEE467d1eb6Ed3933e579c427E168b6E6c),        // populate with deployed spell if deployed
+            deployed_spell_created:         1624042087,        // use get-created-timestamp.sh if deployed
             previous_spell:                 address(0x1dB8b702416D3174dC2F5b12287383AB693f894f),        // supply if there is a need to test prior to its cast() function being called on-chain.
             office_hours_enabled:           false,              // true if officehours is expected to be enabled in the spell
             expiration_threshold:           weekly_expiration  // (weekly_expiration,monthly_expiration) if weekly or monthly spell
