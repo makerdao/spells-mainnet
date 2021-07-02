4638e62ce9fc412738dd677d5ebc4b661e40b0e4
Brian McMichael <brian@brianmcmichael.com>
2021-07-02 13:05:39 -0400

    Deployed spell

--- src/DssSpell.t.sol
+++ src/DssSpell.t.sol
@@ -315,8 +315,8 @@ contract DssSpellTest is DSTest, DSMath {
         // Test for spell-specific parameters
         //
         spellValues = SpellValues({
-            deployed_spell:                 address(0),        // populate with deployed spell if deployed
-            deployed_spell_created:         1624042087,        // use get-created-timestamp.sh if deployed
+            deployed_spell:                 address(0xEC782b5aC1f0Fc096Ad30950f3348670980f7FD3),        // populate with deployed spell if deployed
+            deployed_spell_created:         1625244610,        // use get-created-timestamp.sh if deployed
             previous_spell:                 address(0xd498E7DEE467d1eb6Ed3933e579c427E168b6E6c),        // supply if there is a need to test prior to its cast() function being called on-chain.
             office_hours_enabled:           false,              // true if officehours is expected to be enabled in the spell
             expiration_threshold:           weekly_expiration  // (weekly_expiration,monthly_expiration) if weekly or monthly spell
