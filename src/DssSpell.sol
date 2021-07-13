b17531f035f32ab530f355a31c68de6eaa813262
Jeroen Offerijns <jeroen@offerijns.com>
2021-07-13 14:25:25 +0200

    feat: put back old spell, separate rwa spell

--- src/DssSpell.sol
+++ src/DssSpell.sol
@@ -18,33 +18,42 @@ pragma solidity 0.6.12;
 
 import "dss-exec-lib/DssExec.sol";
 import "dss-exec-lib/DssAction.sol";
-import "lib/dss-interfaces/src/dss/GemJoinAbstract.sol";
-import "lib/dss-interfaces/src/dapp/DSTokenAbstract.sol";
-
-interface Initializable {
-    function init(bytes32) external;
-}
-
-interface Hopeable {
-    function hope(address) external;
-}
-
-interface Kissable {
-    function kiss(address) external;
-}
-
-interface RwaLiquidationLike {
-    function ilks(bytes32) external returns (string memory,address,uint48,uint48);
-    function init(bytes32, uint256, string calldata, uint48) external;
-}
 
 contract DssSpellAction is DssAction {
 
     // Provides a descriptive tag for bot consumption
     // This should be modified weekly to provide a summary of the actions
-    // Hash: seth keccak -- "$(wget https://raw.githubusercontent.com/makerdao/community/2a7a8c915695b7298fe725ee3dc6c613fa9d9bbe/governance/votes/Executive%20vote%20-%20April%2012%2C%202021.md -q -O - 2>/dev/null)"
+    // Hash: seth keccak -- "$(wget https://raw.githubusercontent.com/makerdao/community/713afa54a66e928426cfda0fb482b03ecf291d25/governance/votes/Executive%20vote%20-%20July%202%2C%202021.md -q -O - 2> /dev/null)"
     string public constant description =
-        "";
+        "2021-07-02 MakerDAO Executive Spell | Hash: 0x601096005a6c4241eac0272a7bab57f4fd966d0fb94ec72a6cb17e4123e3bd86";
+
+    // Turn off office hours
+    function officeHours() public override returns (bool) {
+        return false;
+    }
+
+    uint256 constant WAD = 10**18;
+    uint256 constant RAY = 10**27;
+    uint256 constant RAD = 10**45;
+
+    bytes32 constant ILK_PSM_USDC_A     = "PSM-USDC-A";
+
+    // Growth Core Unit
+    address constant GRO_MULTISIG        = 0x7800C137A645c07132886539217ce192b9F0528e;
+    // Content Production Core Unit
+    address constant MKT_MULTISIG        = 0xDCAF2C84e1154c8DdD3203880e5db965bfF09B60;
+    // GovAlpha Core Unit
+    address constant GOV_MULTISIG        = 0x01D26f8c5cC009868A4BF66E268c17B057fF7A73;
+    // Real-World Finance Core Unit
+    address constant RWF_MULTISIG        = 0x9e1585d9CA64243CE43D42f7dD7333190F66Ca09;
+    // Risk Core Unit
+    address constant RISK_CU_EOA         = 0xd98ef20520048a35EdA9A202137847A62120d2d9;
+    // Protocol Engineering Multisig
+    address constant PE_MULTISIG         = 0xe2c16c308b843eD02B09156388Cb240cEd58C01c;
+    // Oracles Core Unit (Operating)
+    address constant ORA_MULTISIG        = 0x2d09B7b95f3F312ba6dDfB77bA6971786c5b50Cf;
+    // Oracles Core Unit (Emergency Fund)
+    address constant ORA_ER_MULTISIG     = 0x53CCAA8E3beF14254041500aCC3f1D4edb5B6D24;
 
     // Many of the settings that change weekly rely on the rate accumulator
     // described at https://docs.makerdao.com/smart-contract-modules/rates-module
@@ -55,155 +64,50 @@ contract DssSpellAction is DssAction {
     // A table of rates can be found at
     //    https://ipfs.io/ipfs/QmefQMseb3AiTapiAKKexdKHig8wroKuZbmLtPLv4u2YwW
     //
-    uint256 constant SIX_PCT            = 1000000001847694957439350562;
-    uint256 constant SEVEN_PCT          = 1000000002145441671308778766;
-    uint256 constant FOUR_PT_FIVE_PCT   = 1000000001395766281313196627;
-    uint256 constant TWO_PCT            = 1000000000627937192491029810;
-
-    // Math
-    uint256 constant MILLION = 10**6;
-    uint256 constant WAD     = 10**18;
-    uint256 constant RAD     = 10**45;
-
-    // Addresses
-    address constant RWA003_OPERATOR         = 0xaf1f5F5a203Aaf08237013A7280B03F35147b9D2;
-    address constant RWA003_GEM              = address(0);
-    address constant MCD_JOIN_RWA003_A       = address(0);
-    address constant RWA003_A_URN            = address(0);
-    address constant RWA003_A_INPUT_CONDUIT  = 0xaf1f5F5a203Aaf08237013A7280B03F35147b9D2;
-    address constant RWA003_A_OUTPUT_CONDUIT = 0xaf1f5F5a203Aaf08237013A7280B03F35147b9D2;
-
-    address constant RWA004_OPERATOR         = 0x5Ad81Ed0b281fDB732d30E496Ac1ba3A7D760c1E;
-    address constant RWA004_GEM              = address(0);
-    address constant MCD_JOIN_RWA004_A       = address(0);
-    address constant RWA004_A_URN            = address(0);
-    address constant RWA004_A_INPUT_CONDUIT  = 0x5Ad81Ed0b281fDB732d30E496Ac1ba3A7D760c1E;
-    address constant RWA004_A_OUTPUT_CONDUIT = 0x5Ad81Ed0b281fDB732d30E496Ac1ba3A7D760c1E;
-
-    address constant RWA005_OPERATOR         = 0xaC7F683F048120Aa35a18F687E5eE7446380A3bE;
-    address constant RWA005_GEM              = address(0);
-    address constant MCD_JOIN_RWA005_A       = address(0);
-    address constant RWA005_A_URN            = address(0);
-    address constant RWA005_A_INPUT_CONDUIT  = 0xaC7F683F048120Aa35a18F687E5eE7446380A3bE;
-    address constant RWA005_A_OUTPUT_CONDUIT = 0xaC7F683F048120Aa35a18F687E5eE7446380A3bE;
-
-    address constant RWA006_OPERATOR         = address(0);
-    address constant RWA006_GEM              = address(0);
-    address constant MCD_JOIN_RWA006_A       = address(0);
-    address constant RWA006_A_URN            = address(0);
-    address constant RWA006_A_INPUT_CONDUIT  = address(0);
-    address constant RWA006_A_OUTPUT_CONDUIT = address(0);
+    uint256 constant ZERO_PCT =            1000000000000000000000000000;
+    uint256 constant ZERO_POINT_FIVE_PCT = 1000000000158153903837946257;
+    uint256 constant ONE_PCT =             1000000000315522921573372069;
+    uint256 constant ONE_POINT_FIVE_PCT =  1000000000472114805215157978;
+    uint256 constant TWO_PCT =             1000000000627937192491029810;
+    uint256 constant SIX_PCT =             1000000001847694957439350562;
 
     function actions() public override {
-        address MIP21_LIQUIDATION_ORACLE =
-            DssExecLib.getChangelogAddress("MIP21_LIQUIDATION_ORACLE");
 
-        address vat = DssExecLib.vat();
+        // ----------- Auto-Line updates -----------
+        // https://vote.makerdao.com/polling/QmZz4ssm?network=mainnet#poll-detail
+        DssExecLib.setIlkAutoLineParameters(ILK_PSM_USDC_A, 10_000_000_000, 1_000_000_000, 24 hours);
 
-        // -------------------------------- RWA003 --------------------------------
-        bytes32 RWA003_ilk   = "RWA003-A";
-        uint256 RWA003_CEIL  = 2 * MILLION;
-        uint256 RWA003_PRICE = 2_247_200 * WAD;
-        uint256 RWA003_MAT   = 10_500;
-        uint48 RWA003_TAU    = 0;
+        // ----------- Stability Fee updates -----------
+        // https://vote.makerdao.com/polling/QmfZWY87?network=mainnet#poll-detail
+        DssExecLib.setIlkStabilityFee("ETH-A", TWO_PCT, true);
+        DssExecLib.setIlkStabilityFee("ETH-B", SIX_PCT, true);
+        DssExecLib.setIlkStabilityFee("ETH-C", ZERO_POINT_FIVE_PCT, true);
+        DssExecLib.setIlkStabilityFee("WBTC-A", TWO_PCT, true);
+        DssExecLib.setIlkStabilityFee("LINK-A", ONE_PCT, true);
+        DssExecLib.setIlkStabilityFee("YFI-A", ONE_PCT, true);
+        DssExecLib.setIlkStabilityFee("UNI-A", ONE_PCT, true);
+        DssExecLib.setIlkStabilityFee("AAVE-A", ONE_PCT, true);
+        DssExecLib.setIlkStabilityFee("RENBTC-A", TWO_PCT, true);
+        DssExecLib.setIlkStabilityFee("COMP-A", ONE_PCT, true);
+        DssExecLib.setIlkStabilityFee("BAL-A", ONE_PCT, true);
+        DssExecLib.setIlkStabilityFee("UNIV2DAIETH-A", ONE_POINT_FIVE_PCT, true);
+        DssExecLib.setIlkStabilityFee("UNIV2USDCETH-A", TWO_PCT, true);
+        DssExecLib.setIlkStabilityFee("UNIV2DAIUSDC-A", ZERO_PCT, true);
+        DssExecLib.setIlkStabilityFee("UNIV2WBTCETH-A", TWO_PCT, true);
+        DssExecLib.setIlkStabilityFee("UNIV2UNIETH-A", TWO_PCT, true);
+        DssExecLib.setIlkStabilityFee("UNIV2ETHUSDT-A", TWO_PCT, true);
 
-        // https://ipfs.io/ipfs/
-        string memory RWA003_DOC = "";
-
-        // Sanity checks
-        require(GemJoinAbstract(MCD_JOIN_RWA003_A).vat() == vat, "join-vat-not-match");
-        require(GemJoinAbstract(MCD_JOIN_RWA003_A).ilk() == RWA003_ilk, "join-ilk-not-match");
-        require(GemJoinAbstract(MCD_JOIN_RWA003_A).gem() == RWA003_GEM, "join-gem-not-match");
-        require(GemJoinAbstract(MCD_JOIN_RWA003_A).dec() == DSTokenAbstract(RWA003_GEM).decimals(), "join-dec-not-match");
-
-        RwaLiquidationLike(MIP21_LIQUIDATION_ORACLE).init(
-            RWA003_ilk, RWA003_PRICE, RWA003_DOC, RWA003_TAU
-        );
-        (,address RWA003_pip,,) = RwaLiquidationLike(MIP21_LIQUIDATION_ORACLE).ilks(RWA003_ilk);
-
-        // Set price feed for RWA003
-        DssExecLib.setContract(DssExecLib.spotter(), RWA003_ilk, "pip", RWA003_pip);
-
-        // Init RWA-003 in Vat
-        Initializable(vat).init(RWA003_ilk);
-        // Init RWA-003 in Jug
-        Initializable(DssExecLib.jug()).init(RWA003_ilk);
-
-        // Allow RWA-003 Join to modify Vat registry
-        DssExecLib.authorize(vat, MCD_JOIN_RWA003_A);
-
-        // Allow RwaLiquidationOracle to modify Vat registry
-        // DssExecLib.authorize(vat, MIP21_LIQUIDATION_ORACLE);
-
-        // Increase the global debt ceiling by the ilk ceiling
-        DssExecLib.increaseGlobalDebtCeiling(RWA003_CEIL);
-        // Set the ilk debt ceiling
-        DssExecLib.setIlkDebtCeiling(RWA003_ilk, RWA003_CEIL);
-
-        // No dust
-        // DssExecLib.setIlkMinVaultAmount(RWA003_ilk, 0);
-
-        // stability fee
-        DssExecLib.setIlkStabilityFee(RWA003_ilk, THREE_PT_FIVE_PCT, false);
-
-        // collateralization ratio
-        DssExecLib.setIlkLiquidationRatio(RWA003_ilk, RWA003_MAT);
-
-        // poke the spotter to pull in a price
-        DssExecLib.updateCollateralPrice(RWA003_ilk);
-
-        // give the urn permissions on the join adapter
-        // DssExecLib.authorize(MCD_JOIN_RWA003_A, RWA003_A_URN);
-
-        // set up the urn
-        Hopeable(RWA003_A_URN).hope(RWA003_OPERATOR);
-
-        // set up output conduit
-        // Hopeable(RWA003_A_OUTPUT_CONDUIT).hope(RWA003_OPERATOR);
-
-        // Authorize the SC Domain team deployer address on the output conduit
-        // during introductory phase. This allows the SC team to assist in the
-        // testing of a complete circuit. Once a broker dealer arrangement is
-        // established the deployer address should be `deny`ed on the conduit.
-        // Kissable(RWA003_A_OUTPUT_CONDUIT).kiss(SC_DOMAIN_DEPLOYER_07);
-
-        // add RWA-003 contract to the changelog
-        DssExecLib.setChangelogAddress("RWA003", RWA003_GEM);
-        DssExecLib.setChangelogAddress("PIP_RWA003", pip);
-        DssExecLib.setChangelogAddress("MCD_JOIN_RWA003_A", MCD_JOIN_RWA003_A);
-        DssExecLib.setChangelogAddress("RWA003_A_URN", RWA003_A_URN);
-        DssExecLib.setChangelogAddress(
-            "RWA003_A_INPUT_CONDUIT", RWA003_A_INPUT_CONDUIT
-        );
-        DssExecLib.setChangelogAddress(
-            "RWA003_A_OUTPUT_CONDUIT", RWA003_A_OUTPUT_CONDUIT
-        );
-
-        // -------------------------------- RWA004 --------------------------------
-        bytes32 RWA004_ilk   = "RWA004-A";
-        uint256 RWA004_CEIL  = 7 * MILLION;
-        uint256 RWA004_PRICE = 8_014_300 * WAD;
-        uint256 RWA004_MAT   = 11_000;
-        uint48 RWA004_TAU    = 0;
-
-
-        // -------------------------------- RWA005 --------------------------------
-        bytes32 RWA005_ilk   = "RWA005-A";
-        uint256 RWA005_CEIL  = 15 * MILLION;
-        uint256 RWA005_PRICE = 16_380_375 * WAD;
-        uint256 RWA005_MAT   = 10_500;
-        uint48 RWA005_TAU    = 0;
-
-
-        // -------------------------------- RWA006 --------------------------------
-        bytes32 RWA006_ilk   = "RWA006-A";
-        uint256 RWA006_CEIL  = 20 * MILLION;
-        uint256 RWA006_PRICE = 20_808_000 * WAD;
-        uint256 RWA006_MAT   = 10_000;
-        uint48 RWA006_TAU    = 0;
-
-        // bump changelog version
-        DssExecLib.setChangelogVersion("");
+        // Core Unit Budget Distributions - July
+        DssExecLib.sendPaymentFromSurplusBuffer(GRO_MULTISIG,    126_117);
+        DssExecLib.sendPaymentFromSurplusBuffer(MKT_MULTISIG,     44_375);
+        DssExecLib.sendPaymentFromSurplusBuffer(GOV_MULTISIG,    273_334);
+        DssExecLib.sendPaymentFromSurplusBuffer(RWF_MULTISIG,    155_000);
+        DssExecLib.sendPaymentFromSurplusBuffer(RISK_CU_EOA,     182_000);
+        DssExecLib.sendPaymentFromSurplusBuffer(PE_MULTISIG,     510_000);
+        DssExecLib.sendPaymentFromSurplusBuffer(ORA_MULTISIG,    419_677);
+        DssExecLib.sendPaymentFromSurplusBuffer(ORA_ER_MULTISIG, 800_000);
+        //                                                     _________
+        //                                         TOTAL DAI:  2,510,503
     }
 }
 
