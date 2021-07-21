eb4954739f392385791fb8b33e1c6a7ed45753bf
Jeroen Offerijns <jeroen@offerijns.com>
2021-07-21 20:37:58 +0200

    fix: upgrade dss-exex-lib address

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all     :; DAPP_LIBRARIES=' lib/dss-exec-lib/src/DssExecLib.sol:DssExecLib:0xc061EDFC9C38912E0ad4F3922aA84ee11FB03347' \
+all     :; DAPP_LIBRARIES=' lib/dss-exec-lib/src/DssExecLib.sol:DssExecLib:0x3644A28AA8204d09A1A0E423F7aC2ACaFf5b8bb3' \
            DAPP_BUILD_OPTIMIZE=1 DAPP_BUILD_OPTIMIZE_RUNS=1 \
            dapp --use solc:0.6.12 build
 clean   :; dapp clean
--- test-dssspell.sh
+++ test-dssspell.sh
@@ -5,11 +5,11 @@ set -e
 
 export DAPP_BUILD_OPTIMIZE=1
 export DAPP_BUILD_OPTIMIZE_RUNS=1
-export DAPP_LIBRARIES=' lib/dss-exec-lib/src/DssExecLib.sol:DssExecLib:0xc061EDFC9C38912E0ad4F3922aA84ee11FB03347'
+export DAPP_LIBRARIES=' lib/dss-exec-lib/src/DssExecLib.sol:DssExecLib:0x3644A28AA8204d09A1A0E423F7aC2ACaFf5b8bb3'
 export DAPP_LINK_TEST_LIBRARIES=0
 
 if [[ -z "$1" ]]; then
   dapp --use solc:0.6.12 test --rpc-url="$ETH_RPC_URL" -v
 else
-  dapp --use solc:0.6.12 test --rpc-url="$ETH_RPC_URL" --match "$1" -vv
+  dapp --use solc:0.6.12 test --rpc-url="$ETH_RPC_URL" --match "$1" -v --cache ./.cache
 fi
