6a2008a720a747e257ce0e9616ac04e513ad4895
Jeroen Offerijns <jeroen@offerijns.com>
2021-07-21 20:40:57 +0200

    fix: undo

--- test-dssspell.sh
+++ test-dssspell.sh
@@ -11,5 +11,5 @@ export DAPP_LINK_TEST_LIBRARIES=0
 if [[ -z "$1" ]]; then
   dapp --use solc:0.6.12 test --rpc-url="$ETH_RPC_URL" -v
 else
-  dapp --use solc:0.6.12 test --rpc-url="$ETH_RPC_URL" --match "$1" -v --cache ./.cache
+  dapp --use solc:0.6.12 test --rpc-url="$ETH_RPC_URL" --match "$1" -vv
 fi
