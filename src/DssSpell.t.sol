35261ca1dda99cf128be53c758476d3055f3912e
Jeroen Offerijns <Offerijns@users.noreply.github.com>
2021-07-23 19:18:18 +0200

    Update src/DssSpell.t.sol
    Co-authored-by: talbaneth <talbaneth@gmail.com>

--- src/DssSpell.t.sol
+++ src/DssSpell.t.sol
@@ -241,7 +241,7 @@ contract DssSpellTest is DSTest, DSMath {
             deployed_spell:                 address(0),        // populate with deployed spell if deployed
             deployed_spell_created:         1626524287,        // use get-created-timestamp.sh if deployed
             previous_spell:                 address(0x9e32B8317733AF825e25a235C99623B4103eb0b7), // supply if there is a need to test prior to its cast() function being called on-chain.
-            office_hours_enabled:           true,              // true if officehours is expected to be enabled in the spell
+            office_hours_enabled:           false,              // true if officehours is expected to be enabled in the spell
             expiration_threshold:           weekly_expiration  // (weekly_expiration,monthly_expiration) if weekly or monthly spell
         });
         spellValues.deployed_spell_created = spellValues.deployed_spell != address(0) ? spellValues.deployed_spell_created : block.timestamp;
