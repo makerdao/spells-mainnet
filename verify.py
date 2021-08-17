eeaf037f96684f700d3597f9cba46f2bd7199ad8
Emilio Silva Schlenker <e18r@disroot.org>
2021-08-17 09:40:41 -0500

    repeat function filtering as many times as necessary

--- verify.py
+++ verify.py
@@ -188,7 +188,11 @@ for signature, block in libraries.items():
     external_code = code.replace(block, '')
     library_name = re.sub('library ', '', signature).strip()
     no_comments = remove_comments(block)
-    selected = select(library_name, no_comments, external_code)
+    selected_pre = no_comments
+    selected_post = select(library_name, selected_pre, external_code)
+    while len(selected_post) < len(selected_pre):
+        selected_pre = selected_post
+        selected_post = select(library_name, selected_pre, external_code)
     stubs = get_stubs(selected)
     new_block = get_warning(library_name) + stubs
     code = code.replace(block, new_block)
