3f031b85770756acc39a855443cd037d6a2f8697
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2021-09-03 11:27:25 -0300

    Change copyright to Dai Foundation

--- src/DssSpell.sol
+++ src/DssSpell.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-// Copyright (C) 2021 Maker Ecosystem Growth Holdings, INC.
+
+// Copyright (C) 2021 Dai Foundation
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
