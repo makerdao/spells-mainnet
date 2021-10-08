d6b984c8709e61999fcd006d0adbff2135f56334
Olivier van den Biggelaar <ovdbigge@gmail.com>
2021-10-08 12:53:11 +0100

    Use VestAbstract in tests

--- src/DssSpell.t.sol
+++ src/DssSpell.t.sol
@@ -42,20 +42,6 @@ interface PsmAbstract {
     function buyGem(address usr, uint256 gemAmt) external;
 }
 
-interface DssVestLike {
-    function cap() external returns (uint256);
-    function ids() external returns (uint256);
-    function usr(uint256) external view returns (address);
-    function bgn(uint256) external view returns (uint256);
-    function clf(uint256) external view returns (uint256);
-    function fin(uint256) external view returns (uint256);
-    function mgr(uint256) external view returns (address);
-    function res(uint256) external view returns (uint256);
-    function tot(uint256) external view returns (uint256);
-    function rxd(uint256) external view returns (uint256);
-    function unrestrict(uint256) external;
-    function vest(uint256) external;
-}
 interface BrokeTokenAbstract {
     function name() external view returns (bytes32);
     function symbol() external view returns (bytes32);
@@ -2630,7 +2616,7 @@ contract DssSpellTest is DSTest, DSMath {
     }
 
     function testVestMKR() public {
-        DssVestLike vest = DssVestLike(addr.addr("MCD_VEST_MKR_TREASURY"));
+        VestAbstract vest = VestAbstract(addr.addr("MCD_VEST_MKR_TREASURY"));
         assertEq(vest.ids(), 0);
 
         vote(address(spell));
