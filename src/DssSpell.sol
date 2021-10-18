9b2fdcca6c6b62c64f0f398f36652c7e2ee0b2d8
Brian McMichael <brian@brianmcmichael.com>
2021-10-18 12:23:47 -0400

    Add WSTETH addresses to spell

--- src/DssSpell.sol
+++ src/DssSpell.sol
@@ -16,10 +16,10 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 pragma solidity 0.6.12;
+pragma experimental ABIEncoderV2;
 
 import "dss-exec-lib/DssExec.sol";
 import "dss-exec-lib/DssAction.sol";
-
 
 contract DssSpellAction is DssAction {
 
@@ -31,12 +31,23 @@ contract DssSpellAction is DssAction {
     string public constant override description =
         "2021-10-22 MakerDAO Executive Spell | Hash: 0x";
 
-    address public constant STETH = 0xae7ab96520de3a18e5e111b5eaab095312d7fe84;
-    address public constant WSTETH  = 0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0;
-    address public constant MCD_JOIN_WSTETH_A = address(0); // TODO
-    address public constant MCD_CLIP_WSTETH_A = address(0); // TODO
-    address public constant MCD_CLIP_CALC_WSTETH_A = address(0); // TODO
-    address public constant MCD_PIP_WSTETH  = 0xfe7a2ac0b945f12089aeeb6ecebf4f384d9f043f;
+    // Many of the settings that change weekly rely on the rate accumulator
+    // described at https://docs.makerdao.com/smart-contract-modules/rates-module
+    // To check this yourself, use the following rate calculation (example 8%):
+    //
+    // $ bc -l <<< 'scale=27; e( l(1.08)/(60 * 60 * 24 * 365) )'
+    //
+    // A table of rates can be found at
+    //    https://ipfs.io/ipfs/QmefQMseb3AiTapiAKKexdKHig8wroKuZbmLtPLv4u2YwW
+    //
+    uint256 constant FOUR_PCT_RATE = 1000000001243680656318820312;
+
+    address public constant STETH_GEM              = 0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84;
+    address public constant WSTETH_GEM             = 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0;
+    address public constant MCD_JOIN_WSTETH_A      = 0x10CD5fbe1b404B7E19Ef964B63939907bdaf42E2;
+    address public constant MCD_CLIP_WSTETH_A      = 0x49A33A28C4C7D9576ab28898F4C9ac7e52EA457A;
+    address public constant MCD_CLIP_CALC_WSTETH_A = 0x15282b886675cc1Ce04590148f456428E87eaf13;
+    address public constant MCD_PIP_WSTETH         = 0xFe7a2aC0B945f12089aEEB6eCebf4F384D9f043F;
 
     function actions() public override {
 
@@ -51,7 +62,7 @@ contract DssSpellAction is DssAction {
         //  https://forum.makerdao.com/t/steth-collateral-onboarding-risk-evaluation/9061
         DssExecLib.setStairstepExponentialDecrease(MCD_CLIP_CALC_WSTETH_A, 90 seconds, 9900);
 
-        CollateralOpts memory STETH_A = CollateralOpts({
+        CollateralOpts memory WSTETH_A = CollateralOpts({
             ilk:                   "WSTETH-A",
             gem:                   address(WSTETH_GEM),
             join:                  address(MCD_JOIN_WSTETH_A),
@@ -78,8 +89,8 @@ contract DssSpellAction is DssAction {
         DssExecLib.setIlkAutoLineParameters("WSTETH-A", 5 * MILLION, 3 * MILLION, 8 hours);
 
 
-        DssExecLib.setChangelogAddress("STETH", STETH);
-        DssExecLib.setChangelogAddress("WSTETH", WSTETH);
+        DssExecLib.setChangelogAddress("STETH", STETH_GEM);
+        DssExecLib.setChangelogAddress("WSTETH", WSTETH_GEM);
         DssExecLib.setChangelogAddress("MCD_JOIN_WSTETH_A", MCD_JOIN_WSTETH_A);
         DssExecLib.setChangelogAddress("MCD_CLIP_WSTETH_A", MCD_CLIP_WSTETH_A);
         DssExecLib.setChangelogAddress("MCD_CLIP_CALC_WSTETH_A", MCD_CLIP_CALC_WSTETH_A);
