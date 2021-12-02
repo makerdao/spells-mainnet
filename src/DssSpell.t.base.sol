b3b10daaed8a8483c7ebb01694484e4e11be3f95
Tannr <tannr@hey.com>
2021-12-02 16:00:37 -0500

    Enable office hour in test

--- src/DssSpell.t.base.sol
+++ src/DssSpell.t.base.sol
@@ -294,7 +294,7 @@ contract DssSpellTestBase is DSTest, DSMath {
             deployed_spell:                 address(0),        // populate with deployed spell if deployed
             deployed_spell_created:         1637937152,        // use get-created-timestamp.sh if deployed
             previous_spell:                 address(0),        // supply if there is a need to test prior to its cast() function being called on-chain.
-            office_hours_enabled:           false,              // true if officehours is expected to be enabled in the spell
+            office_hours_enabled:           true,              // true if officehours is expected to be enabled in the spell
             expiration_threshold:           weekly_expiration  // (weekly_expiration,monthly_expiration) if weekly or monthly spell
         });
         spellValues.deployed_spell_created = spellValues.deployed_spell != address(0) ? spellValues.deployed_spell_created : block.timestamp;
