be8c6e946a5fa7768d8af0f6e200712f397ad587
Julien <80432849+julienmartinlevrai@users.noreply.github.com>
2021-12-10 22:11:37 -0500

    PE-771: 2021-12-10 executive spell (#209)
    * test: add addresses_wallets
    
    * feat: MKR vesting change
    
    * fix: add allowance
    
    * test: regular tests passing
    
    * test: add vest transfer test
    
    * docs: add link to poll for vesting transfer
    
    * feat: wstETH-A param changes
    
    * feat: MATIC-A parameter changes
    
    * test: add GRO, ORA and PE wallet addresses
    
    * test: remove examples
    
    * test: add comment next to new underscore standard for bps
    
    * test: rename totalMint and totalTreasury
    
    * Update src/DssSpell.t.base.sol
    
    Co-authored-by: Nazzareno Massari <nazzareno@nazzarenomassari.com>
    
    * test: make unused tests private
    
    * feat: add copy hash
    
    * test: add déployed test addresse
    
    * chore: archive spell
    
    Co-authored-by: Nazzareno Massari <nazzareno@nazzarenomassari.com>

---
+++ src/test/addresses_wallets.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+// Copyright (C) 2021 Dai Foundation
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+pragma solidity 0.6.12;
+
+contract AddressesWallets {
+
+    mapping (bytes32 => address) public addr_wallet;
+
+    constructor() public {
+        addr_wallet["DUX"]                          = 0x5A994D8428CCEbCC153863CCdA9D2Be6352f89ad;
+        addr_wallet["COM_WALLET"]                   = 0x1eE3ECa7aEF17D1e74eD7C447CcBA61aC76aDbA9;
+        addr_wallet["FLIPFLOPFLAP"]                 = 0x688d508f3a6B0a377e266405A1583B3316f9A2B3;
+        addr_wallet["FEEDBLACKLOOPS"]               = 0x80882f2A36d49fC46C3c654F7f9cB9a2Bf0423e1;
+        addr_wallet["ULTRASCHUPPI"]                 = 0x89C5d54C979f682F40b73a9FC39F338C88B434c6;
+        addr_wallet["FIELDTECHNOLOGIES"]            = 0x0988E41C02915Fe1beFA78c556f946E5F20ffBD3;
+        addr_wallet["GRO_WALLET"]                   = 0x7800C137A645c07132886539217ce192b9F0528e;
+        addr_wallet["ORA_WALLET"]                   = 0x2d09B7b95f3F312ba6dDfB77bA6971786c5b50Cf;
+        addr_wallet["PE_WALLET"]                    = 0xe2c16c308b843eD02B09156388Cb240cEd58C01c;
+    }
+}
