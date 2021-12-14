7b0a0ac84240938b246694a662c1099d94ad31e2
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2021-12-13 17:24:36 -0700

    remove unused abstract

--- src/DssSpell.t.base.sol
+++ src/DssSpell.t.base.sol
@@ -43,31 +43,6 @@ interface PsmAbstract {
     function buyGem(address usr, uint256 gemAmt) external;
 }
 
-interface BrokeTokenAbstract {
-    function name() external view returns (bytes32);
-    function symbol() external view returns (bytes32);
-    function decimals() external view returns (uint256);
-    function totalSupply() external view returns (uint256);
-    function balanceOf(address) external view returns (uint256);
-    function transfer(address, uint256) external;
-    function allowance(address, address) external view returns (uint256);
-    function approve(address, uint256) external;
-    function approve(address) external;
-    function transferFrom(address, address, uint256) external;
-    function push(address, uint256) external;
-    function pull(address, uint256) external;
-    function move(address, address, uint256) external;
-    function mint(uint256) external;
-    function mint(address,uint) external;
-    function burn(uint256) external;
-    function burn(address,uint) external;
-    function setName(bytes32) external;
-    function authority() external view returns (address);
-    function owner() external view returns (address);
-    function setOwner(address) external;
-    function setAuthority(address) external;
-}
-
 interface DirectDepositLike is GemJoinAbstract {
     function file(bytes32, uint256) external;
     function exec() external;
