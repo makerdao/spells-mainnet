8e4661f57e41dff8c721596c0cf23cb2640d1647
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2021-12-14 17:04:50 -0700

    change to comments for use as a template when there isn't an onboarding

--- src/DssSpellCollateralOnboarding.sol
+++ src/DssSpellCollateralOnboarding.sol
@@ -22,49 +22,53 @@ import "dss-exec-lib/DssExecLib.sol";
 contract DssSpellCollateralOnboardingAction {
 
     // --- Rates ---
-    uint256 constant ONE_PCT_RATE = 1000000000315522921573372069;
 
     // --- Math ---
-    uint256 constant MILLION = 10 ** 6;
 
-    // --- GUNIV3DAIUSDC2-A ---
-    address constant GUNIV3DAIUSDC2                 = 0x50379f632ca68D36E50cfBC8F78fe16bd1499d1e;
-    address constant MCD_JOIN_GUNIV3DAIUSDC2_A      = 0xA7e4dDde3cBcEf122851A7C8F7A55f23c0Daf335;
-    address constant MCD_CLIP_GUNIV3DAIUSDC2_A      = 0xB55da3d3100C4eBF9De755b6DdC24BF209f6cc06;
-    address constant MCD_CLIP_CALC_GUNIV3DAIUSDC2_A = 0xef051Ca2A2d809ba47ee0FC8caaEd06E3D832225;
-    address constant PIP_GUNIV3DAIUSDC2             = 0xcCBa43231aC6eceBd1278B90c3a44711a00F4e93;
+    // --- DEPLOYED COLLATERAL ADDRESSES ---
 
     function onboardNewCollaterals() internal {
         // ----------------------------- Collateral onboarding -----------------------------
-        //  Add GUNIV3DAIUSDC2-A as a new Vault Type
-        //  https://vote.makerdao.com/polling/QmSkHE8T?network=mainnet#poll-detail
-        DssExecLib.addNewCollateral(
-            CollateralOpts({
-                ilk:                   "GUNIV3DAIUSDC2-A",
-                gem:                   GUNIV3DAIUSDC2,
-                join:                  MCD_JOIN_GUNIV3DAIUSDC2_A,
-                clip:                  MCD_CLIP_GUNIV3DAIUSDC2_A,
-                calc:                  MCD_CLIP_CALC_GUNIV3DAIUSDC2_A,
-                pip:                   PIP_GUNIV3DAIUSDC2,
-                isLiquidatable:        false,
-                isOSM:                 true,
-                whitelistOSM:          true,
-                ilkDebtCeiling:        10 * MILLION,
-                minVaultAmount:        15_000,
-                maxLiquidationAmount:  5 * MILLION,
-                liquidationPenalty:    1300,
-                ilkStabilityFee:       ONE_PCT_RATE,
-                startingPriceFactor:   10500,
-                breakerTolerance:      9500,
-                auctionDuration:       220 minutes,
-                permittedDrop:         9000,
-                liquidationRatio:      10500,
-                kprFlatReward:         300,
-                kprPctReward:          10
-            })
-        );
+        //  Add ______________ as a new Vault Type
+        //  Poll Link: 
 
-        DssExecLib.setStairstepExponentialDecrease(MCD_CLIP_CALC_GUNIV3DAIUSDC2_A, 120 seconds, 9990);
-        DssExecLib.setIlkAutoLineParameters("GUNIV3DAIUSDC2-A", 10 * MILLION, 10 * MILLION, 8 hours);
+        // DssExecLib.addNewCollateral(
+        //     CollateralOpts({
+        //         ilk:                   ,
+        //         gem:                   ,
+        //         join:                  ,
+        //         clip:                  ,
+        //         calc:                  ,
+        //         pip:                   ,
+        //         isLiquidatable:        ,
+        //         isOSM:                 ,
+        //         whitelistOSM:          ,
+        //         ilkDebtCeiling:        ,
+        //         minVaultAmount:        ,
+        //         maxLiquidationAmount:  ,
+        //         liquidationPenalty:    ,
+        //         ilkStabilityFee:       ,
+        //         startingPriceFactor:   ,
+        //         breakerTolerance:      ,
+        //         auctionDuration:       ,
+        //         permittedDrop:         ,
+        //         liquidationRatio:      ,
+        //         kprFlatReward:         ,
+        //         kprPctReward:          
+        //     })
+        // );
+
+        // DssExecLib.setStairstepExponentialDecrease(
+        //     CALC_ADDR, 
+        //     DURATION,
+        //     PCT_BPS
+        // );
+
+        // DssExecLib.setIlkAutoLineParameters(
+        //     ILK,
+        //     AMOUNT,
+        //     GAP,
+        //     TTL
+        // );
     }
 }
