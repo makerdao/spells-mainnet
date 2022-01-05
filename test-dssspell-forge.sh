79baa755e163b9b809845792e8a0fce6b553db57
Christopher Mooney <chris@dod.net>
2022-01-05 15:19:08 -0500

    fixes verbosity for new version of forge

--- test-dssspell-forge.sh
+++ test-dssspell-forge.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-[[ "$(seth chain --rpc-url=$ETH_RPC_URL)" == "ethlive"  ]] || { echo "Please set a mainnet ETH_RPC_URL"; exit 1;  }
+[[ "$(seth chain --rpc-url="$ETH_RPC_URL")" == "ethlive"  ]] || { echo "Please set a mainnet ETH_RPC_URL"; exit 1;  }
 
 if [[ -z "$1" ]]; then
-  forge test --fork-url "$ETH_RPC_URL" --libraries "src/DssSpell.sol:DssExecLib:0xfD88CeE74f7D78697775aBDAE53f9Da1559728E4" --verbosity 3 --force --match "$1"
+  forge test --fork-url "$ETH_RPC_URL" --libraries "src/DssSpell.sol:DssExecLib:0xfD88CeE74f7D78697775aBDAE53f9Da1559728E4" -vvv --force --match "$1"
 else
-  forge test --fork-url "$ETH_RPC_URL" --libraries "src/DssSpell.sol:DssExecLib:0xfD88CeE74f7D78697775aBDAE53f9Da1559728E4" --verbosity 3 --force
+  forge test --fork-url "$ETH_RPC_URL" --libraries "src/DssSpell.sol:DssExecLib:0xfD88CeE74f7D78697775aBDAE53f9Da1559728E4" -vvv --force 
 fi
