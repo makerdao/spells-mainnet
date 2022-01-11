72365301626f75a387659aac2785d677b67a6a29
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2022-01-11 09:30:33 -0700

    rename to config

--- src/DssSpell.t.base.sol
+++ src/DssSpell.t.base.sol
@@ -10,7 +10,7 @@ import "./test/rates.sol";
 import "./test/addresses_mainnet.sol";
 import "./test/addresses_deployers.sol";
 import "./test/addresses_wallets.sol";
-import "./test/system_config.sol";
+import "./test/config.sol";
 
 import {DssSpell} from "./DssSpell.sol";
 
