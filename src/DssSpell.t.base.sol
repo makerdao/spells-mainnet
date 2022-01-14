faa072a89e0c36e46f420853522c6cf31365ee03
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2022-01-14 13:11:58 -0700

    update with address and timestamp

--- src/DssSpell.t.base.sol
+++ src/DssSpell.t.base.sol
@@ -200,8 +200,8 @@ contract DssSpellTestBase is Config, DSTest, DSMath {
         // Test for spell-specific parameters
         //
         spellValues = SpellValues({
-            deployed_spell:                 address(0),        // populate with deployed spell if deployed
-            deployed_spell_created:         1639162896,        // use get-created-timestamp.sh if deployed
+            deployed_spell:                 address(0xDD5052bfC4D281793653B0037d46Cc2d8d1FD1b5),        // populate with deployed spell if deployed
+            deployed_spell_created:         1642190897,        // use get-created-timestamp.sh if deployed
             previous_spell:                 address(0),        // supply if there is a need to test prior to its cast() function being called on-chain.
             office_hours_enabled:           false,              // true if officehours is expected to be enabled in the spell
             expiration_threshold:           weekly_expiration  // (weekly_expiration,monthly_expiration) if weekly or monthly spell
