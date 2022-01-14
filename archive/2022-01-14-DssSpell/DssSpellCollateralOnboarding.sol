3415327468e3b3c4757938ae8746390a8f891c6f
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2022-01-14 13:43:16 -0700

    (PE-805) Jan 14 2022 (#214)
    * add dust limit changes
    
    * add delegate compensation
    
    * add optimism recovery and test
    
    * add lerp note to error message
    
    * increase the mat limit for lerping collateral
    
    * initial PR comments
    
    * reorder to match executive vote spreadsheet
    
    * add Executive hash
    
    * fix hash
    
    * point to hash not branch
    
    Co-authored-by: Julien <80432849+julienmartinlevrai@users.noreply.github.com>
    
    * make immutable inline vars public
    
    * update with address and timestamp
    
    * archive spell
    
    Co-authored-by: Julien <80432849+julienmartinlevrai@users.noreply.github.com>

---
+++ archive/2022-01-14-DssSpell/DssSpellCollateralOnboarding.sol
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+// Copyright (C) 2021 Dai Foundation
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+pragma solidity 0.6.12;
+
+import "dss-exec-lib/DssExecLib.sol";
+
+contract DssSpellCollateralOnboardingAction {
+
+    // --- Rates ---
+    // Many of the settings that change weekly rely on the rate accumulator
+    // described at https://docs.makerdao.com/smart-contract-modules/rates-module
+    // To check this yourself, use the following rate calculation (example 8%):
+    //
+    // $ bc -l <<< 'scale=27; e( l(1.08)/(60 * 60 * 24 * 365) )'
+    //
+    // A table of rates can be found at
+    //    https://ipfs.io/ipfs/QmTRiQ3GqjCiRhh1ojzKzgScmSsiwQPLyjhgYSxZASQekj
+    //
+
+    // --- Math ---
+
+    // --- DEPLOYED COLLATERAL ADDRESSES ---
+
+    function onboardNewCollaterals() internal {
+        // ----------------------------- Collateral onboarding -----------------------------
+        //  Add ______________ as a new Vault Type
+        //  Poll Link: 
+
+        // DssExecLib.addNewCollateral(
+        //     CollateralOpts({
+        //         ilk:                   ,
+        //         gem:                   ,
+        //         join:                  ,
+        //         clip:                  ,
+        //         calc:                  ,
+        //         pip:                   ,
+        //         isLiquidatable:        ,
+        //         isOSM:                 ,
+        //         whitelistOSM:          ,
+        //         ilkDebtCeiling:        ,
+        //         minVaultAmount:        ,
+        //         maxLiquidationAmount:  ,
+        //         liquidationPenalty:    ,
+        //         ilkStabilityFee:       ,
+        //         startingPriceFactor:   ,
+        //         breakerTolerance:      ,
+        //         auctionDuration:       ,
+        //         permittedDrop:         ,
+        //         liquidationRatio:      ,
+        //         kprFlatReward:         ,
+        //         kprPctReward:          
+        //     })
+        // );
+
+        // DssExecLib.setStairstepExponentialDecrease(
+        //     CALC_ADDR, 
+        //     DURATION,
+        //     PCT_BPS
+        // );
+
+        // DssExecLib.setIlkAutoLineParameters(
+        //     ILK,
+        //     AMOUNT,
+        //     GAP,
+        //     TTL
+        // );
+
+        // ChainLog Updates
+        // Add the new flip and join to the Chainlog
+        // address constant CHAINLOG        = 0xdA0Ab1e0017DEbCd72Be8599041a2aa3bA7e740F;
+        // ChainlogAbstract(CHAINLOG).setAddress("<join-name>", <join-address>);
+        // ChainlogAbstract(CHAINLOG).setAddress("<flip-name>", <flip-address>);
+        // ChainlogAbstract(CHAINLOG).setVersion("<new-version>");
+    }
+}
---
+++ archive/2022-01-14-DssSpell/test/addresses_deployers.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+// Copyright (C) 2021 Dai Foundation
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+pragma solidity 0.6.12;
+
+contract Deployers {
+
+    address[] public addr;
+
+    constructor() public {
+        addr = [
+            0xdDb108893104dE4E1C6d0E47c42237dB4E617ACc,
+            0xDa0FaB05039809e63C5D068c897c3e602fA97457,
+            0xda0fab060e6cc7b1C0AA105d29Bd50D71f036711,
+            0xDA0FaB0700A4389F6E6679aBAb1692B4601ce9bf,
+            0x0048d6225D1F3eA4385627eFDC5B4709Cab4A21c,
+            0xd200790f62c8da69973e61d4936cfE4f356ccD07,
+            0xdA0C0de01d90A5933692Edf03c7cE946C7c50445,
+            0x4D6fbF888c374D7964D56144dE0C0cFBd49750D3,  // Oracles
+            0x1f42e41A34B71606FcC60b4e624243b365D99745,  // Oracles
+            0x075da589886BA445d7c7e81c472059dE7AE65250   // Used for Optimism & Arbitrum bridge contracts
+        ];
+    }
+
+    function count() external view returns (uint256) {
+        return addr.length;
+    }
+}
