e11b20093106e61aeab3f881b632a41eb4e9ca77
Brian McMichael <brian@brianmcmichael.com>
2022-01-21 11:04:54 -0500

    Update copyright dates

--- src/test/config.sol
+++ src/test/config.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 //
-// Copyright (C) 2021 Dai Foundation
+// Copyright (C) 2021-2022 Dai Foundation
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
