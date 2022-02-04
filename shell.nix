556da535d9f37c95d4f09efab396dad30c202338
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2022-02-04 16:07:14 -0300

    Revert "Return optimization"
    This reverts commit 91a37cebf1bda28906f219a6c527f6c19de29b7d.

--- shell.nix
+++ shell.nix
@@ -6,8 +6,8 @@
 
 mkShell {
   DAPP_SOLC = solc-static-versions.solc_0_6_12 + "/bin/solc-0.6.12";
-  DAPP_BUILD_OPTIMIZE = 1;
-  DAPP_BUILD_OPTIMIZE_RUNS = 1;
+  DAPP_BUILD_OPTIMIZE = 0;
+  DAPP_BUILD_OPTIMIZE_RUNS = 200;
   DAPP_LIBRARIES = " lib/dss-exec-lib/src/DssExecLib.sol:DssExecLib:" + (builtins.substring 0 42 (builtins.readFile ./DssExecLib.address));
   DAPP_LINK_TEST_LIBRARIES = 0;
   buildInputs = [
