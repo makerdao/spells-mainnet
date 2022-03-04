23c427921e61a2315d2bec90b7d39d037eef111b
Nazzareno Massari <nas@naszam.co>
2022-03-04 16:22:23 +0000

    Test with deployed spell address and timestamp

--- src/test/config.sol
+++ src/test/config.sol
@@ -98,8 +98,8 @@ contract Config {
         // Values for spell-specific parameters
         //
         spellValues = SpellValues({
-            deployed_spell:                 address(0),        // populate with deployed spell if deployed
-            deployed_spell_created:         1645737440,        // use get-created-timestamp.sh if deployed
+            deployed_spell:                 address(0x24910113D7378cb8BdCbeaC4596Ec68EfccD684d),        // populate with deployed spell if deployed
+            deployed_spell_created:         1646410523,        // use get-created-timestamp.sh if deployed
             previous_spell:                 address(0),        // supply if there is a need to test prior to its cast() function being called on-chain.
             office_hours_enabled:           false,             // true if officehours is expected to be enabled in the spell
             expiration_threshold:           weekly_expiration  // (weekly_expiration,monthly_expiration) if weekly or monthly spell
