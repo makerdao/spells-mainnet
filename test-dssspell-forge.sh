50f55f069a540604d778f9d293c6ab16fd2b80a9
Sam MacPherson <xylom44@gmail.com>
2022-03-07 07:49:40 -0500

    reset spell

--- test-dssspell-forge.sh
+++ test-dssspell-forge.sh
@@ -18,13 +18,14 @@ done
 DSS_EXEC_LIB=$(< DssExecLib.address)
 echo "Using DssExecLib at: $DSS_EXEC_LIB"
 export DAPP_LIBRARIES="src/DssSpell.sol:DssExecLib:$DSS_EXEC_LIB"
+export DAPP_BUILD_OPTIMIZE=0   # forge turns on optimizer by default
 
 if [[ -z "$MATCH" && -z "$BLOCK" ]]; then
-    forge test --fork-url "$ETH_RPC_URL" --force
+    forge test --fork-url "$ETH_RPC_URL"
 elif [[ -z "$BLOCK" ]]; then
-    forge test --fork-url "$ETH_RPC_URL" --match "$MATCH" -vvv --force
+    forge test --fork-url "$ETH_RPC_URL" --match "$MATCH" -vvv
 elif [[ -z "$MATCH" ]]; then
-    forge test --fork-url "$ETH_RPC_URL" --fork-block-number "$BLOCK" --force
+    forge test --fork-url "$ETH_RPC_URL" --fork-block-number "$BLOCK"
 else
-    forge test --fork-url "$ETH_RPC_URL" --match "$MATCH" --fork-block-number "$BLOCK" -vvv --force
+    forge test --fork-url "$ETH_RPC_URL" --match "$MATCH" --fork-block-number "$BLOCK" -vvv
 fi
