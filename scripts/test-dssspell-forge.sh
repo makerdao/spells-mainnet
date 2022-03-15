01d1fbd92ca782d445a8709c664a3c0d9a683cb7
Nazzareno Massari <nas@naszam.co>
2022-03-15 18:24:49 +0000

    shellcheck `test-dssspell-forge.sh`

--- scripts/test-dssspell-forge.sh
+++ scripts/test-dssspell-forge.sh
@@ -5,8 +5,8 @@ set -e
 
 for ARGUMENT in "$@"
 do
-    KEY=$(echo $ARGUMENT | cut -f1 -d=)
-    VALUE=$(echo $ARGUMENT | cut -f2 -d=)
+    KEY=$(echo "$ARGUMENT" | cut -f1 -d=)
+    VALUE=$(echo "$ARGUMENT" | cut -f2 -d=)
 
     case "$KEY" in
             match)      MATCH="$VALUE" ;;
