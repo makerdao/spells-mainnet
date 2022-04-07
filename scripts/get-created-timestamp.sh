0764965638436ffeee268525c68a9629f70bec39
Christopher Mooney <chris@dod.net>
2022-04-07 11:33:33 -0400

    lifted make deploy-stamp fix from goerli

--- scripts/get-created-timestamp.sh
+++ scripts/get-created-timestamp.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
+#!/usr/bin/env bash
 
-seth block "$(seth tx "${1}"|grep blockNumber|awk '{print $2}')"|grep timestamp|awk '{print $2}'
+for ARGUMENT in "$@"
+do
+    KEY=$(echo "$ARGUMENT" | cut -f1 -d=)
+    VALUE=$(echo "$ARGUMENT" | cut -f2 -d=)
+
+    case "$KEY" in
+            tx)      TXHASH="$VALUE" ;;
+            *)       TXHASH="$KEY"
+    esac
+done
+
+seth block "$(seth tx "${TXHASH}"|grep blockNumber|awk '{print $2}')"|grep timestamp|awk '{print $2}'
