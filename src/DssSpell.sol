44efae7f554431c64918c78998836d8734cb78d2
Brian McMichael <brian@brianmcmichael.com>
2022-04-13 12:56:47 -0400

    spell draft

--- src/DssSpell.sol
+++ src/DssSpell.sol
@@ -42,7 +42,19 @@ contract DssSpellAction is DssAction, DssSpellCollateralOnboardingAction {
 
     // Math
     uint256 constant internal MILLION  = 10 ** 6;
-    uint256 constant internal BILLION  = 10 ** 9;
+    uint256 constant internal WAD      = 10 ** 18;
+
+    DssVestLike immutable MCD_VEST_DAI = DssVestLike(DssExecLib.getChangelogAddress("MCD_VEST_DAI"));
+
+    address constant internal MCD_CLIP_CALC_TUSD_A = 0x9B207AfAAAD1ae300Ea659e71306a7Bd6D81C160;
+    address constant internal       GRO_001_WALLET = 0x7800C137A645c07132886539217ce192b9F0528e;
+    address constant internal    AMBASSADOR_WALLET = 0xF411d823a48D18B32e608274Df16a9957fE33E45;
+    address constant internal    GELATO_WALLET_OLD = 0x926c21602FeC84d6d0fA6450b40Edba595B5c6e4;
+    address constant internal    GELATO_WALLET_NEW = 0x478c7Ce3e1df09130f8D65a23AD80e05b352af62;
+
+    uint256 constant APR_01_2022 = 1648771200;
+    // 2022-04-01 to 2022-09-30 23:59:59 UTC
+    uint256 constant SIX_MONTHS = 183 days;
 
     // Many of the settings that change weekly rely on the rate accumulator
     // described at https://docs.makerdao.com/smart-contract-modules/rates-module
@@ -66,6 +78,73 @@ contract DssSpellAction is DssAction, DssSpellCollateralOnboardingAction {
         // ---------------------------------------------------------------------
         // Includes changes from the DssSpellCollateralOnboardingAction
         // onboardNewCollaterals();
+
+        // ----------------- Core Unit Budgets -----------------
+        // https://mips.makerdao.com/mips/details/MIP40c3SP37#budget-implementation
+        DssExecLib.sendPaymentFromSurplusBuffer(GRO_001_WALLET, 474_683);
+
+        // -------------- Fund Ambassador Program --------------
+        // https://vote.makerdao.com/polling/QmPpQ49p#poll-detail
+        DssExecLib.sendPaymentFromSurplusBuffer(AMBASSADOR_WALLET, 25_000);
+
+
+        // -------- CRVV1ETHSTETH-A System Parameters ----------
+        // https://vote.makerdao.com/polling/QmeTfgKP#poll-detail
+        DssExecLib.setIlkAutoLineParameters("CRVV1ETHSTETH-A", 50 * MILLION, 10 * MILLION, 8 hours);
+
+        // ---- Replace Gelato Keeper Top Up Contract Stream ---
+        // https://forum.makerdao.com/t/update-to-the-gelato-keeper-network-top-up-contract/14524
+        // Yank existing stream
+        MCD_VEST_DAI.yank(36);
+        // Replace stream
+        // Address: 0x478c7Ce3e1df09130f8D65a23AD80e05b352af62
+        // Amount: 1,000 DAI/day
+        // Start Date: Apr 1, 2022
+        // End Date: Sep 30, 2022 23:59:59 UTC
+        MCD_VEST_DAI.restrict(
+            MCD_VEST_DAI.create(
+                GELATO_WALLET_NEW,
+                183_000 * WAD,
+                APR_01_2022,
+                SIX_MONTHS,
+                0,
+                address(0)
+            )
+        );
+
+        // ------------------ Offboard TUSD-A -------------------
+        // https://vote.makerdao.com/polling/QmVkRdjg#poll-detail
+        bytes32 _ilk  = bytes32("TUSD-A");
+        address _clip = DssExecLib.getChangelogAddress("MCD_CLIP_TUSD_A");
+        //
+        // Enable liquidations for TUSD-A
+        DssExecLib.authorize(_clip, DssExecLib.clipperMom());
+        DssExecLib.setValue(_clip, "stopped", 0);
+        // Use Abacus/LinearDecrease
+        DssExecLib.setContract(_clip, "calc", MCD_CLIP_CALC_TUSD_A);
+        // Set Liquidation Penalty to 0
+        DssExecLib.setIlkLiquidationPenalty(_ilk, 0);
+        // Set Liquidation Ratio to 150%
+        DssExecLib.setIlkLiquidationRatio(_ilk, 15000);
+        // Set Auction Price Multiplier (buf) to 1
+        DssExecLib.setStartingPriceMultiplicativeFactor(_ilk, 10000);
+        // Set Local Liquidation Limit (ilk.hole) to 5 million DAI
+        DssExecLib.setIlkMaxLiquidationAmount(_ilk, 5 * MILLION);
+        // Set tau for Abacus/LinearDecrease to 21,600,000 second (estimated 10bps drop per 6 hours = 250 days till 0)
+        DssExecLib.setLinearDecrease(MCD_CLIP_CALC_TUSD_A, 21_600_000);
+        // Set Max Auction Duration (tail) to 432,000 seconds (5 days, implies minimum price of 0.98)
+        DssExecLib.setAuctionTimeBeforeReset(_ilk, 432_000);
+        DssExecLib.setAuctionPermittedDrop(_ilk, 9800);
+        // Set Proportional Kick Incentive (chip) to 0
+        DssExecLib.setKeeperIncentivePercent(_ilk, 0);
+        // Set Flat Kick Incentive (tip) to 500
+        DssExecLib.setKeeperIncentiveFlatRate(_ilk, 500);
+
+        // Update calc in changelog
+        DssExecLib.setChangelogAddress("MCD_CLIP_CALC_TUSD_A", MCD_CLIP_CALC_TUSD_A);
+
+        // Update changelog version
+        DssExecLib.setChangelogVersion("1.11.1");
     }
 
 }
