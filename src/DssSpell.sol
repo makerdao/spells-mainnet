a643bea91e12b86046d2dadce86553f60372e423
Nikolaj Lollike <nikolajzl@hotmail.com>
2022-04-29 11:10:45 -0700

    onboard WSTETH-B

--- src/DssSpell.sol
+++ src/DssSpell.sol
@@ -17,48 +17,21 @@
 
 pragma solidity 0.6.12;
 // Enable ABIEncoderV2 when onboarding collateral
-// pragma experimental ABIEncoderV2;
+pragma experimental ABIEncoderV2;
 import "dss-exec-lib/DssExec.sol";
 import "dss-exec-lib/DssAction.sol";
 
 import { DssSpellCollateralOnboardingAction } from "./DssSpellCollateralOnboarding.sol";
 
-interface DssVestLike {
-    function cap() external view returns (uint256);
-    function chainlog() external view returns (address);
-    function daiJoin() external view returns (address);
-    function ids() external view returns (uint256);
-    function vat() external view returns (address);
-    function create(address, uint256, uint256, uint256, uint256, address) external returns (uint256);
-    function file(bytes32, uint256) external;
-    function rely(address) external;
-    function restrict(uint256) external;
-}
-
 contract DssSpellAction is DssAction, DssSpellCollateralOnboardingAction {
     // Provides a descriptive tag for bot consumption
     // This should be modified weekly to provide a summary of the actions
-    // Hash: cast keccak -- "$(wget https://raw.githubusercontent.com/makerdao/community/f76fcca7cb8ca0db328c1241d067a0bb9b30a16e/governance/votes/Executive%20Vote%20-%20April%2029%2C%202022.md -q -O - 2>/dev/null)"
+    // Hash: cast keccak -- "$(wget https://raw.githubusercontent.com/makerdao/community/6b4cedd333d710702f50b0e679b005286773b6d3/governance/votes/Executive%20vote%20-%20April%2022%2C%202022.md -q -O - 2>/dev/null)"
     string public constant override description =
-        "2022-04-29 MakerDAO Executive Spell | Hash: 0x707ebde4cd135ef70155726ab88d178c20fbaad79bd5d1faa32313d6247d7bbf";
-
-    address constant MCD_VEST_DAI = 0xa4c22f0e25C6630B2017979AcF1f865e94695C4b;
-
-    address constant COM_WALLET     = 0x1eE3ECa7aEF17D1e74eD7C447CcBA61aC76aDbA9;
-    address constant COM_EF_WALLET  = 0x99E1696A680c0D9f426Be20400E468089E7FDB0f;
-    address constant EVENTS_WALLET  = 0x3D274fbAc29C92D2F624483495C0113B44dBE7d2;
-    address constant DIN_WALLET     = 0x7327Aed0Ddf75391098e8753512D8aEc8D740a1F;
-    address constant PE_WALLET      = 0xe2c16c308b843eD02B09156388Cb240cEd58C01c;
-    address constant SH_WALLET      = 0x955993Df48b0458A01cfB5fd7DF5F5DCa6443550;
-
-    uint256 constant MAY_01_2022 = 1651363200;
-    uint256 constant JUL_01_2022 = 1656633600;
-    uint256 constant JAN_01_2023 = 1672531200;
-    uint256 constant MAY_01_2023 = 1682899200;
+        // TODO: update this
+        "2022-XX-YY MakerDAO Executive Spell | Hash: 0x0000000000000000000000000000000000000000000000000000000000000000";
 
     // Math
-
-    uint256 constant WAD = 10 ** 18;
 
     // Many of the settings that change weekly rely on the rate accumulator
     // described at https://docs.makerdao.com/smart-contract-modules/rates-module
@@ -71,12 +44,19 @@ contract DssSpellAction is DssAction, DssSpellCollateralOnboardingAction {
     //
 
     // --- Rates ---
-    //uint256 constant FOUR_FIVE_PCT_RATE      = 1000000001395766281313196627;
+    // uint256 constant FOUR_FIVE_PCT_RATE      = 1000000001395766281313196627;
+
+    // Turn office hours off
+    function officeHours() public override returns (bool) {
+        return true;
+    }
 
     function actions() public override {
         // ---------------------------------------------------------------------
         // Includes changes from the DssSpellCollateralOnboardingAction
         onboardNewCollaterals();
+
+        DssExecLib.setChangelogVersion("1.11.3");
     }
 }
 
