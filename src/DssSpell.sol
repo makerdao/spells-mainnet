bb90b0d837b45ee58027000251beb5ab41063802
Nazar Duchak <nazar@iovlabs.org>
2022-06-14 13:41:52 +0300

    feat: univ2 lp off-boarding

--- src/DssSpell.sol
+++ src/DssSpell.sol
@@ -33,6 +33,23 @@ contract DssSpellAction is DssAction, DssSpellCollateralOnboardingAction {
     string public constant override description =
         "2022-06-15 MakerDAO Executive Spell | Hash: 0x0";
 
+    // Math
+    uint256 constant RAY  = 10 ** 27;
+
+    // --- Offboarding: Current Liquidation Ratio ---
+    uint256 constant CURRENT_UNI_A_MAT              =  165 * RAY / 100;
+    uint256 constant CURRENT_UNIV2DAIETH_A_MAT      =  120 * RAY / 100;
+    uint256 constant CURRENT_UNIV2WBTCETH_A_MAT     =  145 * RAY / 100;
+    uint256 constant CURRENT_UNIV2UNIETH_A_MAT      =  160 * RAY / 100;
+    uint256 constant CURRENT_UNIV2WBTCDAI_A_MAT     =  120 * RAY / 100;
+
+    // --- Offboarding: Target Liquidation Ratio ---
+    uint256 constant TARGET_UNI_A_MAT               = 900 * RAY / 100;
+    uint256 constant TARGET_UNIV2DAIETH_A_MAT       = 3100 * RAY / 100;
+    uint256 constant TARGET_UNIV2WBTCETH_A_MAT      = 5300 * RAY / 100;
+    uint256 constant TARGET_UNIV2UNIETH_A_MAT       = 700 * RAY / 100;
+    uint256 constant TARGET_UNIV2WBTCDAI_A_MAT      = 1000 * RAY / 100;
+
     // Many of the settings that change weekly rely on the rate accumulator
     // described at https://docs.makerdao.com/smart-contract-modules/rates-module
     // To check this yourself, use the following rate calculation (example 8%):
@@ -47,6 +64,91 @@ contract DssSpellAction is DssAction, DssSpellCollateralOnboardingAction {
         // ---------------------------------------------------------------------
         // Includes changes from the DssSpellCollateralOnboardingAction
         // onboardNewCollaterals();
+
+        // Offboard UNI-A
+        // https://vote.makerdao.com/polling/QmSfLS6V#poll-detail
+        // https://forum.makerdao.com/t/signal-request-offboard-uni-univ2daieth-univ2wbtceth-univ2unieth-and-univ2wbtcdai/15160
+
+        DssExecLib.setIlkLiquidationPenalty("UNI-A", 0);
+        DssExecLib.setKeeperIncentiveFlatRate("UNI-A", 0);
+        DssExecLib.linearInterpolation({
+            _name:      "UNI-A Offboarding",
+            _target:    DssExecLib.spotter(),
+            _ilk:       "UNI-A",
+            _what:      "mat",
+            _startTime: block.timestamp,
+            _start:     CURRENT_UNI_A_MAT,
+            _end:       TARGET_UNI_A_MAT,
+            _duration:  30 days
+        });
+
+        // Offboard UNIV2DAIETH-A
+        // https://vote.makerdao.com/polling/QmQUozNn#poll-detail
+        // https://forum.makerdao.com/t/signal-request-offboard-uni-univ2daieth-univ2wbtceth-univ2unieth-and-univ2wbtcdai/15160
+
+        DssExecLib.setIlkLiquidationPenalty("UNIV2DAIETH-A", 0);
+        DssExecLib.setKeeperIncentiveFlatRate("UNIV2DAIETH-A", 0);
+        DssExecLib.linearInterpolation({
+            _name:      "UNIV2DAIETH-A Offboarding",
+            _target:    DssExecLib.spotter(),
+            _ilk:       "UNIV2DAIETH-A",
+            _what:      "mat",
+            _startTime: block.timestamp,
+            _start:     CURRENT_UNIV2DAIETH_A_MAT,
+            _end:       TARGET_UNIV2DAIETH_A_MAT,
+            _duration:  30 days
+        });
+
+        // Offboard UNIV2WBTCETH-A
+        // https://vote.makerdao.com/polling/QmY3YsDB#poll-detail
+        // https://forum.makerdao.com/t/signal-request-offboard-uni-univ2daieth-univ2wbtceth-univ2unieth-and-univ2wbtcdai/15160
+
+        DssExecLib.setIlkLiquidationPenalty("UNIV2WBTCETH-A", 0);
+        DssExecLib.setKeeperIncentiveFlatRate("UNIV2WBTCETH-A", 0);
+        DssExecLib.linearInterpolation({
+            _name:      "UNIV2WBTCETH-A Offboarding",
+            _target:    DssExecLib.spotter(),
+            _ilk:       "UNIV2WBTCETH-A",
+            _what:      "mat",
+            _startTime: block.timestamp,
+            _start:     CURRENT_UNIV2WBTCETH_A_MAT,
+            _end:       TARGET_UNIV2WBTCETH_A_MAT,
+            _duration:  30 days
+        });
+
+        // Offboard UNIV2UNIETH-A
+        // https://vote.makerdao.com/polling/QmUeYVa2#poll-detail
+        // https://forum.makerdao.com/t/signal-request-offboard-uni-univ2daieth-univ2wbtceth-univ2unieth-and-univ2wbtcdai/15160
+
+        DssExecLib.setIlkLiquidationPenalty("UNIV2UNIETH-A", 0);
+        DssExecLib.setKeeperIncentiveFlatRate("UNIV2UNIETH-A", 0);
+        DssExecLib.linearInterpolation({
+            _name:      "UNIV2UNIETH-A Offboarding",
+            _target:    DssExecLib.spotter(),
+            _ilk:       "UNIV2UNIETH-A",
+            _what:      "mat",
+            _startTime: block.timestamp,
+            _start:     CURRENT_UNIV2UNIETH_A_MAT,
+            _end:       TARGET_UNIV2UNIETH_A_MAT,
+            _duration:  30 days
+        });
+
+        // Offboard UNIV2WBTCDAI-A
+        // https://vote.makerdao.com/polling/QmZHNkip#poll-detail
+        // https://forum.makerdao.com/t/signal-request-offboard-uni-univ2daieth-univ2wbtceth-univ2unieth-and-univ2wbtcdai/15160
+
+        DssExecLib.setIlkLiquidationPenalty("UNIV2WBTCDAI-A", 0);
+        DssExecLib.setKeeperIncentiveFlatRate("UNIV2WBTCDAI-A", 0);
+        DssExecLib.linearInterpolation({
+            _name:      "UNIV2WBTCDAI-A Offboarding",
+            _target:    DssExecLib.spotter(),
+            _ilk:       "UNIV2WBTCDAI-A",
+            _what:      "mat",
+            _startTime: block.timestamp,
+            _start:     CURRENT_UNIV2WBTCDAI_A_MAT,
+            _end:       TARGET_UNIV2WBTCDAI_A_MAT,
+            _duration:  30 days
+        });
     }
 }
 
