d68b98e49e7889e8bd89bfcb49ed2980c882ae7d
Julien Martin <julienmartinlevrai@protonmail.ch>
2022-06-29 14:14:46 +0200

    feat: deployed contract

--- src/test/config.sol
+++ src/test/config.sol
@@ -98,8 +98,8 @@ contract Config {
         // Values for spell-specific parameters
         //
         spellValues = SpellValues({
-            deployed_spell:                 address(0),        // populate with deployed spell if deployed
-            deployed_spell_created:         1655907493,        // use get-created-timestamp.sh if deployed
+            deployed_spell:                 address(0xD67A415a55A11C128a04fEd4B9C94cF61A633e7f),        // populate with deployed spell if deployed
+            deployed_spell_created:         1656504694,        // use get-created-timestamp.sh if deployed
             previous_spell:                 address(0),        // supply if there is a need to test prior to its cast() function being called on-chain.
             office_hours_enabled:           true,             // true if officehours is expected to be enabled in the spell
             expiration_threshold:           weekly_expiration  // (weekly_expiration,monthly_expiration) if weekly or monthly spell
