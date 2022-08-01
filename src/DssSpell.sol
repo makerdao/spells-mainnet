24f9c4e5e98f3ccff32395e129b349f9d50180f5
Julien Martin <julienmartinlevrai@protonmail.ch>
2022-08-01 19:54:17 +0200

    feat: custom DssExec with require for previous spell

--- src/DssSpell.sol
+++ src/DssSpell.sol
@@ -18,7 +18,7 @@ pragma solidity 0.6.12;
 // Enable ABIEncoderV2 when onboarding collateral through `DssExecLib.addNewCollateral()`
 // pragma experimental ABIEncoderV2;
 
-import "dss-exec-lib/DssExec.sol";
+// import "dss-exec-lib/DssExec.sol";
 import "dss-exec-lib/DssAction.sol";
 
 import { DssSpellCollateralAction } from "./DssSpellCollateral.sol";
@@ -32,14 +32,74 @@ interface RwaUrnLike {
     function draw(uint256) external;
 }
 
+interface DssExecLike {
+    function done() external view returns (bool);
+}
+
+contract DssExec {
+
+    Changelog      constant public log   = Changelog(0xdA0Ab1e0017DEbCd72Be8599041a2aa3bA7e740F);
+    uint256                 public eta;
+    bytes                   public sig;
+    bool                    public done;
+    bytes32       immutable public tag;
+    address       immutable public action;
+    uint256       immutable public expiration;
+    PauseAbstract immutable public pause;
+
+    address constant public PREVIOUS_SPELL = 0x67A45c2163798d47c32Fc2bcD5Dc0ABc6dCDFe78;
+
+    // Provides a descriptive tag for bot consumption
+    // This should be modified weekly to provide a summary of the actions
+    // Hash: seth keccak -- "$(wget https://<executive-vote-canonical-post> -q -O - 2>/dev/null)"
+    function description() external view returns (string memory) {
+        return SpellAction(action).description();
+    }
+
+    function officeHours() external view returns (bool) {
+        return SpellAction(action).officeHours();
+    }
+
+    function nextCastTime() external view returns (uint256 castTime) {
+        return SpellAction(action).nextCastTime(eta);
+    }
+
+    // @param _description  A string description of the spell
+    // @param _expiration   The timestamp this spell will expire. (Ex. now + 30 days)
+    // @param _spellAction  The address of the spell action
+    constructor(uint256 _expiration, address _spellAction) public {
+        pause       = PauseAbstract(log.getAddress("MCD_PAUSE"));
+        expiration  = _expiration;
+        action      = _spellAction;
+
+        sig = abi.encodeWithSignature("execute()");
+        bytes32 _tag;                    // Required for assembly access
+        address _action = _spellAction;  // Required for assembly access
+        assembly { _tag := extcodehash(_action) }
+        tag = _tag;
+    }
+
+    function schedule() public {
+        require(now <= expiration, "This contract has expired");
+        require(eta == 0, "This spell has already been scheduled");
+        eta = now + PauseAbstract(pause).delay();
+        pause.plot(action, tag, sig, eta);
+    }
+
+    function cast() public {
+        require(DssExecLike(PREVIOUS_SPELL).done(), "DssExec/previous-spell-not-cast");
+        require(!done, "spell-already-cast");
+        done = true;
+        pause.exec(action, tag, sig, eta);
+    }
+}
+
 contract DssSpellAction is DssAction, DssSpellCollateralAction {
     // Provides a descriptive tag for bot consumption
     // This should be modified weekly to provide a summary of the actions
-    // Hash: cast keccak -- "$(wget https://raw.githubusercontent.com/makerdao/community/969f04cfec25e56791fbe4503bcbe2df7a58df1e/governance/votes/Executive%20vote%20-%20July%2029%2C%202022.md -q -O - 2>/dev/null)"
+    // Hash: cast keccak -- "$(wget https://raw.githubusercontent.com/makerdao/community/TODO/governance/votes/Executive%20vote%20-%20August%203%2C%202022.md -q -O - 2>/dev/null)"
     string public constant override description =
-        "2022-07-29 MakerDAO Executive Spell | Hash: 0x18850080b101bb43125dd2bec32e5e7196586c5614bb9a0f05f9bbe392901d64";
-
-    address constant RWA_TOKEN_FAB = 0x2B3a4c18705e99bC29b22222dA7E10b643658552;
+        "2022-08-03 MakerDAO Executive Spell | Hash: TODO";
 
     uint256 constant RWA009_DRAW_AMOUNT = 25_000_000 * WAD;
 
@@ -54,22 +114,17 @@ contract DssSpellAction is DssAction, DssSpellCollateralAction {
     //
 
     function officeHours() public override returns (bool) {
-        return true;
+        return false;
     }
 
     function actions() public override {
 
         // ---------------------------------------------------------------------
         // Includes changes from the DssSpellCollateralAction
-        onboardNewCollaterals();
+        // onboardNewCollaterals();
         // offboardCollaterals();
 
         drawFromRWA009Urn();
-
-        // Add RWA_TOKEN_FAB to changelog
-        DssExecLib.setChangelogAddress("RWA_TOKEN_FAB", RWA_TOKEN_FAB);
-
-        DssExecLib.setChangelogVersion("1.13.3");
     }
 
     function drawFromRWA009Urn() internal {
