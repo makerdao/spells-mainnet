fce1ac6cb62807e34390baa61be136c45326f6a1
Sam MacPherson <xylom44@gmail.com>
2022-08-31 15:14:27 +0200

    update to deployed spell

--- src/test/config.sol
+++ src/test/config.sol
@@ -98,8 +98,8 @@ contract Config {
         // Values for spell-specific parameters
         //
         spellValues = SpellValues({
-            deployed_spell:                 address(0),        // populate with deployed spell if deployed
-            deployed_spell_created:         1661290827,        // use get-created-timestamp.sh if deployed
+            deployed_spell:                 address(0xB945b792A1a87308C11573e7E9739f792dBCe226),        // populate with deployed spell if deployed
+            deployed_spell_created:         1661950301,        // use get-created-timestamp.sh if deployed
             previous_spell:                 address(0),        // supply if there is a need to test prior to its cast() function being called on-chain.
             office_hours_enabled:           true,              // true if officehours is expected to be enabled in the spell
             expiration_threshold:           weekly_expiration  // (weekly_expiration,monthly_expiration) if weekly or monthly spell
