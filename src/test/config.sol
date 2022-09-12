51019a835ce64246c4f04a5a02a50c4f8ac8a422
Christopher Mooney <chris@dod.net>
2022-09-12 16:48:51 -0400

    new deploy script, no previous spell

--- src/test/config.sol
+++ src/test/config.sol
@@ -100,7 +100,7 @@ contract Config {
         spellValues = SpellValues({
             deployed_spell:                 address(0),        // populate with deployed spell if deployed
             deployed_spell_created:         0,                 // use get-created-timestamp.sh if deployed
-            previous_spell:                 address(0x07bFA4db5587A75617aFA9E43624b8C2609497b5),        // supply if there is a need to test prior to its cast() function being called on-chain.
+            previous_spell:                 address(0),        // supply if there is a need to test prior to its cast() function being called on-chain.
             office_hours_enabled:           false,             // true if officehours is expected to be enabled in the spell
             expiration_threshold:           weekly_expiration  // (weekly_expiration,monthly_expiration) if weekly or monthly spell
         });
