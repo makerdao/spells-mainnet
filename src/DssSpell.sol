5c7585371ebabbd25970c0776d3ec19d4dfbcf9d
Nazar Duchak <nazar@iovlabs.org>
2022-09-28 18:42:50 +0300

    feat: rwa007 onboarding]

--- src/DssSpell.sol
+++ src/DssSpell.sol
@@ -21,26 +21,24 @@ pragma solidity 0.6.12;
 import "dss-exec-lib/DssExec.sol";
 import "dss-exec-lib/DssAction.sol";
 
-// import { DssSpellCollateralAction } from "./DssSpellCollateral.sol";
+import { DssSpellCollateralAction } from "./DssSpellCollateral.sol";
 
-interface VestLike {
-    function restrict(uint256) external;
-    function create(address, uint256, uint256, uint256, uint256, address) external returns (uint256);
-    function vest(uint256) external;
+interface GemLike {
+    function approve(address, uint256) external returns (bool);
 }
 
-interface RwaLiquidationLike {
-    function ilks(bytes32) external returns (string memory, address, uint48, uint48);
-    function init(bytes32, uint256, string calldata, uint48) external;
+interface RwaUrnLike {
+    function lock(uint256) external;
+    function draw(uint256) external;
 }
 
-contract DssSpellAction is DssAction {
+contract DssSpellAction is DssAction, DssSpellCollateralAction {
     // Provides a descriptive tag for bot consumption
     // This should be modified weekly to provide a summary of the actions
-    // Hash: cast keccak -- "$(wget https://raw.githubusercontent.com/makerdao/community/a4eee9428aba7acf79d1b473db0fff7211405d62/governance/votes/Executive%20vote%20-%20September%2014%2C%202022.md -q -O - 2>/dev/null)"
+    // Hash: cast keccak -- "$(wget https://raw.githubusercontent.com/makerdao/community/TODO/governance/votes/TODO.md -q -O - 2>/dev/null)"
 
     string public constant override description =
-        "2022-09-14 MakerDAO Executive Spell | Hash: 0xe2f55ebabb2b0a86919f33226ad04586bd8fbc3298ecc4dd33d16aba25649c6b";
+        "2022-09-28 MakerDAO Executive Spell | Hash: TODO";
 
     // Many of the settings that change weekly rely on the rate accumulator
     // described at https://docs.makerdao.com/smart-contract-modules/rates-module
@@ -51,133 +49,21 @@ contract DssSpellAction is DssAction {
     // A table of rates can be found at
     //    https://ipfs.io/ipfs/QmVp4mhhbwWGTfbh2BzwQB9eiBrQBKiqcPRZCaAxNUaar6
     //
-    // --- Rates ---
 
-    // HVB (RWA009-A) legal update doc
-    string constant DOC = "QmQx3bMtjncka2jUsGwKu7ButuPJFn9yDEEvpg9xZ71ECh";
-
-    // wallet addresses
-    address internal constant DAIF_WALLET         = 0x34D8d61050Ef9D2B48Ab00e6dc8A8CA6581c5d63;
-    address internal constant DAIF_RESERVE_WALLET = 0x5F5c328732c9E52DfCb81067b8bA56459b33921f;
-    address internal constant ORA_WALLET          = 0x2d09B7b95f3F312ba6dDfB77bA6971786c5b50Cf;
-    address internal constant BIBTA_WALLET        = 0x173d85CD1754daD73cfc673944D9C8BF11A01D3F;
-    address internal constant MIP65_WALLET        = 0x29408abeCe474C85a12ce15B05efBB6A1e8587fe;
-
-    // dates (times in GMT)
-    uint256 constant JUL_01_2022 = 1656633600; // Friday,   July      1, 2022 12:00:00 AM
-    uint256 constant OCT_01_2022 = 1664582400; // Saturday, October   1, 2022 12:00:00 AM
-    uint256 constant OCT_31_2022 = 1667260799; // Monday,   October  31, 2022 11:59:59 PM
-    uint256 constant NOV_01_2022 = 1667260800; // Tuesday,  November  1, 2022 12:00:00 AM
-    uint256 constant JUN_30_2023 = 1688169599; // Friday,   June     30, 2023 11:59:59 PM
-    uint256 constant AUG_31_2023 = 1693526399; // Thursday, August   31, 2023 11:59:59 PM
-    uint256 constant DEC_31_2022 = 1672531199; // Saturday, December 31, 2022 11:59:59 PM
-
-    // math
-    uint256 internal constant WAD = 10**18;
-
-    // Turn office hours off
     function officeHours() public override returns (bool) {
-        return false;
+        return true;
     }
 
     function actions() public override {
         // ---------------------------------------------------------------------
         // Includes changes from the DssSpellCollateralAction
-        // onboardNewCollaterals();
-        // offboardCollaterals();
+        onboardNewCollaterals();
 
-        // ---------------------- CU DAI Vesting Streams -----------------------
-        // https://vote.makerdao.com/polling/QmQJ9hYq#poll-detail
-        VestLike daiVest = VestLike(
-            DssExecLib.getChangelogAddress("MCD_VEST_DAI")
-        );
-
-        // https://mips.makerdao.com/mips/details/MIP40c3SP74
-        // DAIF-001 | 2022-10-01 to 2022-10-31 | 67,863 DAI
-        daiVest.restrict(
-            daiVest.create(
-                DAIF_WALLET,                                             // usr
-                67_863 * WAD,                                            // tot
-                OCT_01_2022,                                             // bgn
-                OCT_31_2022 - OCT_01_2022,                               // tau
-                0,                                                       // eta
-                address(0)                                               // mgr
-            )
-        );
-
-        // https://mips.makerdao.com/mips/details/MIP40c3SP74
-        // DAIF-001 | 2022-11-01 to 2023-08-31 | 329,192 DAI
-        daiVest.restrict(
-            daiVest.create(
-                DAIF_WALLET,                                             // usr
-                329_192 * WAD,                                           // tot
-                NOV_01_2022,                                             // bgn
-                AUG_31_2023 - NOV_01_2022,                               // tau
-                0,                                                       // eta
-                address(0)                                               // mgr
-            )
-        );
-
-        // https://mips.makerdao.com/mips/details/MIP40c3SP74
-        // DAIF-001 | 2022-10-01 to 2022-12-31 | 270,000 DAI
-        daiVest.restrict(
-            daiVest.create(
-                DAIF_RESERVE_WALLET,                                     // usr
-                270_000 * WAD,                                           // tot
-                OCT_01_2022,                                             // bgn
-                DEC_31_2022 - OCT_01_2022,                               // tau
-                0,                                                       // eta
-                address(0)                                               // mgr
-            )
-        );
-
-        // https://mips.makerdao.com/mips/details/MIP40c3SP75
-        // ORA-001 | 2022-07-01 to 2023-06-30 | 2,337,804 DAI
-        daiVest.restrict(
-            daiVest.create(
-                ORA_WALLET,                                              // usr
-                2_337_804 * WAD,                                         // tot
-                JUL_01_2022,                                             // bgn
-                JUN_30_2023 - JUL_01_2022,                               // tau
-                0,                                                       // eta
-                address(0)                                               // mgr
-            )
-        );
-
-        // ---------------------- SPF Funding Transfers ------------------------
-        // https://forum.makerdao.com/t/mip55c3-sp6-legal-domain-work-on-greenlit-collateral-bibta-special-purpose-fund/17166
-        // https://vote.makerdao.com/polling/QmdaG8mo#vote-breakdown
-        DssExecLib.sendPaymentFromSurplusBuffer(BIBTA_WALLET, 50_000);
-        DssExecLib.sendPaymentFromSurplusBuffer(MIP65_WALLET, 30_000);
-
-        // ------------------- GRO-001 MKR Stream Clean-up ---------------------
-        // https://forum.makerdao.com/t/executive-inclusion-gro-001-mkr-vesting-stream-clean-up/17820
-        VestLike mkrVest = VestLike(
-            DssExecLib.getChangelogAddress("MCD_VEST_MKR_TREASURY")
-        );
-        mkrVest.vest(2);
-
-        // -------------------- Update HVB Legal Documents ---------------------
-        // https://forum.makerdao.com/t/poll-inclusion-request-hvbank-legal-update/17547
-        // https://vote.makerdao.com/polling/QmX81EhP#vote-breakdown
-        bytes32 ilk                      = "RWA009-A";
-        address MIP21_LIQUIDATION_ORACLE = DssExecLib.getChangelogAddress(
-            "MIP21_LIQUIDATION_ORACLE"
-        );
-
-        ( , address pip, uint48 tau, ) = RwaLiquidationLike(
-            MIP21_LIQUIDATION_ORACLE
-        ).ilks(ilk);
-
-        require(pip != address(0), "Abort spell execution: pip must be set");
-
-        // Init the RwaLiquidationOracle to reset the doc
-        RwaLiquidationLike(MIP21_LIQUIDATION_ORACLE).init(
-            ilk,       // ilk to update
-            0,         // price ignored if init() has already been called
-            DOC,       // new legal document
-            tau        // old tau value
-        );
+        // lock RWA007 Token in the URN
+        GemLike(RWA007).approve(RWA007_A_URN, 1 * WAD);
+        RwaUrnLike(RWA007_A_URN).lock(1 * WAD);
+        
+        DssExecLib.setChangelogVersion("1.14.1");
     }
 }
 
--- src/DssSpellCollateral.sol
+++ src/DssSpellCollateral.sol
@@ -18,6 +18,45 @@ pragma solidity 0.6.12;
 // Enable ABIEncoderV2 when onboarding collateral through `DssExecLib.addNewCollateral()`
 // pragma experimental ABIEncoderV2;
 
+import "dss-exec-lib/DssExecLib.sol";
+import "dss-interfaces/dss/ChainlogAbstract.sol";
+import "dss-interfaces/dss/GemJoinAbstract.sol";
+import "dss-interfaces/dss/IlkRegistryAbstract.sol";
+import "dss-interfaces/ERC/GemAbstract.sol";
+
+interface RwaLiquidationLike {
+    function ilks(bytes32) external returns (string memory, address, uint48, uint48);
+    function init(bytes32, uint256, string calldata, uint48) external;
+}
+
+interface RwaUrnLike {
+    function wards(address) external view returns(uint256);
+    function vat() external view returns(address);
+    function jug() external view returns(address);
+    function gemJoin() external view returns(address);
+    function daiJoin() external view returns(address);
+    function outputConduit() external view returns(address);
+    function hope(address) external;
+}
+
+interface RwaOutputConduitLike {
+    function wards(address) external view returns(uint256);
+    function dai() external view returns(address);
+    function psm() external view returns(address);
+    function quitTo() external view returns(address);
+    function hope(address) external;
+    function mate(address) external;
+    function kiss(address) external;
+}
+
+interface RwaInputConduitLike {
+    function wards(address) external view returns(uint256);
+    function dai() external view returns(address);
+    function psm() external view returns(address);
+    function to() external view returns(address);
+    function mate(address usr) external;
+    function file(bytes32 what, address data) external;
+}
 contract DssSpellCollateralAction {
     // --- Rates ---
     // Many of the settings that change weekly rely on the rate accumulator
@@ -27,11 +66,176 @@ contract DssSpellCollateralAction {
     // $ bc -l <<< 'scale=27; e( l(1.08)/(60 * 60 * 24 * 365) )'
     //
     // A table of rates can be found at
-    //    https://ipfs.io/ipfs/QmX2QMoM1SZq2XMoTbMak8pZP86Y2icpgPAKDjQg4r4YHn
+    //    https://ipfs.io/ipfs/QmVp4mhhbwWGTfbh2BzwQB9eiBrQBKiqcPRZCaAxNUaar6
+
+    uint256 internal constant ZERO_PCT_RATE                  = 1000000000000000000000000000;
+
+    // --- Math ---
+    uint256 internal constant WAD = 10**18;
+
+    // -- RWA007 MIP21 components --
+    address internal constant RWA007                         = 0x078fb926b041a816FaccEd3614Cf1E4bc3C723bD;
+    address internal constant MCD_JOIN_RWA007_A              = 0x476aaD14F42469989EFad0b7A31f07b795FF0621;
+    address internal constant RWA007_A_URN                   = 0x481bA2d2e86a1c41427893899B5B0cEae41c6726;
+    address internal constant RWA007_A_JAR                   = 0xef1B095F700BE471981aae025f92B03091c3AD47;
+    // Goerli: Coinbase / Mainnet: Coinbase
+    address internal constant RWA007_A_OUTPUT_CONDUIT        = 0x701C3a384c613157bf473152844f368F2d6EF191;
+    // Jar and URN Input Conduits
+    address internal constant RWA007_A_INPUT_CONDUIT_URN     = 0x58f5e979eF74b60a9e5F955553ab8e0e65ba89c9;
+    address internal constant RWA007_A_INPUT_CONDUIT_JAR     = 0xc8bb4e2B249703640e89265e2Ae7c9D5eA2aF742;
+
+    // MIP21_LIQUIDATION_ORACLE params
+    string  internal constant RWA007_DOC                     = "QmRe77P2JsvQWygVr9ZAMs4SHnjUQXz6uawdSboAaj2ryF"; // TODO
+    // There is no DssExecLib helper, so WAD precision is used.
+    uint256 internal constant RWA007_A_INITIAL_PRICE         = 250_000_000 * WAD;
+    uint48  internal constant RWA007_A_TAU                   = 0;
+
+    // Ilk registry params
+    uint256 internal constant RWA007_REG_CLASS_RWA           = 3;
+
+    // Remaining params
+    uint256 internal constant RWA007_A_LINE                  = 1_000_000;
+    uint256 internal constant RWA007_A_MAT                   = 100_00; // 100% in basis-points
+    uint256 internal constant RWA007_A_RATE                  = ZERO_PCT_RATE;
+
+    // Monetalis operator address
+    address internal constant RWA007_A_OPERATOR              = 0x94cfBF071f8be325A5821bFeAe00eEbE9CE7c279;
+    // Coinbase custody address
+    address internal constant RWA007_A_COINBASE_CUSTODY      = 0xC3acf3B96E46Aa35dBD2aA3BD12D23c11295E774;
+
+    // -- RWA007 END --
+
+    function onboardRwa007(
+        ChainlogAbstract CHANGELOG,
+        IlkRegistryAbstract REGISTRY,
+        address MIP21_LIQUIDATION_ORACLE,
+        address MCD_VAT,
+        address MCD_JUG,
+        address MCD_SPOT,
+        address MCD_JOIN_DAI,
+        address MCD_PSM_USDC_A
+    ) internal {
+        // RWA007-A collateral deploy
+        bytes32 ilk      = "RWA007-A";
+        uint256 decimals = GemAbstract(RWA007).decimals();
+
+        // Sanity checks
+        require(GemJoinAbstract(MCD_JOIN_RWA007_A).vat()                             == MCD_VAT,                 "join-vat-not-match");
+        require(GemJoinAbstract(MCD_JOIN_RWA007_A).ilk()                             == ilk,                     "join-ilk-not-match");
+        require(GemJoinAbstract(MCD_JOIN_RWA007_A).gem()                             == RWA007,                  "join-gem-not-match");
+        require(GemJoinAbstract(MCD_JOIN_RWA007_A).dec()                             == decimals,                "join-dec-not-match");
+
+        require(RwaUrnLike(RWA007_A_URN).vat()                                       == MCD_VAT,                 "urn-vat-not-match");
+        require(RwaUrnLike(RWA007_A_URN).jug()                                       == MCD_JUG,                 "urn-jug-not-match");
+        require(RwaUrnLike(RWA007_A_URN).daiJoin()                                   == MCD_JOIN_DAI,            "urn-daijoin-not-match");
+        require(RwaUrnLike(RWA007_A_URN).gemJoin()                                   == MCD_JOIN_RWA007_A,       "urn-gemjoin-not-match");
+        require(RwaUrnLike(RWA007_A_URN).outputConduit()                             == RWA007_A_OUTPUT_CONDUIT, "urn-outputconduit-not-match");
+        require(RwaUrnLike(RWA007_A_URN).wards(address(this))                        == 1,                       "pause-proxy-not-relyed-on-urn");
+
+        require(RwaOutputConduitLike(RWA007_A_OUTPUT_CONDUIT).psm()                  == MCD_PSM_USDC_A,          "output-conduit-psm-not-match");
+        require(RwaOutputConduitLike(RWA007_A_OUTPUT_CONDUIT).quitTo()               == RWA007_A_URN,            "output-conduit-quit-to-not-match");
+        require(RwaOutputConduitLike(RWA007_A_OUTPUT_CONDUIT).wards(address(this))   == 1,                       "pause-proxy-not-relyed-on-output-conduit");
+
+        require(RwaInputConduitLike(RWA007_A_INPUT_CONDUIT_URN).psm()                == MCD_PSM_USDC_A,          "input-conduit-urn-psm-not-match");
+        require(RwaInputConduitLike(RWA007_A_INPUT_CONDUIT_URN).to()                 == RWA007_A_URN,            "input-conduit-urn-to-not-match");
+        require(RwaInputConduitLike(RWA007_A_INPUT_CONDUIT_URN).wards(address(this)) == 1,                       "pause-proxy-not-relyed-on-input-conduit-urn");
+
+        require(RwaInputConduitLike(RWA007_A_INPUT_CONDUIT_JAR).psm()                == MCD_PSM_USDC_A,          "input-conduit-jar-psm-not-match");
+        require(RwaInputConduitLike(RWA007_A_INPUT_CONDUIT_JAR).to()                 == RWA007_A_JAR,            "input-conduit-har-to-not-match");
+        require(RwaInputConduitLike(RWA007_A_INPUT_CONDUIT_JAR).wards(address(this)) == 1,                       "pause-proxy-not-relyed-on-input-conduit-jar");
 
 
-    function onboardNewCollaterals() internal {}
+        // Init the RwaLiquidationOracle
+        RwaLiquidationLike(MIP21_LIQUIDATION_ORACLE).init(ilk, RWA007_A_INITIAL_PRICE, RWA007_DOC, RWA007_A_TAU);
+        (, address pip, , ) = RwaLiquidationLike(MIP21_LIQUIDATION_ORACLE).ilks(ilk);
 
-    function offboardCollaterals() internal {}
+        // Set price feed for RWA007
+        DssExecLib.setContract(MCD_SPOT, ilk, "pip", pip);
+
+        // Init RWA007 in Vat
+        Initializable(MCD_VAT).init(ilk);
+        // Init RWA007 in Jug
+        Initializable(MCD_JUG).init(ilk);
+
+        // Allow RWA007 Join to modify Vat registry
+        DssExecLib.authorize(MCD_VAT, MCD_JOIN_RWA007_A);
+
+        // 100m debt ceiling
+        DssExecLib.increaseIlkDebtCeiling(ilk, RWA007_A_LINE, /* _global = */ true);
+
+        // Set the stability fee
+        DssExecLib.setIlkStabilityFee(ilk, RWA007_A_RATE, /* _doDrip = */ false);
+
+        // Set collateralization ratio
+        DssExecLib.setIlkLiquidationRatio(ilk, RWA007_A_MAT);
+
+        // Poke the spotter to pull in a price
+        DssExecLib.updateCollateralPrice(ilk);
+
+        // Give the urn permissions on the join adapter
+        DssExecLib.authorize(MCD_JOIN_RWA007_A, RWA007_A_URN);
+
+        // MCD_PAUSE_PROXY and Monetalis permission on URN
+        RwaUrnLike(RWA007_A_URN).hope(address(this));
+        RwaUrnLike(RWA007_A_URN).hope(address(RWA007_A_OPERATOR));
+
+        // MCD_PAUSE_PROXY and Monetails permission on RWA007_A_OUTPUT_CONDUIT
+        RwaOutputConduitLike(RWA007_A_OUTPUT_CONDUIT).hope(address(this));
+        RwaOutputConduitLike(RWA007_A_OUTPUT_CONDUIT).mate(address(this));
+        RwaOutputConduitLike(RWA007_A_OUTPUT_CONDUIT).hope(RWA007_A_OPERATOR);
+        RwaOutputConduitLike(RWA007_A_OUTPUT_CONDUIT).mate(RWA007_A_OPERATOR);
+        // Coinbase custody whitelist for URN destination address
+        RwaOutputConduitLike(RWA007_A_OUTPUT_CONDUIT).kiss(address(RWA007_A_COINBASE_CUSTODY));
+
+        // MCD_PAUSE_PROXY and Monetails permission on RWA007_A_INPUT_CONDUIT_URN
+        RwaInputConduitLike(RWA007_A_INPUT_CONDUIT_URN).mate(address(this));
+        RwaInputConduitLike(RWA007_A_INPUT_CONDUIT_URN).mate(RWA007_A_OPERATOR);
+        // Set "quitTo" address for RWA007_A_INPUT_CONDUIT_URN
+        RwaInputConduitLike(RWA007_A_INPUT_CONDUIT_URN).file("quitTo", RWA007_A_COINBASE_CUSTODY);
+
+        // MCD_PAUSE_PROXY and Monetails permission on RWA007_A_INPUT_CONDUIT_JAR
+        RwaInputConduitLike(RWA007_A_INPUT_CONDUIT_JAR).mate(address(this));
+        RwaInputConduitLike(RWA007_A_INPUT_CONDUIT_JAR).mate(RWA007_A_OPERATOR);
+        // Set "quitTo" address for RWA007_A_INPUT_CONDUIT_JAR
+        RwaInputConduitLike(RWA007_A_INPUT_CONDUIT_JAR).file("quitTo", RWA007_A_COINBASE_CUSTODY);
+
+        // Add RWA007 contract to the changelog
+        CHANGELOG.setAddress("RWA007",                     RWA007);
+        CHANGELOG.setAddress("PIP_RWA007",                 pip);
+        CHANGELOG.setAddress("MCD_JOIN_RWA007_A",          MCD_JOIN_RWA007_A);
+        CHANGELOG.setAddress("RWA007_A_URN",               RWA007_A_URN);
+        CHANGELOG.setAddress("RWA007_A_JAR",               RWA007_A_JAR);
+        CHANGELOG.setAddress("RWA007_A_OUTPUT_CONDUIT",    RWA007_A_OUTPUT_CONDUIT);
+        CHANGELOG.setAddress("RWA007_A_INPUT_CONDUIT_URN", RWA007_A_INPUT_CONDUIT_URN);
+        CHANGELOG.setAddress("RWA007_A_INPUT_CONDUIT_JAR", RWA007_A_INPUT_CONDUIT_JAR);
+
+        // Add RWA007 to ILK REGISTRY
+        REGISTRY.put(
+            ilk,
+            MCD_JOIN_RWA007_A,
+            RWA007,
+            decimals,
+            RWA007_REG_CLASS_RWA,
+            pip,
+            address(0),
+            "RWA007-A: Monetalis Clydesdale",
+            GemAbstract(RWA007).symbol()
+        );
+    }
+
+    function onboardNewCollaterals() internal {
+        ChainlogAbstract CHANGELOG       = ChainlogAbstract(DssExecLib.LOG);
+        IlkRegistryAbstract REGISTRY     = IlkRegistryAbstract(DssExecLib.reg());
+        address MIP21_LIQUIDATION_ORACLE = CHANGELOG.getAddress("MIP21_LIQUIDATION_ORACLE");
+        address MCD_PSM_USDC_A           = CHANGELOG.getAddress("MCD_PSM_USDC_A");
+        address MCD_VAT                  = DssExecLib.vat();
+        address MCD_JUG                  = DssExecLib.jug();
+        address MCD_SPOT                 = DssExecLib.spotter();
+        address MCD_JOIN_DAI             = DssExecLib.daiJoin();
+
+        // --------------------------- RWA Collateral onboarding ---------------------------
+
+        // Onboard Monetalis: https://vote.makerdao.com/polling/TODO
+        onboardRwa007(CHANGELOG, REGISTRY, MIP21_LIQUIDATION_ORACLE, MCD_VAT, MCD_JUG, MCD_SPOT, MCD_JOIN_DAI, MCD_PSM_USDC_A);
+    }
 }
-
