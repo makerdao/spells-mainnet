de34b7984fa7b8f894ba763ac921df573064ae19
Nazzareno Massari <nas@naszam.co>
2022-10-14 13:04:41 +0100

    Add `ETHERSCAN_API_KEY` check  in `deploy.sh`

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,7 @@
 set -e
 
 [[ "$ETH_RPC_URL" && "$(cast chain)" == "ethlive" && "$(cast chain-id)" == "1" ]] || { echo "Please set a mainnet ETH_RPC_URL"; exit 1; }
+[[ "$ETHERSCAN_API_KEY" ]] || { echo "Please set a ETHERSCAN_API_KEY"; exit 1; }
 
 make && \
   dapp create DssSpell | \
