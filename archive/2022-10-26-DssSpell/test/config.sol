d4aed5eb3df170b55521ca4ef2a30d6ae36b4b7a
Arby <nofacedev@protonmail.com>
2022-10-27 01:19:14 +0400

    Fixed incorrect deployed spell address and timestamp in archive

--- archive/2022-10-26-DssSpell/test/config.sol
+++ archive/2022-10-26-DssSpell/test/config.sol
@@ -98,8 +98,8 @@ contract Config {
         // Values for spell-specific parameters
         //
         spellValues = SpellValues({
-            deployed_spell:                 address(0xd4e3C9D45C15881DB96A940DDE8E6Ab50296B43E),   // populate with deployed spell if deployed
-            deployed_spell_created:         1666806947,        // use `./scripts/get-created-timestamp.sh <deployment-tx>`
+            deployed_spell:                 address(0x4124b7a881DBBF98D72474D1A882d3afE3758526),   // populate with deployed spell if deployed
+            deployed_spell_created:         1666807691,        // use `./scripts/get-created-timestamp.sh <deployment-tx>`
             previous_spell:                 address(0),        // supply if there is a need to test prior to its cast() function being called on-chain.
             office_hours_enabled:           true,              // true if officehours is expected to be enabled in the spell
             expiration_threshold:           weekly_expiration  // (weekly_expiration,monthly_expiration) if weekly or monthly spell
