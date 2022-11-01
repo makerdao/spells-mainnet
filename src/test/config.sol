5a7437fd28bd354ead141ac9f1d2f73dcfa3576a
Brian McMichael <brian@brianmcmichael.com>
2022-11-01 10:29:57 -0400

    Clean up SpellValues

--- src/test/config.sol
+++ src/test/config.sol
@@ -98,11 +98,11 @@ contract Config {
         // Values for spell-specific parameters
         //
         spellValues = SpellValues({
-            deployed_spell:                 address(0),   // populate with deployed spell if deployed
-            deployed_spell_created:         0,        // use `./scripts/get-created-timestamp.sh <deployment-tx>`
-            previous_spell:                 address(0x4124b7a881DBBF98D72474D1A882d3afE3758526),        // supply if there is a need to test prior to its cast() function being called on-chain.
-            office_hours_enabled:           true,              // true if officehours is expected to be enabled in the spell
-            expiration_threshold:           weekly_expiration  // (weekly_expiration,monthly_expiration) if weekly or monthly spell
+            deployed_spell:                 address(0),         // populate with deployed spell if deployed
+            deployed_spell_created:         0,                  // use `./scripts/get-created-timestamp.sh <deployment-tx>`
+            previous_spell:                 address(0),         // supply if there is a need to test prior to its cast() function being called on-chain.
+            office_hours_enabled:           false,              // true if officehours is expected to be enabled in the spell
+            expiration_threshold:           30 days             // Amount of time before spell expires
         });
 
         //
