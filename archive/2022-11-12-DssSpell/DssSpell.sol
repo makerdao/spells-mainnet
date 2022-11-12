63267c5e67d6c61edfbfe8230c491d8d2a73c1eb
Christopher Mooney <chris@dod.net>
2022-11-12 15:31:52 -0500

    archive

---
+++ archive/2022-11-12-DssSpell/DssSpell.sol
@@ -0,0 +1,62 @@
+// SPDX-FileCopyrightText: © 2020 Dai Foundation <www.daifoundation.org>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+pragma solidity 0.6.12;
+// Enable ABIEncoderV2 when onboarding collateral through `DssExecLib.addNewCollateral()`
+// pragma experimental ABIEncoderV2;
+
+import "dss-exec-lib/DssExec.sol";
+import "dss-exec-lib/DssAction.sol";
+
+import { DssSpellCollateralAction } from "./DssSpellCollateral.sol";
+
+contract DssSpellAction is DssAction, DssSpellCollateralAction {
+    // Provides a descriptive tag for bot consumption
+    // This should be modified weekly to provide a summary of the actions
+    // Hash: cast keccak -- "$(wget https://raw.githubusercontent.com/makerdao/community/172ffd2b2d87db1fd0e40b2eeae4b45f1dcb8964/governance/votes/Executive%20vote%20-%20November%2012%2C%202022.md -q -O - 2>/dev/null)"
+
+    string public constant override description =
+        "2022-11-12 MakerDAO Executive Spell | Hash: 0xba560e92e2334a5d657a1f02e6ed42a93b83eac6681f1ec4c56d3bf229d6305c";
+
+
+    // Turn office hours off
+    function officeHours() public override returns (bool) {
+        return false;
+    }
+
+    // Many of the settings that change weekly rely on the rate accumulator
+    // described at https://docs.makerdao.com/smart-contract-modules/rates-module
+    // To check this yourself, use the following rate calculation (example 8%):
+    //
+    // $ bc -l <<< 'scale=27; e( l(1.08)/(60 * 60 * 24 * 365) )'
+    //
+    // A table of rates can be found at
+    //    https://ipfs.io/ipfs/QmVp4mhhbwWGTfbh2BzwQB9eiBrQBKiqcPRZCaAxNUaar6
+    //
+
+    // --- Rates ---
+
+    // --- Math ---
+
+    function actions() public override {
+        // Includes changes from the DssSpellCollateralAction
+        collateralAction();
+    }
+}
+
+contract DssSpell is DssExec {
+    constructor() DssExec(block.timestamp + 30 days, address(new DssSpellAction())) public {}
+}
