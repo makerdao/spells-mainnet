927f39543f525848dc9e97c24ffa2ccbd8346365
Brian McMichael <brian@brianmcmichael.com>
2022-12-02 15:07:13 -0500

    Add previous spell

--- src/test/config.sol
+++ src/test/config.sol
@@ -98,7 +98,7 @@ contract Config {
         spellValues = SpellValues({
             deployed_spell:                 address(0),         // populate with deployed spell if deployed
             deployed_spell_created:         0,                  // use `./scripts/get-created-timestamp.sh <deployment-tx>`
-            previous_spell:                 address(0),         // supply if there is a need to test prior to its cast() function being called on-chain.
+            previous_spell:                 address(0x08DbDBCF56aC587b4E6d6c6ecC5F242Ea569e5CB),         // supply if there is a need to test prior to its cast() function being called on-chain.
             office_hours_enabled:           true,               // true if officehours is expected to be enabled in the spell
             expiration_threshold:           30 days             // Amount of time before spell expires
         });
