a6ff7d293d877df5bba1cb558328b7b341d86f27
Brian McMichael <brian@brianmcmichael.com>
2022-12-05 12:15:57 -0500

    drip pot before cast

--- src/DssSpell.t.sol
+++ src/DssSpell.t.sol
@@ -97,7 +97,6 @@ contract DssSpellTest is DssSpellTestBase {
     }
 
     function testPayments() public { // make public to enable
-        uint256 prevSin = vat.sin(address(vow));
 
         // For each payment, create a Payee object with
         //    the Payee address,
@@ -141,7 +140,11 @@ contract DssSpellTest is DssSpellTestBase {
         }
 
         vote(address(spell));
-        scheduleWaitAndCast(address(spell));
+        spell.schedule();
+        hevm.warp(now + pause.delay());
+        pot.drip();
+        uint256 prevSin = vat.sin(address(vow));
+        spell.cast();
         assertTrue(spell.done());
 
         assertEq(vat.sin(address(vow)) - prevSin, totAmount * RAD, "testPayments/vat-sin-mismatch");
