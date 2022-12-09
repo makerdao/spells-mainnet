fda1e7b6c8b6f7506178c871343ab4caa87815b0
Brian L. McMichael <brian@brianmcmichael.com>
2022-12-09 12:39:15 -0500

    Update src/DssSpell.t.sol
    Co-authored-by: Arby <103920908+The-Arbiter@users.noreply.github.com>

--- src/DssSpell.t.sol
+++ src/DssSpell.t.sol
@@ -412,7 +412,7 @@ contract DssSpellTest is DssSpellTestBase {
 
 
     // Fail when contract code size exceeds 24576 bytes (a limit introduced in Spurious Dragon).
-    //  This contract may not be deployable on mainnet.
+    // This contract may not be deployable.
     //  Consider enabling the optimizer (with a low "runs" value!),
     //   turning off revert strings, or using libraries.
     function testContractSize() public {
