4101abe280ed54b446060359432284831a3dabe1
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2023-01-10 14:19:30 -0700

    add deployed spell

--- src/test/config.sol
+++ src/test/config.sol
@@ -96,8 +96,8 @@ contract Config {
         // Values for spell-specific parameters
         //
         spellValues = SpellValues({
-            deployed_spell:                 address(0),         // populate with deployed spell if deployed
-            deployed_spell_created:         0,                  // use `./scripts/get-created-timestamp.sh <deployment-tx>`
+            deployed_spell:                 address(0xB02FD2a51517C035D05b11272F769097dC2EaD87),         // populate with deployed spell if deployed
+            deployed_spell_created:         1673385455,                  // use `./scripts/get-created-timestamp.sh <deployment-tx>`
             previous_spell:                 address(0),         // supply if there is a need to test prior to its cast() function being called on-chain.
             office_hours_enabled:           false,              // true if officehours is expected to be enabled in the spell
             expiration_threshold:           30 days             // Amount of time before spell expires
