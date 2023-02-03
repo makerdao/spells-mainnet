472258894d2e80a01780780fd33c69eb24945343
Christopher Mooney <chris@dod.net>
2023-02-03 12:35:07 -0500

    published spell address

--- src/test/config.sol
+++ src/test/config.sol
@@ -96,8 +96,8 @@ contract Config {
         // Values for spell-specific parameters
         //
         spellValues = SpellValues({
-            deployed_spell:                 address(0),         // populate with deployed spell if deployed
-            deployed_spell_created:         0,                  // use `./scripts/get-created-timestamp.sh <deployment-tx>`
+            deployed_spell:                 address(0x2aC9F1c14Eec4B7955978f1E808B28dE337f8D20),         // populate with deployed spell if deployed
+            deployed_spell_created:         1675445519,                  // use `./scripts/get-created-timestamp.sh <deployment-tx>`
             previous_spell:                 address(0),         // supply if there is a need to test prior to its cast() function being called on-chain.
             office_hours_enabled:           false,              // true if officehours is expected to be enabled in the spell
             expiration_threshold:           30 days             // Amount of time before spell expires
