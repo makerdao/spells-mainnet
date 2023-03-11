f3d77c6955ec3c5f9906f5a0c49a1d664e9dc908
rockyfour <115617270+rockyfour@users.noreply.github.com>
2023-03-11 15:04:58 +0200

    Add spell

--- src/test/config.sol
+++ src/test/config.sol
@@ -97,9 +97,9 @@ contract Config {
         // Values for spell-specific parameters
         //
         spellValues = SpellValues({
-            deployed_spell:                 address(0),         // populate with deployed spell if deployed
-            deployed_spell_created:         0,                  // use `make deploy-info tx=<deployment-tx>` to obtain the timestamp
-            deployed_spell_block:           0,                  // use `make deploy-info tx=<deployment-tx>` to obtain the block number
+            deployed_spell:                 address(0x1D07F15aB8c3C015c7b312759dDd858b0f1D8e72),         // populate with deployed spell if deployed
+            deployed_spell_created:         1678539431,                  // use `make deploy-info tx=<deployment-tx>` to obtain the timestamp
+            deployed_spell_block:           16804986,                  // use `make deploy-info tx=<deployment-tx>` to obtain the block number
             previous_spell:                 address(0x4Fe8caf634004cb3Dd54AcD3F59c861FdC6de215),         // supply if there is a need to test prior to its cast() function being called on-chain.
             office_hours_enabled:           false,               // true if officehours is expected to be enabled in the spell
             expiration_threshold:           30 days             // Amount of time before spell expires
