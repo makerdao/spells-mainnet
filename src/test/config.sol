9a4468bb40941d06c4e252b9698a725d33bd936a
Kurt Barry <kurt.m.barry@gmail.com>
2023-03-14 10:31:54 -0400

    test with deployed spell

--- src/test/config.sol
+++ src/test/config.sol
@@ -102,9 +102,9 @@ contract Config {
         // Values for spell-specific parameters
         //
         spellValues = SpellValues({
-            deployed_spell:                 address(0),         // populate with deployed spell if deployed
-            deployed_spell_created:         0,                  // use `make deploy-info tx=<deployment-tx>` to obtain the timestamp
-            deployed_spell_block:           0,                  // use `make deploy-info tx=<deployment-tx>` to obtain the block number
+            deployed_spell:                 address(0xDae88B7E4b5aBa407C40676B98559f7aEC925817),         // populate with deployed spell if deployed
+            deployed_spell_created:         1678803119,                  // use `make deploy-info tx=<deployment-tx>` to obtain the timestamp
+            deployed_spell_block:           16826704,                  // use `make deploy-info tx=<deployment-tx>` to obtain the block number
             previous_spells:                prevSpells,         // older spells to ensure are executed first
             office_hours_enabled:           false,              // true if officehours is expected to be enabled in the spell
             expiration_threshold:           30 days             // Amount of time before spell expires
