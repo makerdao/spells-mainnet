d1b40cc22982974a74419dda48da2913a69e4173
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2023-04-04 15:43:20 -0600

    add deployed spell info

--- src/test/config.sol
+++ src/test/config.sol
@@ -102,9 +102,9 @@ contract Config {
         // Values for spell-specific parameters
         //
         spellValues = SpellValues({
-            deployed_spell:                 address(0),         // populate with deployed spell if deployed
-            deployed_spell_created:         0,         // use `make deploy-info tx=<deployment-tx>` to obtain the timestamp
-            deployed_spell_block:           0,           // use `make deploy-info tx=<deployment-tx>` to obtain the block number
+            deployed_spell:                 address(0xDF4F93454CbfC864A8f045Bd22566824360042B7),         // populate with deployed spell if deployed
+            deployed_spell_created:         1680644507,         // use `make deploy-info tx=<deployment-tx>` to obtain the timestamp
+            deployed_spell_block:           16978232,           // use `make deploy-info tx=<deployment-tx>` to obtain the block number
             previous_spells:                prevSpells,         // older spells to ensure are executed first
             office_hours_enabled:           false,               // true if officehours is expected to be enabled in the spell
             expiration_threshold:           30 days             // Amount of time before spell expires
