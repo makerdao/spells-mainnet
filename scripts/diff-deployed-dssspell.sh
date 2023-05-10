72be6a474408f3471dd676372a4dd3ef22f56603
amusingaxl <112016538+amusingaxl@users.noreply.github.com>
2023-05-10 17:37:10 -0300

    fix: address matching regex
    Allow addresses to be provided both like `address(0x...)` and `0x....`

--- scripts/diff-deployed-dssspell.sh
+++ scripts/diff-deployed-dssspell.sh
@@ -2,13 +2,21 @@
 
 set -e
 
+if ! command -v jq &> /dev/null
+then
+    echo '`jq` command could not be found. Please install it: https://github.com/stedolan/jq'
+    exit
+fi
+
+[[ "$ETHERSCAN_API_KEY" ]] || { echo -e "Please set ETHERSCAN_API_KEY"; exit 1; }
+
 if [[ "$1" =~  ^0x[0-9a-fA-F]{40}$ ]]; then
     deployed_spell_address=$1
 else
     # Read contract address from config.sol
-    deployed_spell_address=$(grep -oE 'deployed_spell:\s+address\((0x[a-fA-F0-9]+)\)' "src/test/config.sol" | sed -E 's/^.*\((.*)\)/\1/')
+    deployed_spell_address=$(grep -oE 'deployed_spell:\s*(address\((0x[a-fA-F0-9]{40})\)|(0x[a-fA-F0-9]{40}))' "src/test/config.sol" | sed -E 's/.*(0x[a-fA-F0-9]{40}).*/\1/')
     # Check if contract address, block number, and timestamp are zero
-    [[ "$deployed_spell_address" =~ ^(address\(0\)|0)$ ]] && { echo "DssSpell address is not set in config file."; exit 1; }
+    [[ "$deployed_spell_address" =~ ^(address\(0\)|0|0x0{40})$ ]] && { echo "DssSpell address is not set in config file."; exit 1; }
 fi
 
 make all && make flatten
@@ -17,7 +25,7 @@ spell_source="out/flat.sol"
 etherscan_source="out/etherscan.sol"
 
 # Download the deployed spell source code from Etherscan API
-curl -s "https://api.etherscan.io/api?module=contract&action=getsourcecode&address=${deployed_spell_address}" \
+curl -s "https://api.etherscan.io/api?module=contract&action=getsourcecode&address=${deployed_spell_address}&apikey=${ETHERSCAN_API_KEY}" \
     | jq -r '.result[0].SourceCode' > $etherscan_source
 
 # Compare the downloaded source code with the local spell
