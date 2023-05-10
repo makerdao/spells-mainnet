b52b2acbb00ebc66566ccdb772cec9e9d0505f34
0xDecr1pto <0xdecr1pto@proton.me>
2023-05-10 23:27:35 +0300

    chore: add deploy info

--- src/test/config.sol
+++ src/test/config.sol
@@ -102,11 +102,11 @@ contract Config {
         // Values for spell-specific parameters
         //
         spellValues = SpellValues({
-            deployed_spell:                 address(0),         // populate with deployed spell if deployed
-            deployed_spell_created:         0,         // use `make deploy-info tx=<deployment-tx>` to obtain the timestamp
-            deployed_spell_block:           0,           // use `make deploy-info tx=<deployment-tx>` to obtain the block number
+            deployed_spell:                 address(0x57C8ea2995a1277B560704792155690FA8a98643),     // populate with deployed spell if deployed
+            deployed_spell_created:         1683750203,         // use `make deploy-info tx=<deployment-tx>` to obtain the timestamp
+            deployed_spell_block:           17232341,           // use `make deploy-info tx=<deployment-tx>` to obtain the block number
             previous_spells:                prevSpells,         // older spells to ensure are executed first
-            office_hours_enabled:           false,               // true if officehours is expected to be enabled in the spell
+            office_hours_enabled:           false,              // true if officehours is expected to be enabled in the spell
             expiration_threshold:           30 days             // Amount of time before spell expires
         });
 
