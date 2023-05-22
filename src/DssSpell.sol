f9c801546dee34b9be8dee78d0d96e6d8a4b2f71
0xDecr1pto <0xdecr1pto@proton.me>
2023-05-22 20:19:45 +0300

    chore: Add exec hash

--- src/DssSpell.sol
+++ src/DssSpell.sol
@@ -106,9 +106,9 @@ interface AaveOracleLike {
 contract DssSpellAction is DssAction {
     // Provides a descriptive tag for bot consumption
     // This should be modified weekly to provide a summary of the actions
-    // Hash: cast keccak -- "$(wget 'https://raw.githubusercontent.com/makerdao/community/TODO' -q -O - 2>/dev/null)"
+    // Hash: cast keccak -- "$(wget 'https://raw.githubusercontent.com/makerdao/community/ce40e721ba58dc631ee1b66f5259423dd8e504ce/governance/votes/Executive%20vote%20-%20May%2024%2C%202023.md' -q -O - 2>/dev/null)"
     string public constant override description =
-        "2023-05-24 MakerDAO Executive Spell | Hash: TODO";
+        "2023-05-24 MakerDAO Executive Spell | Hash: 0xfe3ea529455620ded327e3f6781e75c799567ce8d87824c6585671c8fe392946";
 
     // Set office hours according to the summary
     function officeHours() public pure override returns (bool) {
