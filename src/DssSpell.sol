2e31804abc87b136a581f45ca5182e0a5c025115
Sweaty Pumpkin <SidestreamSweatyPumpkin@sidestream.tech>
2023-06-14 08:37:31 +0200

    update hash

--- src/DssSpell.sol
+++ src/DssSpell.sol
@@ -71,9 +71,9 @@ interface RwaOutputConduitLike {
 contract DssSpellAction is DssAction {
     // Provides a descriptive tag for bot consumption
     // This should be modified weekly to provide a summary of the actions
-    // Hash: cast keccak -- "$(wget 'https://raw.githubusercontent.com/makerdao/community/master/governance/votes/Executive%20vote%20-%20June%207%2C%202023.md' -q -O - 2>/dev/null)"
+    // Hash: cast keccak -- "$(wget 'https://github.com/makerdao/community/blob/78ca48d951e0b1a80d233497dbbb8a0ebd63749d/governance/votes/Executive%20vote%20-%20June%2014%2C%202023.md' -q -O - 2>/dev/null)"
     string public constant override description =
-        "2023-06-07 MakerDAO Executive Spell | Hash: 0x90a57ed060ac67bdd9667f5fffffbb849b02e2b84e54809e048facc0a5211c11";
+        "2023-06-07 MakerDAO Executive Spell | Hash: 0xbaa10bcd6a57e0ea8c979de1a97b9f2c36651f4ae88dc143f032b1c447966c59";
 
     address internal immutable MCD_GOV = DssExecLib.mkr();
     address internal immutable MIP21_LIQUIDATION_ORACLE = DssExecLib.getChangelogAddress("MIP21_LIQUIDATION_ORACLE");
