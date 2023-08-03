f5c5c08dadd287d982fe397407547835564c44fc
amusingaxl <112016538+amusingaxl@users.noreply.github.com>
2023-08-03 22:35:10 +0200

    fix: archive with missing deployed addresses

--- archive/2023-08-02-DssSpell/test/config.sol
+++ archive/2023-08-02-DssSpell/test/config.sol
@@ -99,9 +99,9 @@ contract Config {
         // Values for spell-specific parameters
         //
         spellValues = SpellValues({
-            deployed_spell:                 address(0),         // populate with deployed spell if deployed
-            deployed_spell_created:         0,                  // use `make deploy-info tx=<deployment-tx>` to obtain the timestamp
-            deployed_spell_block:           0,                  // use `make deploy-info tx=<deployment-tx>` to obtain the block number
+            deployed_spell:                 address(0x73603dB34814b22379CeD3d2Cbb450B3968Fd892),         // populate with deployed spell if deployed
+            deployed_spell_created:         1691091323,                  // use `make deploy-info tx=<deployment-tx>` to obtain the timestamp
+            deployed_spell_block:           17836588,                  // use `make deploy-info tx=<deployment-tx>` to obtain the block number
             previous_spells:                prevSpells,         // older spells to ensure are executed first
             office_hours_enabled:           false,              // true if officehours is expected to be enabled in the spell
             expiration_threshold:           30 days             // Amount of time before spell expires
