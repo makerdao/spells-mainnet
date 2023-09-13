9c54b72bbaf2aaebef5750e745f774f9a69d4a88
SidestreamSweatyPumpkin <132688698+SidestreamSweatyPumpkin@users.noreply.github.com>
2023-09-13 16:44:28 +0200

    Update src/DssSpell.sol
    Co-authored-by: amusingaxl <112016538+amusingaxl@users.noreply.github.com>

--- src/DssSpell.sol
+++ src/DssSpell.sol
@@ -69,9 +69,9 @@ contract DssSpellAction is DssAction {
     //
     // uint256 internal constant X_PCT_RATE      = ;
 
-    uint256 internal constant THREE_PT_FOUR_FIVE_PCT_RATE   = 1000000001075539644270067964;
-    uint256 internal constant THREE_PT_SEVEN_ZERO_PCT_RATE  = 1000000001152077919467240095;
-    uint256 internal constant FOUR_PT_TWO_ZERO_PCT_RATE     = 1000000001304602465690389263;
+    uint256 internal constant THREE_PT_FOUR_FIVE_PCT_RATE  = 1000000001075539644270067964;
+    uint256 internal constant THREE_PT_SEVEN_ZERO_PCT_RATE = 1000000001152077919467240095;
+    uint256 internal constant FOUR_PT_TWO_ZERO_PCT_RATE    = 1000000001304602465690389263;
 
     // ----------- MKR transfer Addresses -----------
     address internal constant DECO_WALLET = 0xF482D1031E5b172D42B2DAA1b6e5Cbf6519596f7;
