b5c84c087ded47e3477d711c2d4a5de1fe79bad7
SidestreamColdMelon <SASCoreUnit+1@gmail.com>
2023-11-24 10:28:45 +0100

    add comments

--- src/DssSpell.sol
+++ src/DssSpell.sol
@@ -43,7 +43,58 @@ contract DssSpellAction is DssAction {
     //
     // uint256 internal constant X_PCT_RATE      = ;
 
-    function actions() public override {}
+    function actions() public override {
+        // ---------- Stability Fee Changes ----------
+        // Forum: https://forum.makerdao.com/t/stability-scope-parameter-changes-7/22882#increase-rwa014-a-coinbase-custody-debt-ceiling-9
+
+        // Decrease the WBTC-A Stability Fee (SF) by 0.07%, from 5.86% to 5.79%
+        // TODO
+
+        // Decrease the WBTC-B Stability Fee (SF) by 0.07%, from 6.36% to 6.29%
+        // TODO
+
+        // Decrease the WBTC-C Stability Fee (SF) by 0.07%, from 5.61% to 5.54%
+        // TODO
+
+        // ---------- Reduce PSM-GUSD-A Debt Ceiling ----------
+        // Forum: https://forum.makerdao.com/t/stability-scope-parameter-changes-7/22882#increase-rwa014-a-coinbase-custody-debt-ceiling-9
+
+        // Decrease the PSM-GUSD-A DC-IAM LINE (max DC) by 110M DAI, from 110M to 0
+        // TODO
+
+        // ---------- Increase RWA014-A (Coinbase Custody) Debt Ceiling ----------
+        // Forum: https://forum.makerdao.com/t/stability-scope-parameter-changes-7/22882#increase-rwa014-a-coinbase-custody-debt-ceiling-9
+
+        // Increase the RWA014-A (Coinbase Custody) debt ceiling by 1b DAI, from 500M to 1.5b
+        // TODO
+
+        // ---------- SBE parameter changes ----------
+        // Forum: https://forum.makerdao.com/t/smart-burn-engine-transaction-analysis-parameter-reconfiguration-update-3/22876
+
+        // Increase bump by 10,000, from 20,000 to 30,000
+        // TODO
+
+        // Increase hop by 9,460, from 6,308 to 15,768
+        // TODO
+
+        // ---------- RWA Foundation Service Provider Changes ----------
+        // Forum: https://forum.makerdao.com/t/dao-resolution-rwa-foundation-service-provider-changes/22866
+
+        // Approve Dao resolution with IPFS hash QmPiEHtt8rkVtSibBXMrhEzHUmSriXWz4AL2bjscq8dUvU
+        // TODO
+
+        // ---------- Andromeda Legal Expenses ----------
+        // Forum: https://forum.makerdao.com/t/project-andromeda-legal-expenses-ii/22577/4
+
+        // Transfer 201,738 Dai to 0xc4dB894A11B1eACE4CDb794d0753A3cB7A633767
+        // TODO
+
+        // ---------- Trigger Spark Proxy Spell ----------
+        // Forum: https://forum.makerdao.com/t/accounting-discrepancy-in-the-dai-market/22845/2
+
+        // Mainnet - 0x68a075249fA77173b8d1B92750c9920423997e2B
+        // TODO
+    }
 }
 
 contract DssSpell is DssExec {
