d6238612319ca527a5606c10bf6ab2942a4c4919
SidestreamColdMelon <SASCoreUnit+1@gmail.com>
2023-12-04 10:35:35 +0100

    add support for Darwin platform

--- scripts/time.sh
+++ scripts/time.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+SYSTEM=`uname`;
+FORMAT="%Y-%m-%d %H:%M:%S";
+
 for ARGUMENT in "$@"
 do
     KEY=$(echo "$ARGUMENT" | cut -f1 -d=)
@@ -16,15 +19,25 @@ done
 if [[ -z "$STAMP" && -z "$DATE" ]]; then
 cat <&2 <<.
 Please use the date or stamp flag, ex:
-    ${0##*/} date="1 May 2022"
+    ${0##*/} date="2023-12-01 00:00:00"
         or
     ${0##*/} stamp="1651363200"
 .
     exit 1
 elif [[ -z "$DATE" ]]; then
-    date -u -d @"$STAMP"
-    date -u -d @"$STAMP" +%s
+    if [ "$SYSTEM" = "Darwin" ]; then
+        date -u -r "$STAMP"
+        date -u -r "$STAMP" "+$FORMAT"
+    else
+        date -u -d @"$STAMP"
+        date -u -d @"$STAMP" +%s
+    fi
 else
-    date --utc --date="$DATE"
-    date --utc --date="$DATE" +%s
+    if [ "$SYSTEM" = "Darwin" ]; then
+        date -u -j -f "$FORMAT" "$DATE"
+        date -u -j -f "$FORMAT" "$DATE" +%s
+    else
+        date --utc --date="$DATE"
+        date --utc --date="$DATE" +%s
+    fi
 fi
