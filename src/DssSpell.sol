1cbb0ca496b166760d94c875f3099e4a3de2e5d6
SidestreamColdMelon <SASCoreUnit+1@gmail.com>
2024-01-18 20:18:19 +0100

    base spell

--- src/DssSpell.sol
+++ src/DssSpell.sol
@@ -43,7 +43,80 @@ contract DssSpellAction is DssAction {
     //
     // uint256 internal constant X_PCT_RATE = ;
 
-    function actions() public override {}
+    function actions() public override {
+        // ---------- Stability Fee Changes ----------
+        // Forum: https://forum.makerdao.com/t/stability-scope-parameter-changes-8/23445
+
+        // Increase the ETH-A Stability Fee (SF) by 1.49%, from 5.25% to 6.74%.
+        // TODO
+
+        // Increase the ETH-B Stability Fee (SF) by 1.49%, from 5.75% to 7.24%.
+        // TODO
+
+        // Increase the ETH-C Stability Fee (SF) by 1.49%, from 5.00% to 6.49%.
+        // TODO
+
+        // Increase the WSTETH-A Stability Fee (SF) by 1.91%, from 5.25% to 7.16%.
+        // TODO
+
+        // Increase the WSTETH-B Stability Fee (SF) by 1.91%, from 5.00% to 6.91%.
+        // TODO
+
+        // Increase the WBTC-A Stability Fee (SF) by 0.91%, from 5.79% to 6.70%.
+        // TODO
+
+        // Increase the WBTC-B Stability Fee (SF) by 0.91%, from 6.29% to 7.20%.
+        // TODO
+
+        // Increase the WBTC-C Stability Fee (SF) by 0.91%, from 5.54% to 6.45%.
+        // TODO
+
+        // ---------- Reduce PSM-PAX-A Debt Ceiling & Disable DC-IAM ----------
+        // Forum: https://forum.makerdao.com/t/stability-scope-parameter-changes-8/23445
+
+        // Remove PSM-PAX-A from Autoline.
+        // TODO
+
+        // Set PSM-PAX-A debt ceiling to 0
+        // TODO
+
+        // Reduce Global Debt Ceiling? Yes
+        // TODO
+
+        // ---------- RETH-A Offboarding Parameters Finalization ----------
+        // Forum: https://forum.makerdao.com/t/stability-scope-parameter-changes-8/23445
+
+        // Set chop to 0%.
+        // TODO
+
+        // Set tip to 0%
+        // TODO
+
+        // Set chip to 0%
+        // TODO
+
+        // Set Liquidation Ratio to 10,000%.
+        // TODO
+
+        // ---------- SBE parameter changes ----------
+        // Forum: https://forum.makerdao.com/t/smart-burn-engine-transaction-analysis-parameter-reconfiguration-update-4/23441
+
+        // Increase bump by 20,000, from 30,000 to 50,000 DAI
+        // TODO
+
+        // Increase hop by 10,512, from 15,768 to 26,280 Seconds
+        // TODO
+
+        // ---------- Trigger Spark Proxy Spell ----------
+        // Forum: https://forum.makerdao.com/t/jan-10-2024-proposed-changes-to-sparklend-for-upcoming-spell/23389
+        // Poll: https://vote.makerdao.com/polling/Qmc3NjZA
+        // Poll: https://vote.makerdao.com/polling/QmNrXB9P
+        // Poll: https://vote.makerdao.com/polling/QmTauEqL
+        // Forum: https://forum.makerdao.com/t/stability-scope-parameter-changes-8/23445
+
+        // Activate Spark Proxy Spell - TBD
+        // TODO
+    }
 }
 
 contract DssSpell is DssExec {
