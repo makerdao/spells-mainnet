2cc71e458f6ac851a977f429331a4d7982fcfac4
SidestreamColdMelon <SASCoreUnit+1@gmail.com>
2024-01-23 10:40:55 +0100

    add executive url and hash

--- src/DssSpell.sol
+++ src/DssSpell.sol
@@ -29,9 +29,9 @@ interface ProxyLike {
 contract DssSpellAction is DssAction {
     // Provides a descriptive tag for bot consumption
     // This should be modified weekly to provide a summary of the actions
-    // Hash: cast keccak -- "$(wget 'TODO' -q -O - 2>/dev/null)"
+    // Hash: cast keccak -- "$(wget 'https://raw.githubusercontent.com/makerdao/community/052330ed0eb8680dd9e4efaf92a4c056e46a4284/governance/votes/Executive%20vote%20-%20January%2024%2C%202024.md' -q -O - 2>/dev/null)"
     string public constant override description =
-        "2024-01-24 MakerDAO Executive Spell | Hash: TODO";
+        "2024-01-24 MakerDAO Executive Spell | Hash: 0x17cf7409b1ad2b06b8d3fdbe943ffc65ddc933b57674880c85b2a3d8168e1bf4";
 
     // Set office hours according to the summary
     function officeHours() public pure override returns (bool) {
