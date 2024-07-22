c6b418576ac36b9b13db25a80e4faa33436446c9
amusingaxl <112016538+amusingaxl@users.noreply.github.com>
2024-07-22 16:11:01 -0300

    chore: alignment adjustments

--- src/DssSpell.sol
+++ src/DssSpell.sol
@@ -66,19 +66,19 @@ contract DssSpellAction is DssAction {
     uint256 constant RAD      = 10 ** 45;
 
     // ---------- LITE-PSM-USDC-A Phase 1 ----------
-    address internal immutable MCD_PAUSE                     = DssExecLib.getChangelogAddress("MCD_PAUSE");
-    address internal immutable MCD_ESM                       = DssExecLib.getChangelogAddress("MCD_ESM");
-    address internal immutable USDC                          = DssExecLib.getChangelogAddress("USDC");
-    address internal immutable PIP_USDC                      = DssExecLib.getChangelogAddress("PIP_USDC");
-    address internal constant MCD_LITE_PSM_USDC_A            = 0xf6e72Db5454dd049d0788e411b06CfAF16853042;
-    address internal constant MCD_LITE_PSM_USDC_A_POCKET     = 0x37305B1cD40574E4C5Ce33f8e8306Be057fD7341;
-    address internal constant LITE_PSM_MOM               = 0x467b32b0407Ad764f56304420Cddaa563bDab425;
-    address internal constant MCD_LITE_PSM_USDC_A_JAR        = 0x69cA348Bd928A158ADe7aa193C133f315803b06e;
-    address internal constant MCD_LITE_PSM_USDC_A_IN_CDT_JAR = 0x5eeB3D8D60B06a44f6124a84EeE7ec0bB747BE6d;
+    address internal immutable MCD_PAUSE                      = DssExecLib.getChangelogAddress("MCD_PAUSE");
+    address internal immutable MCD_ESM                        = DssExecLib.getChangelogAddress("MCD_ESM");
+    address internal immutable USDC                           = DssExecLib.getChangelogAddress("USDC");
+    address internal immutable PIP_USDC                       = DssExecLib.getChangelogAddress("PIP_USDC");
+    address internal constant  MCD_LITE_PSM_USDC_A            = 0xf6e72Db5454dd049d0788e411b06CfAF16853042;
+    address internal constant  MCD_LITE_PSM_USDC_A_POCKET     = 0x37305B1cD40574E4C5Ce33f8e8306Be057fD7341;
+    address internal constant  LITE_PSM_MOM                   = 0x467b32b0407Ad764f56304420Cddaa563bDab425;
+    address internal constant  MCD_LITE_PSM_USDC_A_JAR        = 0x69cA348Bd928A158ADe7aa193C133f315803b06e;
+    address internal constant  MCD_LITE_PSM_USDC_A_IN_CDT_JAR = 0x5eeB3D8D60B06a44f6124a84EeE7ec0bB747BE6d;
 
-    // ---------- Deploy LitePSM keeper network job ----------
-    address internal immutable CRON_SEQUENCER   = DssExecLib.getChangelogAddress("CRON_SEQUENCER");
-    address internal constant CRON_LITE_PSM_JOB = 0x689cE517a4DfCf0C5eC466F2757D324fc292C8Be;
+    // ---------- Add LitePSM keeper network job ----------
+    address internal immutable CRON_SEQUENCER    = DssExecLib.getChangelogAddress("CRON_SEQUENCER");
+    address internal constant  CRON_LITE_PSM_JOB = 0x689cE517a4DfCf0C5eC466F2757D324fc292C8Be;
 
     // ---------- Spark Spell ----------
     // Spark Proxy: https://github.com/marsfoundation/sparklend-deployments/blob/bba4c57d54deb6a14490b897c12a949aa035a99b/script/output/1/primary-sce-latest.json#L2
@@ -89,27 +89,14 @@ contract DssSpellAction is DssAction {
         // ---------- LITE-PSM-USDC-A Phase 1 ----------
         // Forum: https://forum.makerdao.com/t/lite-psm-usdc-a-phase-1-test-period-proposed-parameters/24644
 
-        // LitePsm: 0xf6e72Db5454dd049d0788e411b06CfAF16853042
-        // LitePsm Pocket: 0x37305B1cD40574E4C5Ce33f8e8306Be057fD7341
-
-        // ---------- Update PSM-USDC-A DC-IAM ----------
-        // Forum: https://forum.makerdao.com/t/lite-psm-usdc-a-phase-1-test-period-proposed-parameters/24644
-
-        // DC-IAM line: 10B (Unchanged)
-        // DC-IAM gap: Decrease for 20M from 400M to 380M
-        // DC-IAM ttl: 12h (Unchanged)
-
         // ---------- PSM_MOM GSM Delay Exception ----------
-        // Forum: https://forum.makerdao.com/t/lite-psm-usdc-a-phase-1-test-period-proposed-parameters/24644
-
-        // Activate PSM_MOM GSM Delay Exception at 0x467b32b0407Ad764f56304420Cddaa563bDab425
         // Forum: https://forum.makerdao.com/t/lite-psm-usdc-a-phase-1-test-period-proposed-parameters/24644
 
         DssInstance memory dss = MCD.loadFromChainlog(DssExecLib.LOG);
         DssLitePsmInstance memory inst = DssLitePsmInstance({
-            // ---------- LITE-PSM-USDC-A Phase 1 ----------
             // Onboard MCD_LITE_PSM_USDC_A at 0xf6e72Db5454dd049d0788e411b06CfAF16853042
             litePsm: MCD_LITE_PSM_USDC_A,
+            // Activate PSM_MOM GSM Delay Exception at 0x467b32b0407Ad764f56304420Cddaa563bDab425
             mom:     LITE_PSM_MOM
         });
         DssLitePsmMigrationConfigPhase1 memory cfg = DssLitePsmMigrationConfigPhase1({
@@ -130,36 +117,35 @@ contract DssSpellAction is DssAction {
 
             dstIlk:       "LITE-PSM-USDC-A",
             // buf: Set to 20M
-            dstBuf:       20 * MILLION * WAD,
+            dstBuf:        20 * MILLION * WAD,
             // Onboard MCD_LITE_PSM_USDC_A_POCKET at 0x37305B1cD40574E4C5Ce33f8e8306Be057fD7341
             dstPocket:    MCD_LITE_PSM_USDC_A_POCKET,
 
             // ---------- Update PSM-USDC-A DC-IAM ----------
 
             // DC-IAM line: Set to 50M
-            dstMaxLine:   50 * MILLION * RAD,
+            dstMaxLine:    50 * MILLION * RAD,
             // DC-IAM gap: Set to 20M
-            dstGap:       20 * MILLION * RAD,
+            dstGap:        20 * MILLION * RAD,
             // DC-IAM ttl: Set to 12h
-            dstTtl:       12 hours,
+            dstTtl:        12 hours,
 
             // ---------- Set up LITE-PSM-USDC-A DC-IAM ----------
 
             // DC-IAM line: 10B (Unchanged)
-            srcMaxLine:   10 * BILLION * RAD,
+            srcMaxLine:    10 * BILLION * RAD,
             // DC-IAM gap: Decrease for 20M from 400M to 380M
             srcGap:       380 * MILLION * RAD,
             // DC-IAM ttl: 12h (Unchanged)
-            srcTtl:       12 hours,
+            srcTtl:        12 hours,
 
             // ---------- Initial USDC Migration from PSM-USDC-A to LitePSM ----------
             // Migrate 20 million USDC from PSM-USDC-A to LITE-PSM-USDC-A
-            dstWant:      20 * MILLION * WAD,
+            dstWant:       20 * MILLION * WAD,
             // Leave at least 200M USDC reserves in PSM-USDC-A
             srcKeep:      200 * MILLION * WAD
         });
         DssLitePsmMigrationPhase1.initAndMigrate(dss, inst, cfg);
-
 
         // ---------- GSM Delay Update ----------
         // Forum: https://forum.makerdao.com/t/lite-psm-usdc-a-phase-1-test-period-proposed-parameters/24644
@@ -173,8 +159,7 @@ contract DssSpellAction is DssAction {
         // Increase the ESM (Emergency Shutdown Module) minimum threshold by 150k MKR from 150k MKR to 300k MKR
         DssExecLib.setValue(MCD_ESM, "min", 300 * THOUSAND * WAD);
 
-
-        // ---------- Deploy LitePSM keeper network job ----------
+        // ---------- Add LitePSM keeper network job ----------
         // Forum: https://forum.makerdao.com/t/lite-psm-usdc-a-phase-1-test-period-proposed-parameters/24644
 
         // sequencer.addJob(0x689cE517a4DfCf0C5eC466F2757D324fc292C8Be)
@@ -190,6 +175,7 @@ contract DssSpellAction is DssAction {
         DssExecLib.setChangelogVersion("1.17.5");
 
         // ---------- Spark Spell ----------
+        // Forum: https://forum.makerdao.com/t/jul-12-2024-proposed-changes-to-spark-for-upcoming-spell/24635
 
         // Trigger Spark Proxy Spell at 0x18427dB17D3113309a0406284aC738f4E649613B
         ProxyLike(SPARK_PROXY).exec(SPARK_SPELL, abi.encodeWithSignature("execute()"));
