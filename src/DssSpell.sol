d56c61a4ff8f4837d6fad55d1e59153ee97c52fa
lucas-manuel <lucasmanuel.tech@gmail.com>
2024-10-25 16:56:04 -0400

    feat: update solc version

--- src/DssSpell.sol
+++ src/DssSpell.sol
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-pragma solidity 0.8.16;
+pragma solidity ^0.8.16;
 
 import "dss-exec-lib/DssExec.sol";
 import "dss-exec-lib/DssAction.sol";
