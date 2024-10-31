88207c71952467a794b66b7d7850659cf747e4a5
0xDecr1pto <0xdecr1pto@proton.me>
2024-10-31 19:20:52 +0200

    chore: reorder var to be able to reverify contract on ethererscan

--- src/DssSpell.sol
+++ src/DssSpell.sol
@@ -72,8 +72,8 @@ contract DssSpellAction is DssAction {
 
     // ---------- BASE Token Bridge ----------
     // Mainnet addresses
-    address internal constant BASE_GOV_RELAY           = 0x1Ee0AE8A993F2f5abDB51EAF4AC2876202b65c3b;
     address internal constant BASE_ESCROW              = 0x7F311a4D48377030bD810395f4CCfC03bdbe9Ef3;
+    address internal constant BASE_GOV_RELAY           = 0x1Ee0AE8A993F2f5abDB51EAF4AC2876202b65c3b;
     address internal constant BASE_TOKEN_BRIDGE        = 0xA5874756416Fa632257eEA380CAbd2E87cED352A;
     address internal constant BASE_TOKEN_BRIDGE_IMP    = 0xaeFd31c2e593Dc971f9Cb42cBbD5d4AD7F1970b6;
     address internal constant MESSENGER                = 0x866E82a600A1414e583f7F13623F1aC5d58b0Afa;
