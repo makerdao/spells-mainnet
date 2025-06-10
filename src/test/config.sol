748b7b46b65fb3ef3689355b14238589a3232bc8
SidestreamIcedMango <SASCoreUnit+icedMango@gmail.com>
2025-06-10 10:12:58 +0200

    cleanup

--- src/test/config.sol
+++ src/test/config.sol
@@ -117,11 +117,11 @@ contract Config {
         // Values for spell-specific parameters
         //
         spellValues = SpellValues({
-            deployed_spell:         address(0x86F5A13C95ecf7263634b6e14E483d05D7d4aA0B), // populate with deployed spell if deployed
-            deployed_spell_created: 1748550983, // use `make deploy-info tx=<deployment-tx>` to obtain the timestamp
-            deployed_spell_block:   22590824,   // use `make deploy-info tx=<deployment-tx>` to obtain the block number
+            deployed_spell:         address(0), // populate with deployed spell if deployed
+            deployed_spell_created: 0,          // use `make deploy-info tx=<deployment-tx>` to obtain the timestamp
+            deployed_spell_block:   0,          // use `make deploy-info tx=<deployment-tx>` to obtain the block number
             previous_spells:        prevSpells, // older spells to ensure are executed first
-            office_hours_enabled:   true,       // true if officehours is expected to be enabled in the spell
+            office_hours_enabled:   false,      // true if officehours is expected to be enabled in the spell
             expiration_threshold:   30 days     // Amount of time before spell expires
         });
 
