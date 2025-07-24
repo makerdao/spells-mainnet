04341350079547f63bdbcf761f3d438677641085
SidestreamColdMelon <SASCoreUnit+1@gmail.com>
2025-07-24 16:08:38 +0200

    add deployed spell info

--- src/test/config.sol
+++ src/test/config.sol
@@ -118,9 +118,9 @@ contract Config {
         // Values for spell-specific parameters
         //
         spellValues = SpellValues({
-            deployed_spell:         address(0), // populate with deployed spell if deployed
-            deployed_spell_created: 0, // use `make deploy-info tx=<deployment-tx>` to obtain the timestamp
-            deployed_spell_block:   0,   // use `make deploy-info tx=<deployment-tx>` to obtain the block number
+            deployed_spell:         address(0x12a79fAcd8D6Bbb1583593ef414897A9bA8f0D26), // populate with deployed spell if deployed
+            deployed_spell_created: 1753365743, // use `make deploy-info tx=<deployment-tx>` to obtain the timestamp
+            deployed_spell_block:   22989480,   // use `make deploy-info tx=<deployment-tx>` to obtain the block number
             previous_spells:        prevSpells, // older spells to ensure are executed first
             office_hours_enabled:   true,       // true if officehours is expected to be enabled in the spell
             expiration_threshold:   30 days     // Amount of time before spell expires
